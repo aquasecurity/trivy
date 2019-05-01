a4b4a7ba4ab49c45b18cfc148c6dab782a3d5cb8
knqyf263 <knqyf263@gmail.com>
2019-05-01 16:04:35 +0900

    Add rpm analyzer without rpm command

--- analyzer/pkg/rpm/rpm.go
+++ analyzer/pkg/rpm/rpm.go
@@ -1,18 +1,15 @@
 package rpm
 
 import (
-	"bufio"
-	"errors"
 	"io/ioutil"
 	"os"
-	"os/exec"
-	"strconv"
-	"strings"
+	"path/filepath"
 
 	"golang.org/x/xerrors"
 
 	"github.com/knqyf263/fanal/analyzer"
 	"github.com/knqyf263/fanal/extractor"
+	rpmdb "github.com/knqyf263/go-rpmdb/pkg"
 )
 
 func init() {
@@ -34,76 +31,47 @@ func (a rpmPkgAnalyzer) Analyze(fileMap extractor.FileMap) (pkgs []analyzer.Pack
 		detected = true
 	}
 	if !detected {
-		return pkgs, errors.New("No package detected")
+		return nil, xerrors.New("No package detected")
 	}
 	return pkgs, err
 }
 
 func (a rpmPkgAnalyzer) parsePkgInfo(packageBytes []byte) (pkgs []analyzer.Package, err error) {
-	tmpDir, err := ioutil.TempDir(os.TempDir(), "rpm")
+	tmpDir, err := ioutil.TempDir("", "rpm")
 	defer os.RemoveAll(tmpDir)
 	if err != nil {
-		return pkgs, err
+		return nil, err
 	}
 
-	err = ioutil.WriteFile(tmpDir+"/Packages", packageBytes, 0700)
+	filename := filepath.Join(tmpDir, "Packages")
+	err = ioutil.WriteFile(filename, packageBytes, 0700)
 	if err != nil {
-		return pkgs, err
+		return nil, err
 	}
 
 	// rpm-python 4.11.3 rpm-4.11.3-35.el7.src.rpm
 	// Extract binary package names because RHSA refers to binary package names.
-	out, err := outputPkgInfo(tmpDir)
+	db := rpmdb.DB{}
+	if err = db.Open(filename); err != nil {
+		return nil, err
+	}
+
+	pkgList, err := db.ListPackages()
 	if err != nil {
-		return pkgs, nil
+		return nil, err
 	}
 
-	pkgString := string(out)
-
-	scanner := bufio.NewScanner(strings.NewReader(pkgString))
-	for scanner.Scan() {
-		pkg, err := parseRPMOutput(scanner.Text())
-		if err != nil {
-			return pkgs, err
+	for _, pkg := range pkgList {
+		p := analyzer.Package{
+			Name:    pkg.Name,
+			Epoch:   pkg.Epoch,
+			Version: pkg.Version,
+			Release: pkg.Release,
 		}
-		pkgs = append(pkgs, pkg)
+		pkgs = append(pkgs, p)
 	}
+
 	return pkgs, nil
-}
-
-func parseRPMOutput(line string) (pkg analyzer.Package, err error) {
-	fields := strings.Fields(line)
-	if len(fields) != 4 {
-		return pkg, xerrors.Errorf("Failed to parse package line: %s", line)
-	}
-
-	var epoch int
-	epochStr := fields[1]
-	if epochStr == "0" || epochStr == "(none)" {
-		epoch = 0
-	} else {
-		epoch, err = strconv.Atoi(epochStr)
-		if err != nil {
-			return pkg, err
-		}
-	}
-
-	return analyzer.Package{
-		Name:    fields[0],
-		Epoch:   epoch,
-		Version: fields[2],
-		Release: fields[3],
-	}, nil
-}
-
-func outputPkgInfo(dir string) (out []byte, err error) {
-	const old = "%{NAME} %{EPOCH} %{VERSION} %{RELEASE}\n"
-	const new = "%{NAME} %{EPOCHNUM} %{VERSION} %{RELEASE}\n"
-	out, err = exec.Command("rpm", "--dbpath", dir, "-qa", "--qf", new).Output()
-	if err != nil {
-		return exec.Command("rpm", "--dbpath", dir, "-qa", "--qf", old).Output()
-	}
-	return out, nil
 }
 
 func (a rpmPkgAnalyzer) RequiredFiles() []string {
