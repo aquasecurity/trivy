b62290eee0f7e6113eb6196e80b9ef86316d098b
knqyf263 <knqyf263@gmail.com>
2019-05-06 12:41:50 +0900

    update

---
+++ ci/deploy-deb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RELEASES=(wheezy jessie stretch buster trusty xenial bionic)
+
+cd trivy-repo/deb
+
+for release in ${RELEASES[@]}; do
+  echo "Adding deb package to $release"
+  reprepro remove $release trivy
+  reprepro includedeb $release ../../dist/*.deb
+done
+
+git add .
+git commit -m "Update deb packages"
+git push origin master
---
+++ ci/deploy-rpm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+RPM_EL6=$(find dist/ -type f -name "*.rpm" -printf "%f\n" | head -n1 | sed -e 's/_/-/g' -e 's/-Linux/.el6/' -e 's/-64bit/.x86_64/')
+RPM_EL7=$(find dist/ -type f -name "*.rpm" -printf "%f\n" | head -n1 | sed -e 's/_/-/g' -e 's/-Linux/.el7/' -e 's/-64bit/.x86_64/')
+
+cd trivy-repo
+mkdir -p rpm/releases/6/x86_64
+mkdir -p rpm/releases/7/x86_64
+
+cd rpm
+cp ../../dist/*.rpm releases/6/x86_64/${RPM_EL6}
+cp ../../dist/*.rpm releases/7/x86_64/${RPM_EL7}
+
+createrepo releases/6/x86_64/
+createrepo releases/7/x86_64/
+
+git add .
+git commit -m "Update rpm packages"
+git push origin master
+
