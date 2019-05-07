1778abe4e3d50adb5fb4f44cb5dae8489bdf6d8f
Teppei Fukuda <knqyf263@gmail.com>
2019-05-07 21:32:06 +0900

    Clear cache (fanal#8)

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,19 @@
 package utils
 
+import (
+	"os"
+	"path/filepath"
+)
+
+func CacheDir() string {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		cacheDir = os.TempDir()
+	}
+	dir := filepath.Join(cacheDir, "fanal")
+	return dir
+}
+
 func StringInSlice(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
