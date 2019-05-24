73635e0f22a1f7abeceb9cb41c8759bb40ce1042
Snow-HardWolf <e155728@ie.u-ryukyu.ac.jp>
2019-05-24 13:26:41 +0900

    auth for private docker registry from ENV vars (#52)
    * auth for private docker registry from env vars
    
    * fix typo
    
    * delete GCP and AWS ENV vars
    
    * update TOC
    
    * fix typo

---
+++ pkg/types/dockerConf.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	"time"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/knqyf263/fanal/types"
+)
+
+type DockerConfig struct {
+	AuthURL  string        `env:"TRIVY_AUTH_URL"`
+	UserName string        `env:"TRIVY_USERNAME"`
+	Password string        `env:"TRIVY_PASSWORD"`
+	Timeout  time.Duration `env:"TRIVY_TIMEOUT_SEC" envDefault:"60s"`
+	Insecure bool          `env:"TRIVY_INSECURE" envDefault:"true"`
+	NonSSL   bool          `env:"TRIVY_NON_SSL" envDefault:"false"`
+}
+
+func GetDockerOption() (types.DockerOption, error) {
+	cfg := DockerConfig{}
+	if err := env.Parse(&cfg); err != nil {
+		return types.DockerOption{}, err
+	}
+	return types.DockerOption{
+		AuthURL:  cfg.AuthURL,
+		UserName: cfg.UserName,
+		Password: cfg.Password,
+		Timeout:  cfg.Timeout,
+		Insecure: cfg.Insecure,
+		NonSSL:   cfg.NonSSL,
+	}, nil
+}
