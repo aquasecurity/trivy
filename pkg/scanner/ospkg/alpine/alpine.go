0611bf915bffa762b9f3407f51497f13c1f403b8
Teppei Fukuda <knqyf263@gmail.com>
2019-08-18 21:34:46 -1000

    Display a warning for OS that has reached EOL (#118)

--- pkg/scanner/ospkg/alpine/alpine.go
+++ pkg/scanner/ospkg/alpine/alpine.go
@@ -2,6 +2,7 @@ package alpine
 
 import (
 	"strings"
+	"time"
 
 	"github.com/knqyf263/fanal/analyzer"
 	version "github.com/knqyf263/go-rpm-version"
@@ -10,6 +11,30 @@ import (
 	"github.com/knqyf263/trivy/pkg/vulnsrc/alpine"
 	"github.com/knqyf263/trivy/pkg/vulnsrc/vulnerability"
 	"golang.org/x/xerrors"
+)
+
+var (
+	eolDates = map[string]time.Time{
+		"2.0":  time.Date(2012, 4, 1, 23, 59, 59, 0, time.UTC),
+		"2.1":  time.Date(2012, 11, 1, 23, 59, 59, 0, time.UTC),
+		"2.2":  time.Date(2013, 5, 1, 23, 59, 59, 0, time.UTC),
+		"2.3":  time.Date(2013, 11, 1, 23, 59, 59, 0, time.UTC),
+		"2.4":  time.Date(2014, 5, 1, 23, 59, 59, 0, time.UTC),
+		"2.5":  time.Date(2014, 11, 1, 23, 59, 59, 0, time.UTC),
+		"2.6":  time.Date(2015, 5, 1, 23, 59, 59, 0, time.UTC),
+		"2.7":  time.Date(2015, 11, 1, 23, 59, 59, 0, time.UTC),
+		"3.0":  time.Date(2016, 5, 1, 23, 59, 59, 0, time.UTC),
+		"3.1":  time.Date(2016, 11, 1, 23, 59, 59, 0, time.UTC),
+		"3.2":  time.Date(2017, 5, 1, 23, 59, 59, 0, time.UTC),
+		"3.3":  time.Date(2017, 11, 1, 23, 59, 59, 0, time.UTC),
+		"3.4":  time.Date(2018, 5, 1, 23, 59, 59, 0, time.UTC),
+		"3.5":  time.Date(2018, 11, 1, 23, 59, 59, 0, time.UTC),
+		"3.6":  time.Date(2019, 5, 1, 23, 59, 59, 0, time.UTC),
+		"3.7":  time.Date(2019, 11, 1, 23, 59, 59, 0, time.UTC),
+		"3.8":  time.Date(2020, 5, 1, 23, 59, 59, 0, time.UTC),
+		"3.9":  time.Date(2020, 11, 1, 23, 59, 59, 0, time.UTC),
+		"3.10": time.Date(2021, 5, 1, 23, 59, 59, 0, time.UTC),
+	}
 )
 
 type Scanner struct{}
@@ -50,4 +75,22 @@ func (s *Scanner) Detect(osVer string, pkgs []analyzer.Package) ([]vulnerability
 		}
 	}
 	return vulns, nil
+}
+
+func (s *Scanner) IsSupportedVersion(osFamily, osVer string) bool {
+	now := time.Now()
+	return s.isSupportedVersion(now, osFamily, osVer)
+}
+
+func (s *Scanner) isSupportedVersion(now time.Time, osFamily, osVer string) bool {
+	if strings.Count(osVer, ".") > 1 {
+		osVer = osVer[:strings.LastIndex(osVer, ".")]
+	}
+
+	eol, ok := eolDates[osVer]
+	if !ok {
+		log.Logger.Warnf("This OS version is not on the EOL list: %s %s", osFamily, osVer)
+		return false
+	}
+	return now.Before(eol)
 }
--- pkg/scanner/ospkg/ubuntu/ubuntu.go
+++ pkg/scanner/ospkg/ubuntu/ubuntu.go
@@ -1,6 +1,8 @@
 package ubuntu
 
 import (
+	"time"
+
 	version "github.com/knqyf263/go-deb-version"
 	"github.com/knqyf263/trivy/pkg/scanner/utils"
 	"github.com/knqyf263/trivy/pkg/vulnsrc/vulnerability"
@@ -9,6 +11,42 @@ import (
 	"github.com/knqyf263/fanal/analyzer"
 	"github.com/knqyf263/trivy/pkg/log"
 	"github.com/knqyf263/trivy/pkg/vulnsrc/ubuntu"
+)
+
+var (
+	eolDates = map[string]time.Time{
+		"4.10":  time.Date(2006, 4, 30, 23, 59, 59, 0, time.UTC),
+		"5.04":  time.Date(2006, 10, 31, 23, 59, 59, 0, time.UTC),
+		"5.10":  time.Date(2007, 4, 13, 23, 59, 59, 0, time.UTC),
+		"6.06":  time.Date(2011, 6, 1, 23, 59, 59, 0, time.UTC),
+		"6.10":  time.Date(2008, 4, 25, 23, 59, 59, 0, time.UTC),
+		"7.04":  time.Date(2008, 10, 19, 23, 59, 59, 0, time.UTC),
+		"7.10":  time.Date(2009, 4, 18, 23, 59, 59, 0, time.UTC),
+		"8.04":  time.Date(2013, 5, 9, 23, 59, 59, 0, time.UTC),
+		"8.10":  time.Date(2010, 4, 30, 23, 59, 59, 0, time.UTC),
+		"9.04":  time.Date(2010, 10, 23, 23, 59, 59, 0, time.UTC),
+		"9.10":  time.Date(2011, 4, 29, 23, 59, 59, 0, time.UTC),
+		"10.04": time.Date(2015, 4, 29, 23, 59, 59, 0, time.UTC),
+		"10.10": time.Date(2012, 4, 10, 23, 59, 59, 0, time.UTC),
+		"11.04": time.Date(2012, 10, 28, 23, 59, 59, 0, time.UTC),
+		"11.10": time.Date(2013, 5, 9, 23, 59, 59, 0, time.UTC),
+		"12.04": time.Date(2019, 4, 26, 23, 59, 59, 0, time.UTC),
+		"12.10": time.Date(2014, 5, 16, 23, 59, 59, 0, time.UTC),
+		"13.04": time.Date(2014, 1, 27, 23, 59, 59, 0, time.UTC),
+		"13.10": time.Date(2014, 7, 17, 23, 59, 59, 0, time.UTC),
+		"14.04": time.Date(2022, 4, 25, 23, 59, 59, 0, time.UTC),
+		"14.10": time.Date(2015, 7, 23, 23, 59, 59, 0, time.UTC),
+		"15.04": time.Date(2016, 1, 23, 23, 59, 59, 0, time.UTC),
+		"15.10": time.Date(2016, 7, 22, 23, 59, 59, 0, time.UTC),
+		"16.04": time.Date(2024, 4, 21, 23, 59, 59, 0, time.UTC),
+		"16.10": time.Date(2017, 7, 20, 23, 59, 59, 0, time.UTC),
+		"17.04": time.Date(2018, 1, 13, 23, 59, 59, 0, time.UTC),
+		"17.10": time.Date(2018, 7, 19, 23, 59, 59, 0, time.UTC),
+		"18.04": time.Date(2028, 4, 26, 23, 59, 59, 0, time.UTC),
+		"18.10": time.Date(2019, 7, 18, 23, 59, 59, 0, time.UTC),
+		"19.04": time.Date(2020, 1, 18, 23, 59, 59, 0, time.UTC),
+		"19.10": time.Date(2020, 7, 17, 23, 59, 59, 0, time.UTC),
+	}
 )
 
 type Scanner struct{}
@@ -61,4 +99,18 @@ func (s *Scanner) Detect(osVer string, pkgs []analyzer.Package) ([]vulnerability
 		}
 	}
 	return vulns, nil
+}
+
+func (s *Scanner) IsSupportedVersion(osFamily, osVer string) bool {
+	now := time.Now()
+	return s.isSupportedVersion(now, osFamily, osVer)
+}
+
+func (s *Scanner) isSupportedVersion(now time.Time, osFamily, osVer string) bool {
+	eol, ok := eolDates[osVer]
+	if !ok {
+		log.Logger.Warnf("This OS version is not on the EOL list: %s %s", osFamily, osVer)
+		return false
+	}
+	return now.Before(eol)
 }
