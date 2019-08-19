3636539d64422371a73acfa07124f7ed974d5e32
Snow-HardWolf <e155728@ie.u-ryukyu.ac.jp>
2019-08-18 21:34:46 -1000

    Display a warning for OS that has reached EOL (#118)

---
+++ pkg/scanner/ospkg/debian/debian_test.go
@@ -0,0 +1,64 @@
+package debian
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/knqyf263/trivy/pkg/log"
+)
+
+func TestMain(m *testing.M) {
+	log.InitLogger(false)
+	os.Exit(m.Run())
+}
+
+func TestScanner_IsSupportedVersion(t *testing.T) {
+	vectors := map[string]struct {
+		now       time.Time
+		osFamily  string
+		osVersion string
+		expected  bool
+	}{
+		"debian7": {
+			now:       time.Date(2019, 3, 31, 23, 59, 59, 0, time.UTC),
+			osFamily:  "debian",
+			osVersion: "7",
+			expected:  false,
+		},
+		"debian8": {
+			now:       time.Date(2019, 3, 31, 23, 59, 59, 0, time.UTC),
+			osFamily:  "debian",
+			osVersion: "8.11",
+			expected:  true,
+		},
+		"debian8 eol ends": {
+			now:       time.Date(2020, 7, 31, 23, 59, 59, 0, time.UTC),
+			osFamily:  "debian",
+			osVersion: "8.0",
+			expected:  false,
+		},
+		"debian9": {
+			now:       time.Date(2020, 7, 31, 23, 59, 59, 0, time.UTC),
+			osFamily:  "debian",
+			osVersion: "9",
+			expected:  true,
+		},
+		"unknown": {
+			now:       time.Date(2020, 7, 31, 23, 59, 59, 0, time.UTC),
+			osFamily:  "debian",
+			osVersion: "unknown",
+			expected:  false,
+		},
+	}
+
+	for testName, v := range vectors {
+		s := NewScanner()
+		t.Run(testName, func(t *testing.T) {
+			actual := s.isSupportedVersion(v.now, v.osFamily, v.osVersion)
+			if actual != v.expected {
+				t.Errorf("[%s] got %v, want %v", testName, actual, v.expected)
+			}
+		})
+	}
+}
