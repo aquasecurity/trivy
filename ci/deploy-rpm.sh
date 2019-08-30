8f04fd3903b33885e79319520bdb4652a5f62307
knqyf263 <knqyf263@gmail.com>
2019-08-30 13:07:15 +0900

    Use variable

--- ci/deploy-rpm.sh
+++ ci/deploy-rpm.sh
@@ -2,14 +2,15 @@
 
 function create_rpm_repo () {
         version=$1
+        rpm_path=rpm/releases/${version}/x86_64
 
         RPM_EL=$(find ../dist/ -type f -name "*64bit.rpm" -printf "%f\n" | head -n1 | sed -e "s/_/-/g" -e "s/-Linux/.el$version/" -e "s/-64bit/.x86_64/")
         echo $RPM_EL
 
-        mkdir -p rpm/releases/${version}/x86_64
-        cp ../dist/*64bit.rpm rpm/releases/${version}/x86_64/${RPM_EL}
+        mkdir -p $rpm_path
+        cp ../dist/*64bit.rpm ${rpm_path}/${RPM_EL}
 
-        createrepo --update rpm/releases/${version}/x86_64/
+        createrepo --update $rpm_path
 }
 
 cd trivy-repo
