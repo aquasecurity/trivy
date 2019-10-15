231a974823d80d0d27d57b9aa1605f489cf4916c
Simarpreet Singh <simar@linux.com>
2019-10-15 14:38:42 -0700

    writer_test: Add a happy path for TestReportWriter
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- pkg/report/writer_test.go
+++ pkg/report/writer_test.go
@@ -1,1 +1,41 @@
-package report
+package report_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/aquasecurity/trivy/pkg/vulnsrc/vulnerability"
+
+	"github.com/aquasecurity/trivy/pkg/report"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReportWriter(t *testing.T) {
+	tw := report.TableWriter{}
+	inputResults := report.Results{
+		{
+			FileName: "foo",
+			Vulnerabilities: []vulnerability.DetectedVulnerability{
+				{
+					VulnerabilityID:  "123",
+					PkgName:          "foo",
+					InstalledVersion: "1.2.3",
+					FixedVersion:     "3.4.5",
+					Title:            "foobar",
+					Description:      "baz",
+					Severity:         "HIGH",
+				},
+			},
+		},
+	}
+	tableWritten := bytes.Buffer{}
+	tw.Output = &tableWritten
+	assert.NoError(t, tw.Write(inputResults))
+	assert.Equal(t, `+---------+------------------+----------+-------------------+---------------+--------+
+| LIBRARY | VULNERABILITY ID | SEVERITY | INSTALLED VERSION | FIXED VERSION | TITLE  |
++---------+------------------+----------+-------------------+---------------+--------+
+| foo     |              123 | HIGH     | 1.2.3             | 3.4.5         | foobar |
++---------+------------------+----------+-------------------+---------------+--------+
+`, tableWritten.String())
+
+}
