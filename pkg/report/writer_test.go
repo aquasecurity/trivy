e8ff28a851493e7409a61e57aeceafb7097f6fc0
knqyf263 <knqyf263@gmail.com>
2019-10-18 23:37:46 +0300

    test(template): add happy path

--- pkg/report/writer_test.go
+++ pkg/report/writer_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"testing"
+	"text/template"
 
 	"github.com/aquasecurity/trivy/pkg/vulnsrc/vulnerability"
 
@@ -83,17 +84,19 @@ func TestReportWriter_Table(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		tw := report.TableWriter{}
-		inputResults := report.Results{
-			{
-				FileName:        "foo",
-				Vulnerabilities: tc.detectedVulns,
-			},
-		}
-		tableWritten := bytes.Buffer{}
-		tw.Output = &tableWritten
-		assert.Nil(t, tw.Write(inputResults))
-		assert.Equal(t, tc.expectedOutput, tableWritten.String(), tc.name)
+		t.Run(tc.name, func(t *testing.T) {
+			tw := report.TableWriter{}
+			inputResults := report.Results{
+				{
+					FileName:        "foo",
+					Vulnerabilities: tc.detectedVulns,
+				},
+			}
+			tableWritten := bytes.Buffer{}
+			tw.Output = &tableWritten
+			assert.Nil(t, tw.Write(inputResults))
+			assert.Equal(t, tc.expectedOutput, tableWritten.String(), tc.name)
+		})
 	}
 }
 
@@ -130,41 +133,64 @@ func TestReportWriter_JSON(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		jw := report.JsonWriter{}
-		jsonWritten := bytes.Buffer{}
-		jw.Output = &jsonWritten
+		t.Run(tc.name, func(t *testing.T) {
+			jw := report.JsonWriter{}
+			jsonWritten := bytes.Buffer{}
+			jw.Output = &jsonWritten
 
-		err := jw.Write(report.Results{
-			{
-				FileName:        "foojson",
-				Vulnerabilities: tc.detectedVulns,
-			},
+			err := jw.Write(report.Results{
+				{
+					FileName:        "foojson",
+					Vulnerabilities: tc.detectedVulns,
+				},
+			})
+
+			writtenResults := report.Results{}
+			errJson := json.Unmarshal([]byte(jsonWritten.String()), &writtenResults)
+			assert.NoError(t, errJson, "invalid json written", tc.name)
+
+			assert.Equal(t, tc.expectedJSON, writtenResults, tc.name)
+			assert.NoError(t, err, tc.name)
 		})
-
-		writtenResults := report.Results{}
-		errJson := json.Unmarshal([]byte(jsonWritten.String()), &writtenResults)
-		assert.NoError(t, errJson, "invalid json written", tc.name)
-
-		assert.Equal(t, tc.expectedJSON, writtenResults, tc.name)
-		assert.NoError(t, err, tc.name)
 	}
 
 }
 
-// TODO: What would be an acceptable example for an input template?
-//func TestReportWriter_Template(t *testing.T) {
-//	tmplWritten := bytes.Buffer{}
-//	tmplw := report.TemplateWriter{
-//		Output:   &tmplWritten,
-//		Template: template.New("happy template"),
-//	}
-//
-//	err := tmplw.Write(report.Results{
-//		{
-//			FileName:        "foojson",
-//			Vulnerabilities: []vulnerability.DetectedVulnerability{},
-//		},
-//	})
-//	assert.NoError(t, err)
-//	assert.Equal(t, "", tmplWritten.String())
-//}
+func TestReportWriter_Template(t *testing.T) {
+	testCases := []struct {
+		name          string
+		detectedVulns []vulnerability.DetectedVulnerability
+		template      string
+		expected      string
+	}{
+		{
+			name: "happy path",
+			detectedVulns: []vulnerability.DetectedVulnerability{
+				{VulnerabilityID: "CVE-2019-0000", PkgName: "foo", Severity: vulnerability.SeverityHigh.String()},
+				{VulnerabilityID: "CVE-2019-0000", PkgName: "bar", Severity: vulnerability.SeverityHigh.String()},
+				{VulnerabilityID: "CVE-2019-0001", PkgName: "baz", Severity: vulnerability.SeverityCritical.String()},
+			},
+			template: "{{ range . }}{{ range .Vulnerabilities}}{{ println .VulnerabilityID .Severity }}{{ end }}{{ end }}",
+			expected: "CVE-2019-0000 HIGH\nCVE-2019-0000 HIGH\nCVE-2019-0001 CRITICAL\n",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tmplWritten := bytes.Buffer{}
+			tmpl, _ := template.New(tc.name).Parse(tc.template)
+			tmplw := report.TemplateWriter{
+				Output:   &tmplWritten,
+				Template: tmpl,
+			}
+
+			err := tmplw.Write(report.Results{
+				{
+					FileName:        "foojson",
+					Vulnerabilities: tc.detectedVulns,
+				},
+			})
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expected, tmplWritten.String())
+		})
+	}
+}
