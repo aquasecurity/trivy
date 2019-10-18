cce341ed2810e3cc3c1eb603cb30133924ea2a6c
Simarpreet Singh <simar@linux.com>
2019-10-18 13:51:23 -0700

    tar_input_test: Move gunzipDB
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- integration/tar_input_test.go
+++ integration/tar_input_test.go
@@ -1,9 +1,13 @@
 package integration_test
 
 import (
+	"compress/gzip"
 	"flag"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -13,6 +17,39 @@ import (
 )
 
 var update = flag.Bool("update", false, "update golden files")
+
+func gunzipDB() string {
+	gz, err := os.Open("testdata/trivy.db.gz")
+	if err != nil {
+		log.Panic(err)
+	}
+	zr, err := gzip.NewReader(gz)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	tmpDir, err := ioutil.TempDir("", "integration")
+	if err != nil {
+		log.Panic(err)
+	}
+	dbDir := filepath.Join(tmpDir, "db")
+	err = os.MkdirAll(dbDir, 0700)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	file, err := os.Create(filepath.Join(dbDir, "trivy.db"))
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, zr)
+	if err != nil {
+		log.Panic(err)
+	}
+	return tmpDir
+}
 
 func TestRun_WithTar(t *testing.T) {
 	type args struct {
