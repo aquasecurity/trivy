55d5c29f3833b1c0d6358c671795467b57f0c9be
knqyf263 <knqyf263@gmail.com>
2019-10-30 16:26:00 +0200

    feat(db): download a db file from GitHub Release

---
+++ pkg/db/db.go
@@ -0,0 +1,90 @@
+package db
+
+import (
+	"compress/gzip"
+	"context"
+	"io"
+	"os"
+	"time"
+
+	"golang.org/x/xerrors"
+
+	"github.com/aquasecurity/trivy-db/pkg/db"
+	"github.com/aquasecurity/trivy/pkg/github"
+	"github.com/aquasecurity/trivy/pkg/log"
+	"github.com/aquasecurity/trivy/pkg/utils"
+)
+
+const (
+	fullDB  = "trivy.db.gz"
+	lightDB = "trivy-light.db.gz"
+)
+
+type Client struct{}
+
+func (c Client) Download(cacheDir string, light bool) error {
+	dbType := db.TypeFull
+	dbFile := fullDB
+	message := " Downloading Full DB file..."
+	if light {
+		dbFile = lightDB
+		message = " Downloading Lightweight DB file..."
+		dbType = db.TypeLight
+	}
+
+	metadata, err := db.GetMetadata()
+	if err != nil {
+		log.Logger.Debug("This is the first run")
+	}
+
+	if metadata.Type == dbType && time.Now().Before(metadata.UpdatedAt) {
+		return nil
+	}
+
+	if err = c.download(cacheDir, message, dbFile); err != nil {
+		return xerrors.Errorf("failed to download the DB file: %w", err)
+	}
+
+	log.Logger.Info("Reopen vulnerability DB")
+	if err = db.Close(); err != nil {
+		return xerrors.Errorf("unable to close old DB: %w", err)
+	}
+	if err = db.Init(cacheDir); err != nil {
+		return xerrors.Errorf("unable to open new DB: %w", err)
+	}
+
+	return nil
+}
+
+func (c Client) download(cacheDir, message, dbFile string) error {
+	spinner := utils.NewSpinner(message)
+	spinner.Start()
+	defer spinner.Stop()
+
+	ctx := context.Background()
+	client := github.NewClient(ctx)
+
+	rc, err := client.DownloadDB(ctx, dbFile)
+	if err != nil {
+		return xerrors.Errorf("failed to download vulnerability DB: %w", err)
+	}
+	defer rc.Close()
+
+	gr, err := gzip.NewReader(rc)
+	if err != nil {
+		return xerrors.Errorf("invalid gzip file: %w", err)
+	}
+
+	dbPath := db.Path(cacheDir)
+	file, err := os.Create(dbPath)
+	if err != nil {
+		return xerrors.Errorf("unable to open DB file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, gr)
+	if err != nil {
+		return xerrors.Errorf("failed to save DB file: %w", err)
+	}
+	return nil
+}
---
+++ pkg/github/github.go
@@ -0,0 +1,119 @@
+package github
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/aquasecurity/trivy-db/pkg/db"
+	"github.com/aquasecurity/trivy/pkg/log"
+	"github.com/google/go-github/v28/github"
+	"golang.org/x/oauth2"
+	"golang.org/x/xerrors"
+)
+
+const (
+	owner = "aquasecurity"
+	repo  = "trivy-db"
+)
+
+type RepositoryInterface interface {
+	ListReleases(ctx context.Context, opt *github.ListOptions) ([]*github.RepositoryRelease, *github.Response, error)
+	DownloadAsset(ctx context.Context, id int64) (io.ReadCloser, string, error)
+}
+
+type Repository struct {
+	repository *github.RepositoriesService
+	git        *github.GitService
+	owner      string
+	repoName   string
+}
+
+func (r Repository) ListReleases(ctx context.Context, opt *github.ListOptions) ([]*github.RepositoryRelease, *github.Response, error) {
+	return r.repository.ListReleases(ctx, r.owner, r.repoName, opt)
+}
+
+func (r Repository) DownloadAsset(ctx context.Context, id int64) (io.ReadCloser, string, error) {
+	return r.repository.DownloadReleaseAsset(ctx, r.owner, r.repoName, id)
+}
+
+type Client struct {
+	Repository RepositoryInterface
+}
+
+func NewClient(ctx context.Context) Client {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	gc := github.NewClient(tc)
+
+	repo := Repository{
+		repository: gc.Repositories,
+		git:        gc.Git,
+		owner:      owner,
+		repoName:   repo,
+	}
+
+	return Client{
+		Repository: repo,
+	}
+}
+
+func (c Client) DownloadDB(ctx context.Context, fileName string) (io.ReadCloser, error) {
+	options := github.ListOptions{}
+	releases, _, err := c.Repository.ListReleases(ctx, &options)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to list releases: %w", err)
+	}
+
+	sort.Slice(releases, func(i, j int) bool {
+		return releases[i].PublishedAt.After(releases[j].PublishedAt.Time)
+	})
+
+	prefix := fmt.Sprintf("v%d", db.SchemaVersion)
+	for _, release := range releases {
+		log.Logger.Debugf("release name: %s", release.GetName())
+		if !strings.HasPrefix(*release.Name, prefix) {
+			continue
+		}
+
+		for _, asset := range release.Assets {
+			rc, err := c.downloadAsset(ctx, asset, fileName)
+			if err != nil {
+				log.Logger.Debug(err)
+				continue
+			}
+			return rc, nil
+		}
+
+	}
+	return nil, xerrors.New("DB file not found")
+}
+
+func (c Client) downloadAsset(ctx context.Context, asset github.ReleaseAsset, fileName string) (io.ReadCloser, error) {
+	log.Logger.Debugf("asset name: %s", *asset.Name)
+	if *asset.Name != fileName {
+		return nil, xerrors.New("file name doesn't match")
+	}
+
+	rc, url, err := c.Repository.DownloadAsset(ctx, asset.GetID())
+	if err != nil {
+		return nil, xerrors.Errorf("unable to download the asset: %w", err)
+	}
+
+	if rc != nil {
+		return rc, nil
+	}
+
+	log.Logger.Debugf("asset URL: %s", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return nil, xerrors.Errorf("unable to download the asset via URL: %w", err)
+	}
+	return resp.Body, nil
+}
