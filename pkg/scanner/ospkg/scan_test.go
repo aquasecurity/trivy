910b96ea6e053216d69d5ebb88e748f383d496ea
knqyf263 <knqyf263@gmail.com>
2019-12-06 19:19:38 +0200

    test(scan): remove an unused mock

--- pkg/scanner/ospkg/scan_test.go
+++ pkg/scanner/ospkg/scan_test.go
@@ -4,8 +4,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/mock"
-
 	ospkg2 "github.com/aquasecurity/trivy/pkg/detector/ospkg"
 
 	"golang.org/x/xerrors"
@@ -19,23 +17,6 @@ import (
 	"github.com/aquasecurity/trivy/pkg/log"
 	"github.com/aquasecurity/trivy/pkg/types"
 )
-
-type MockDetector struct {
-	mock.Mock
-}
-
-func (_m *MockDetector) Detect(a, b string, c []analyzer.Package) ([]types.DetectedVulnerability, error) {
-	ret := _m.Called(a, b, c)
-	ret0 := ret.Get(0)
-	if ret0 == nil {
-		return nil, ret.Error(1)
-	}
-	vulns, ok := ret0.([]types.DetectedVulnerability)
-	if !ok {
-		return nil, ret.Error(1)
-	}
-	return vulns, ret.Error(1)
-}
 
 func TestMain(m *testing.M) {
 	log.InitLogger(false, true)
