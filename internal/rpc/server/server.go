47c0cf09701d4a0ca7a4299208d0eb49f3e0efe4
knqyf263 <knqyf263@gmail.com>
2019-12-07 14:34:27 +0200

    fix(server): display the error detail

--- internal/rpc/server/server.go
+++ internal/rpc/server/server.go
@@ -77,7 +77,7 @@ func updateDBWorker(appVersion, cacheDir string, dbUpdateWg, requestWg *sync.Wai
 
 		log.Logger.Info("Updating DB...")
 		if err = hotUpdateDB(ctx, client, cacheDir, dbUpdateWg, requestWg); err != nil {
-			log.Logger.Error("%+v", err)
+			log.Logger.Errorf("%+v\n", err)
 			continue
 		}
 	}
