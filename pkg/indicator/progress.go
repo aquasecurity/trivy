861074c6c3c3638b35be1f3eabff0ac7d09e829e
knqyf263 <knqyf263@gmail.com>
2019-12-13 17:48:08 +0200

    feat(indicator): add progress bar

--- pkg/indicator/progress.go
+++ pkg/indicator/progress.go
@@ -1,69 +1,40 @@
-package utils
+package indicator
 
 import (
 	"io"
-	"time"
 
-	"github.com/briandowns/spinner"
-	pb "github.com/cheggaaa/pb/v3"
+	"github.com/cheggaaa/pb/v3"
 )
-
-var (
-	Quiet = false
-)
-
-type Spinner struct {
-	client *spinner.Spinner
-}
-
-func NewSpinner(suffix string) *Spinner {
-	if Quiet {
-		return &Spinner{}
-	}
-	s := spinner.New(spinner.CharSets[36], 100*time.Millisecond)
-	s.Suffix = suffix
-	return &Spinner{client: s}
-}
-
-func (s *Spinner) Start() {
-	if s.client == nil {
-		return
-	}
-	s.client.Start()
-}
-func (s *Spinner) Stop() {
-	if s.client == nil {
-		return
-	}
-	s.client.Stop()
-}
 
 type ProgressBar struct {
 	quiet bool
-	bar   *pb.ProgressBar
 }
 
 func NewProgressBar(quiet bool) ProgressBar {
 	return ProgressBar{quiet: quiet}
 }
 
-func (p ProgressBar) Start(total int64) *ProgressBar {
+func (p ProgressBar) Start(total int64) Bar {
 	if p.quiet {
-		return &ProgressBar{}
+		return Bar{}
 	}
 	bar := pb.Full.Start64(total)
-	return &ProgressBar{bar: bar}
+	return Bar{bar: bar}
 }
 
-func (p *ProgressBar) NewProxyReader(r io.Reader) io.Reader {
-	if p.quiet {
+type Bar struct {
+	bar *pb.ProgressBar
+}
+
+func (b Bar) NewProxyReader(r io.Reader) io.Reader {
+	if b.bar == nil {
 		return r
 	}
-	return p.bar.NewProxyReader(r)
+	return b.bar.NewProxyReader(r)
 }
-func (p *ProgressBar) Finish() {
-	if p.quiet {
+func (b Bar) Finish() {
+	if b.bar == nil {
 		return
 	}
-	p.bar.Finish()
+	b.bar.Finish()
 }
