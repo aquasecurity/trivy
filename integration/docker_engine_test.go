122b79cb428e01c860736600bd21983b65ef9b83
Simarpreet Singh <simar@linux.com>
2019-12-17 21:09:09 -0800

    integration: Add an integration test for docker mode
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- integration/docker_engine_test.go
+++ integration/docker_engine_test.go
@@ -9,30 +9,65 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/trivy/internal"
-
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
-
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRun_WithDockerEngine(t *testing.T) {
-	f, err := os.Open("testdata/alpine-310.tar.gz")
-	defer f.Close()
-	assert.NoError(t, err)
+	testCases := []struct {
+		name               string
+		testfile           string
+		expectedOutputFile string
+		expectedError      error
+	}{
+		{
+			name:               "happy path, alpine:3.10",
+			expectedOutputFile: "testdata/alpine-310.json.golden",
+			testfile:           "testdata/fixtures/alpine-310.tar.gz",
+		},
+	}
 
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-	assert.NoError(t, err)
+	for _, tc := range testCases {
+		ctx := context.Background()
+		testfile, _ := os.Open(tc.testfile)
+		cli, _ := client.NewClientWithOpts(client.FromEnv)
 
-	// load image into docker engine
-	_, err = cli.ImageLoad(context.Background(), f, true)
-	assert.NoError(t, err)
+		// ensure image doesnt already exists
+		_, _ = cli.ImageRemove(ctx, tc.testfile, types.ImageRemoveOptions{
+			Force:         true,
+			PruneChildren: true,
+		})
 
-	// run trivy
-	of, _ := ioutil.TempFile("", "integration-docker-engine-*")
-	defer func() {
-		os.Remove(of.Name())
-	}()
-	app := internal.NewApp("dev")
-	assert.NoError(t, app.Run([]string{"--skip-update", "alpine:3.10", "--output", of.Name()}))
+		// load image into docker engine
+		_, err := cli.ImageLoad(ctx, testfile, true)
+		assert.NoError(t, err, tc.name)
+
+		// tag our image to something unique
+		err = cli.ImageTag(ctx, "alpine:3.10", tc.testfile)
+		assert.NoError(t, err, tc.name)
+
+		// run trivy
+		of, _ := ioutil.TempFile("", "integration-docker-engine-*")
+		defer func() {
+			os.Remove(of.Name())
+		}()
+		app := internal.NewApp("dev")
+		assert.NoError(t, app.Run([]string{"--skip-update", "--quiet", "--format=json", "--output", of.Name(), tc.testfile}), tc.name)
+
+		// check for vulnerability output info
+		got, err := ioutil.ReadFile(of.Name())
+		assert.NoError(t, err, tc.name)
+		want, err := ioutil.ReadFile(tc.expectedOutputFile)
+		assert.NoError(t, err, tc.name)
+		assert.JSONEq(t, string(want), string(got))
+
+		// cleanup
+		_, err = cli.ImageRemove(ctx, tc.testfile, types.ImageRemoveOptions{
+			Force:         true,
+			PruneChildren: true,
+		})
+		assert.NoError(t, err, tc.name)
+	}
 
 }
