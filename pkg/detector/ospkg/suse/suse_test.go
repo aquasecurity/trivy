5a36b0d8269645f634b6e3cd33285f6ba46bbd01
masahiro331 <mur4m4s4.331@gmail.com>
2019-12-23 01:53:43 +0900

    Fix suse test

--- pkg/detector/ospkg/suse/suse_test.go
+++ pkg/detector/ospkg/suse/suse_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	suseoval "github.com/aquasecurity/trivy-db/pkg/vulnsrc/suse-oval"
+	susecvrf "github.com/aquasecurity/trivy-db/pkg/vulnsrc/suse-cvrf"
 	"github.com/aquasecurity/trivy/pkg/log"
 
 	"k8s.io/utils/clock"
@@ -19,58 +19,66 @@ func TestMain(m *testing.M) {
 
 func TestScanner_IsSupportedVersion(t *testing.T) {
 	vectors := map[string]struct {
-		clock     clock.Clock
-		osFamily  string
-		osVersion string
-		expected  bool
+		clock        clock.Clock
+		osFamily     string
+		osVersion    string
+		distribution susecvrf.Distribution
+		expected     bool
 	}{
 		"opensuse.leap42.3": {
-			clock:     clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
-			osFamily:  "opensuse.leap",
-			osVersion: "42.3",
-			expected:  true,
+			clock:        clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
+			osFamily:     "opensuse.leap",
+			osVersion:    "42.3",
+			distribution: susecvrf.OpenSUSE,
+			expected:     true,
 		},
 		"opensuse.leap15": {
-			clock:     clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
-			osFamily:  "opensuse.leap",
-			osVersion: "15.0",
-			expected:  true,
+			clock:        clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
+			osFamily:     "opensuse.leap",
+			osVersion:    "15.0",
+			distribution: susecvrf.OpenSUSE,
+			expected:     true,
 		},
 		"opensuse.leap15.1": {
-			clock:     clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
-			osFamily:  "opensuse.leap",
-			osVersion: "15.1",
-			expected:  true,
+			clock:        clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
+			osFamily:     "opensuse.leap",
+			osVersion:    "15.1",
+			distribution: susecvrf.OpenSUSE,
+			expected:     true,
 		},
 		"opensuse.leap15.1-sametime": {
-			clock:     clocktesting.NewFakeClock(time.Date(2020, 11, 30, 23, 59, 59, 0, time.UTC)),
-			osFamily:  "opensuse.leap",
-			osVersion: "15.1",
-			expected:  false,
+			clock:        clocktesting.NewFakeClock(time.Date(2020, 11, 30, 23, 59, 59, 0, time.UTC)),
+			osFamily:     "opensuse.leap",
+			osVersion:    "15.1",
+			distribution: susecvrf.OpenSUSE,
+			expected:     false,
 		},
 		"sles12.3": {
-			clock:     clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
-			osFamily:  "suse linux enterprise server",
-			osVersion: "12.3",
-			expected:  false,
+			clock:        clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
+			osFamily:     "suse linux enterprise server",
+			osVersion:    "12.3",
+			distribution: susecvrf.SUSEEnterpriseLinux,
+			expected:     false,
 		},
 		"sles15": {
-			clock:     clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
-			osFamily:  "suse linux enterprise server",
-			osVersion: "15",
-			expected:  true,
+			clock:        clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
+			osFamily:     "suse linux enterprise server",
+			osVersion:    "15",
+			distribution: susecvrf.SUSEEnterpriseLinux,
+			expected:     true,
 		},
 		"unknown": {
-			clock:     clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
-			osFamily:  "oracle",
-			osVersion: "unknown",
-			expected:  false,
+			clock:        clocktesting.NewFakeClock(time.Date(2019, 5, 31, 23, 59, 59, 0, time.UTC)),
+			osFamily:     "oracle",
+			osVersion:    "unknown",
+			distribution: susecvrf.SUSEEnterpriseLinux,
+			expected:     false,
 		},
 	}
 
 	for testName, v := range vectors {
 		s := &Scanner{
-			vs:    suseoval.NewVulnSrc(),
+			vs:    susecvrf.NewVulnSrc(v.distribution),
 			clock: v.clock,
 		}
 		t.Run(testName, func(t *testing.T) {
