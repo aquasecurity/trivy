09dafe83db54d09871730244251025eaf8772455
knqyf263 <knqyf263@gmail.com>
2019-12-30 14:07:18 +0200

    feat(client): retry HTTP request when getting an unavailable error

--- pkg/rpc/client/library/client.go
+++ pkg/rpc/client/library/client.go
@@ -3,17 +3,24 @@ package library
 import (
 	"context"
 	"net/http"
+	"time"
 
-	"github.com/aquasecurity/trivy/pkg/rpc/client"
-
+	"github.com/cenkalti/backoff"
 	"github.com/google/wire"
+	"github.com/twitchtv/twirp"
 	"golang.org/x/xerrors"
 
 	ptypes "github.com/aquasecurity/go-dep-parser/pkg/types"
 	detector "github.com/aquasecurity/trivy/pkg/detector/library"
+	"github.com/aquasecurity/trivy/pkg/log"
 	r "github.com/aquasecurity/trivy/pkg/rpc"
+	"github.com/aquasecurity/trivy/pkg/rpc/client"
 	"github.com/aquasecurity/trivy/pkg/types"
 	rpc "github.com/aquasecurity/trivy/rpc/detector"
+)
+
+const (
+	maxRetries = 10
 )
 
 var SuperSet = wire.NewSet(
@@ -41,9 +48,31 @@ func NewDetector(customHeaders CustomHeaders, detector rpc.LibDetector) Detector
 
 func (d Detector) Detect(filePath string, libs []ptypes.Library) ([]types.DetectedVulnerability, error) {
 	ctx := client.WithCustomHeaders(context.Background(), http.Header(d.customHeaders))
-	res, err := d.client.Detect(ctx, &rpc.LibDetectRequest{
-		FilePath:  filePath,
-		Libraries: r.ConvertToRpcLibraries(libs),
+
+	var res *rpc.DetectResponse
+	operation := func() error {
+		var err error
+		res, err = d.client.Detect(ctx, &rpc.LibDetectRequest{
+			FilePath:  filePath,
+			Libraries: r.ConvertToRpcLibraries(libs),
+		})
+		if err != nil {
+			twerr, ok := err.(twirp.Error)
+			if !ok {
+				return backoff.Permanent(err)
+			}
+			if twerr.Code() == twirp.Unavailable {
+				return err
+			}
+			return backoff.Permanent(err)
+		}
+		return nil
+	}
+
+	b := backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxRetries)
+	err := backoff.RetryNotify(operation, b, func(err error, _ time.Duration) {
+		log.Logger.Warn(err)
+		log.Logger.Info("Retrying HTTP request...")
 	})
 	if err != nil {
 		return nil, xerrors.Errorf("failed to detect vulnerabilities via RPC: %w", err)
--- pkg/rpc/client/ospkg/client.go
+++ pkg/rpc/client/ospkg/client.go
@@ -3,17 +3,24 @@ package ospkg
 import (
 	"context"
 	"net/http"
+	"time"
 
-	"github.com/aquasecurity/trivy/pkg/rpc/client"
-
+	"github.com/cenkalti/backoff"
 	"github.com/google/wire"
+	"github.com/twitchtv/twirp"
 	"golang.org/x/xerrors"
 
 	"github.com/aquasecurity/fanal/analyzer"
 	detector "github.com/aquasecurity/trivy/pkg/detector/ospkg"
+	"github.com/aquasecurity/trivy/pkg/log"
 	r "github.com/aquasecurity/trivy/pkg/rpc"
+	"github.com/aquasecurity/trivy/pkg/rpc/client"
 	"github.com/aquasecurity/trivy/pkg/types"
 	rpc "github.com/aquasecurity/trivy/rpc/detector"
+)
+
+const (
+	maxRetries = 10
 )
 
 var SuperSet = wire.NewSet(
@@ -41,10 +48,32 @@ func NewDetector(customHeaders CustomHeaders, detector rpc.OSDetector) Detector 
 
 func (d Detector) Detect(osFamily, osName string, pkgs []analyzer.Package) ([]types.DetectedVulnerability, bool, error) {
 	ctx := client.WithCustomHeaders(context.Background(), http.Header(d.customHeaders))
-	res, err := d.client.Detect(ctx, &rpc.OSDetectRequest{
-		OsFamily: osFamily,
-		OsName:   osName,
-		Packages: r.ConvertToRpcPkgs(pkgs),
+
+	var res *rpc.DetectResponse
+	operation := func() error {
+		var err error
+		res, err = d.client.Detect(ctx, &rpc.OSDetectRequest{
+			OsFamily: osFamily,
+			OsName:   osName,
+			Packages: r.ConvertToRpcPkgs(pkgs),
+		})
+		if err != nil {
+			twerr, ok := err.(twirp.Error)
+			if !ok {
+				return backoff.Permanent(err)
+			}
+			if twerr.Code() == twirp.Unavailable {
+				return err
+			}
+			return backoff.Permanent(err)
+		}
+		return nil
+	}
+
+	b := backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxRetries)
+	err := backoff.RetryNotify(operation, b, func(err error, _ time.Duration) {
+		log.Logger.Warn(err)
+		log.Logger.Info("Retrying HTTP request...")
 	})
 	if err != nil {
 		return nil, false, xerrors.Errorf("failed to detect vulnerabilities via RPC: %w", err)
