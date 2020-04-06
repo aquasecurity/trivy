cfec7a7248901d66837f675bf5f658d0cdae0c6c
Teppei Fukuda <knqyf263@gmail.com>
2020-04-06 19:25:43 +0300

    test(integration): add tests with TLS registry (#99)
    * test(integration): add tests with TLS registry
    
    * test(integration): add the build tag
    
    * test(registry): aggregate imports
    
    * test(registry): remove un-needed expectations

--- go.mod
+++ go.mod
@@ -4,36 +4,32 @@ go 1.13
 
 require (
 	cloud.google.com/go v0.37.4 // indirect
-	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/GoogleCloudPlatform/docker-credential-gcr v1.5.0
-	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/aquasecurity/go-dep-parser v0.0.0-20190819075924-ea223f0ef24b
 	github.com/aws/aws-sdk-go v1.25.31
 	github.com/containers/image/v5 v5.1.0
 	github.com/containers/storage v1.15.5 // indirect
 	github.com/deckarep/golang-set v1.7.1
 	github.com/docker/distribution v2.7.1+incompatible
-	github.com/docker/docker v0.0.0-20180924202107-a9c061deec0f
-	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/docker v0.7.3-0.20190506211059-b20a14b54661
+	github.com/docker/go-connections v0.4.0
 	github.com/docker/go-metrics v0.0.0-20181218153428-b84716841b82 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
-	github.com/gorilla/mux v1.7.1 // indirect
-	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/knqyf263/go-deb-version v0.0.0-20190517075300-09fca494f03d
 	github.com/knqyf263/go-rpmdb v0.0.0-20190501070121-10a1c42a10dc
 	github.com/knqyf263/nested v0.0.1
 	github.com/kylelemons/godebug v0.0.0-20170820004349-d65d576e9348
-	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/opencontainers/go-digest v1.0.0-rc1
-	github.com/pkg/errors v0.8.1
-	github.com/prometheus/client_golang v0.9.3 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.2.1 // indirect
 	github.com/stretchr/testify v1.4.0
+	github.com/testcontainers/testcontainers-go v0.3.1
 	go.etcd.io/bbolt v1.3.3
 	golang.org/x/crypto v0.0.0-20191112222119-e1110fd1c708
 	golang.org/x/net v0.0.0-20191108221443-4ba9e2ef068c // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
 	google.golang.org/genproto v0.0.0-20190502173448-54afdca5d873 // indirect
-	gotest.tools v2.2.0+incompatible
 	k8s.io/client-go v11.0.0+incompatible // indirect
 )
---
+++ integration/registry_test.go
@@ -0,0 +1,219 @@
+// +build integration
+
+package integration
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	testcontainers "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/aquasecurity/fanal/analyzer"
+	"github.com/aquasecurity/fanal/cache"
+	"github.com/aquasecurity/fanal/extractor/docker"
+	testdocker "github.com/aquasecurity/fanal/integration/docker"
+	"github.com/aquasecurity/fanal/types"
+
+	_ "github.com/aquasecurity/fanal/analyzer"
+)
+
+const (
+	registryImage    = "registry:2"
+	registryPort     = "5443/tcp"
+	registryUsername = "testuser"
+	registryPassword = "testpassword"
+)
+
+func TestTLSRegistry(t *testing.T) {
+	ctx := context.Background()
+
+	baseDir, err := filepath.Abs(".")
+	require.NoError(t, err)
+
+	req := testcontainers.ContainerRequest{
+		Name:         "registry",
+		Image:        registryImage,
+		ExposedPorts: []string{registryPort},
+		Env: map[string]string{
+			"REGISTRY_HTTP_ADDR":            "0.0.0.0:5443",
+			"REGISTRY_HTTP_TLS_CERTIFICATE": "/certs/cert.pem",
+			"REGISTRY_HTTP_TLS_KEY":         "/certs/key.pem",
+			"REGISTRY_AUTH":                 "htpasswd",
+			"REGISTRY_AUTH_HTPASSWD_PATH":   "/auth/htpasswd",
+			"REGISTRY_AUTH_HTPASSWD_REALM":  "Registry Realm",
+		},
+		BindMounts: map[string]string{
+			filepath.Join(baseDir, "data", "registry", "certs"): "/certs",
+			filepath.Join(baseDir, "data", "registry", "auth"):  "/auth",
+		},
+		WaitingFor: wait.ForLog("listening on [::]:5443"),
+	}
+
+	registryC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	require.NoError(t, err)
+	defer registryC.Terminate(ctx)
+
+	registryURL, err := getRegistryURL(ctx, registryC, registryPort)
+	require.NoError(t, err)
+
+	config := testdocker.RegistryConfig{
+		URL:      registryURL,
+		Username: registryUsername,
+		Password: registryPassword,
+	}
+
+	testCases := []struct {
+		name       string
+		imageName  string
+		imageFile  string
+		option     types.DockerOption
+		login      bool
+		expectedOS types.OS
+		wantErr    bool
+	}{
+		{
+			name:      "happy path",
+			imageName: "alpine:3.10",
+			imageFile: "testdata/fixtures/alpine-310.tar.gz",
+			option: types.DockerOption{
+				Timeout:               60 * time.Second,
+				UserName:              registryUsername,
+				Password:              registryPassword,
+				InsecureSkipTLSVerify: true,
+			},
+			expectedOS: types.OS{Name: "3.10.2", Family: "alpine"},
+			wantErr:    false,
+		},
+		{
+			name:      "happy path with docker login",
+			imageName: "alpine:3.10",
+			imageFile: "testdata/fixtures/alpine-310.tar.gz",
+			option: types.DockerOption{
+				Timeout:               60 * time.Second,
+				InsecureSkipTLSVerify: true,
+			},
+			login: true,
+			// TODO: this should be false, but there is regression now.
+			// After replacing containers/image with google/go-containerregistry, it is supposed to pass.
+			wantErr: true,
+		},
+		{
+			name:      "sad path: tls verify",
+			imageName: "alpine:3.10",
+			imageFile: "testdata/fixtures/alpine-310.tar.gz",
+			option: types.DockerOption{
+				Timeout:  60 * time.Second,
+				UserName: registryUsername,
+				Password: registryPassword,
+			},
+			wantErr: true,
+		},
+		{
+			name:      "sad path: no credential",
+			imageName: "alpine:3.10",
+			imageFile: "testdata/fixtures/alpine-310.tar.gz",
+			option: types.DockerOption{
+				Timeout:               60 * time.Second,
+				InsecureSkipTLSVerify: true,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			d, err := testdocker.New()
+			require.NoError(t, err)
+
+			// 1. Load a test image from the tar file, tag it and push to the test registry.
+			err = d.ReplicateImage(ctx, tc.imageName, tc.imageFile, config)
+			require.NoError(t, err)
+
+			if tc.login {
+				err = d.Login(ctx, config)
+				require.NoError(t, err)
+
+				defer exec.Command("docker", "logout", registryURL.Host).Run()
+			}
+
+			// 2. Analyze it
+			imageRef := fmt.Sprintf("%s/%s", registryURL.Host, tc.imageName)
+			imageDetail, err := analyze(ctx, imageRef, tc.option)
+			require.Equal(t, tc.wantErr, err != nil)
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, &tc.expectedOS, imageDetail.OS)
+		})
+	}
+}
+
+func getRegistryURL(ctx context.Context, registryC testcontainers.Container, exposedPort nat.Port) (*url.URL, error) {
+	ip, err := registryC.Host(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	port, err := registryC.MappedPort(ctx, exposedPort)
+	if err != nil {
+		return nil, err
+	}
+
+	urlStr := fmt.Sprintf("https://%s:%s", ip, port.Port())
+	return url.Parse(urlStr)
+}
+
+func analyze(ctx context.Context, imageRef string, opt types.DockerOption) (*types.ImageDetail, error) {
+	d, err := ioutil.TempDir("", "TestRegistry-*")
+	if err != nil {
+		return nil, err
+	}
+	defer os.RemoveAll(d)
+
+	c, err := cache.NewFSCache(d)
+	if err != nil {
+		return nil, err
+	}
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+	cli.NegotiateAPIVersion(ctx)
+
+	ext, cleanup, err := docker.NewDockerExtractor(ctx, imageRef, opt)
+	if err != nil {
+		return nil, err
+	}
+	defer cleanup()
+
+	ac := analyzer.New(ext, c)
+	applier := analyzer.NewApplier(c)
+
+	imageInfo, err := ac.Analyze(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	imageDetail, err := applier.ApplyLayers(imageInfo.ID, imageInfo.LayerIDs)
+	if err != nil {
+		return nil, err
+	}
+	return &imageDetail, nil
+}
