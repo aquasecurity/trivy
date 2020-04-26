68389ec78c62a21003f459856e4568f814b75995
Teppei Fukuda <knqyf263@gmail.com>
2020-04-26 13:13:28 +0300

    feat(image): support OCI Image Format (#101)
    * feat(image): support OCI Image Format
    
    * refactor: rename NewDockerArchiveImage to NewArchiveImage
    
    * test: rename TestNewDockerArchiveImage to TestNewArchiveImage
    
    * fix: introduce go-multierror
    
    * image: add more sad paths for tryOCI func
    
    Signed-off-by: Simarpreet Singh <simar@linux.com>
    
    * test(image): add more test case
    
    Co-authored-by: Simarpreet Singh <simar@linux.com>

--- extractor/image/image.go
+++ extractor/image/image.go
@@ -12,9 +12,11 @@ import (
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
-	"github.com/google/go-containerregistry/pkg/v1"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/layout"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/hashicorp/go-multierror"
 	"golang.org/x/xerrors"
 
 	"github.com/aquasecurity/fanal/extractor/image/daemon"
@@ -23,6 +25,8 @@ import (
 )
 
 func NewDockerImage(ctx context.Context, imageName string, option types.DockerOption) (v1.Image, func(), error) {
+	var result error
+
 	var nameOpts []name.Option
 	if option.NonSSL {
 		nameOpts = append(nameOpts, name.Insecure)
@@ -38,6 +42,7 @@ func NewDockerImage(ctx context.Context, imageName string, option types.DockerOp
 		// Return v1.Image if the image is found in Docker Engine
 		return img, cleanup, nil
 	}
+	result = multierror.Append(result, err)
 
 	// Try accessing Docker Registry
 	var remoteOpts []remote.Option
@@ -58,17 +63,38 @@ func NewDockerImage(ctx context.Context, imageName string, option types.DockerOp
 	}
 
 	img, err = remote.Image(ref, remoteOpts...)
-	if err != nil {
-		return nil, func() {}, xerrors.Errorf("unable to access the remote image (%s): %w", ref.Name(), err)
+	if err == nil {
+		// Return v1.Image if the image is found in Docker Registry
+		return img, func() {}, nil
 	}
+	result = multierror.Append(result, err)
 
-	return img, func() {}, nil
+	return nil, func() {}, result
 }
 
-func NewDockerArchiveImage(fileName string) (v1.Image, error) {
+func NewArchiveImage(fileName string) (v1.Image, error) {
+	var result error
+	img, err := tryDockerArchive(fileName)
+	if err == nil {
+		// Return v1.Image if the file can be opened as Docker archive
+		return img, nil
+	}
+	result = multierror.Append(result, err)
+
+	img, err = tryOCI(fileName)
+	if err == nil {
+		// Return v1.Image if the directory can be opened as OCI Image Format
+		return img, nil
+	}
+	result = multierror.Append(result, err)
+
+	return nil, result
+}
+
+func tryDockerArchive(fileName string) (v1.Image, error) {
 	img, err := tarball.Image(fileOpener(fileName), nil)
 	if err != nil {
-		return nil, xerrors.Errorf("unable to open %s as an image: %w", fileName, err)
+		return nil, xerrors.Errorf("unable to open %s as a Docker image: %w", fileName, err)
 	}
 	return img, nil
 }
@@ -92,4 +118,34 @@ func fileOpener(fileName string) func() (io.ReadCloser, error) {
 		}
 		return ioutil.NopCloser(r), nil
 	}
+}
+
+func tryOCI(fileName string) (v1.Image, error) {
+	lp, err := layout.FromPath(fileName)
+	if err != nil {
+		return nil, xerrors.Errorf("unable to open %s as an OCI Image: %w", fileName, err)
+	}
+
+	index, err := lp.ImageIndex()
+	if err != nil {
+		return nil, xerrors.Errorf("unable to retrieve index.json: %w", err)
+	}
+
+	m, err := index.IndexManifest()
+	if err != nil {
+		return nil, xerrors.Errorf("invalid index.json: %w", err)
+	}
+
+	if len(m.Manifests) == 0 {
+		return nil, xerrors.New("no valid manifest")
+	}
+
+	// Support only first image
+	h := m.Manifests[0].Digest
+	img, err := index.Image(h)
+	if err != nil {
+		return nil, xerrors.New("invalid OCI image")
+	}
+
+	return img, nil
 }
