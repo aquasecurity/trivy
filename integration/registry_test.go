f168394a12284aa3e63807800a6f3c45606b0ad9
knqyf263 <knqyf263@gmail.com>
2020-05-19 16:06:43 +0300

    test(integration): comment in terminate

--- integration/registry_test.go
+++ integration/registry_test.go
@@ -116,7 +116,7 @@ func TestRegistry(t *testing.T) {
 	// set up auth server
 	authC, err := setupAuthServer(ctx, baseDir)
 	require.NoError(t, err)
-	//defer authC.Terminate(ctx)
+	defer authC.Terminate(ctx)
 
 	authURL, err := getURL(ctx, authC, authPort)
 	require.NoError(t, err)
@@ -124,7 +124,7 @@ func TestRegistry(t *testing.T) {
 	// set up registry
 	registryC, err := setupRegistry(ctx, baseDir, authURL)
 	require.NoError(t, err)
-	//defer registryC.Terminate(ctx)
+	defer registryC.Terminate(ctx)
 
 	registryURL, err := getURL(ctx, registryC, registryPort)
 	require.NoError(t, err)
