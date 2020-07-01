264415b46e05e3a2432ff04a7176bb7a1f7b929c
Carol <krol3@users.noreply.github.com>
2020-06-30 22:06:44 -0300

    chore: return error in Init

--- internal/artifact/image.go
+++ internal/artifact/image.go
@@ -38,7 +38,7 @@ func ImageRun(cliCtx *cli.Context) error {
 
 	// initialize config
 	if err = c.Init(true); err != nil {
-		return nil
+		return xerrors.Errorf("failed to initialize options: %w", err)
 	}
 
 	if c.Input != "" {
