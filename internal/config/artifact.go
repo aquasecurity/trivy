b6ce7b2883fe4e2ac8a09a729ae74be7f914efbc
Carol Valencia <8355621+krol3@users.noreply.github.com>
2020-08-06 09:20:13 -0300

    Update internal/config/artifact.go
    Co-authored-by: Teppei Fukuda <knqyf263@gmail.com>

--- internal/config/artifact.go
+++ internal/config/artifact.go
@@ -30,7 +30,7 @@ var ErrNoTarget = xerrors.New("no tagert is specified")
 func (c *ArtifactConfig) Init(args cli.Args, logger *zap.SugaredLogger) (err error) {
 	if c.Input == "" && args.Len() == 0 {
 		logger.Debug(`trivy requires at least 1 argument or --input option`)
-		return &ErrNoTarget{}
+		return ErrNoTarget
 	} else if args.Len() > 1 {
 		logger.Error(`multiple targets cannot be specified`)
 		return xerrors.New("arguments error")
