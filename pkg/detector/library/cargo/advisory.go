cc05008d9ee2a13a600e7fdb202371df91051dd8
knqyf263 <knqyf263@gmail.com>
2020-11-09 21:59:15 +0200

    feat(cargo): integrate comparer

--- pkg/detector/library/cargo/advisory.go
+++ pkg/detector/library/cargo/advisory.go
@@ -3,28 +3,31 @@ package cargo
 import (
 	"strings"
 
-	"github.com/Masterminds/semver/v3"
+	dbTypes "github.com/aquasecurity/trivy-db/pkg/types"
+
 	"golang.org/x/xerrors"
 
 	cargoSrc "github.com/aquasecurity/trivy-db/pkg/vulnsrc/cargo"
-	"github.com/aquasecurity/trivy/pkg/scanner/utils"
+	"github.com/aquasecurity/trivy/pkg/detector/library/comparer"
 	"github.com/aquasecurity/trivy/pkg/types"
 )
 
 // Advisory encapsulates the cargo vulnerability scanner
 type Advisory struct {
-	vs cargoSrc.VulnSrc
+	vs       cargoSrc.VulnSrc
+	comparer comparer.Comparer
 }
 
 // NewAdvisory is the factory method to return cargo Scanner
 func NewAdvisory() *Advisory {
 	return &Advisory{
-		vs: cargoSrc.NewVulnSrc(),
+		vs:       cargoSrc.NewVulnSrc(),
+		comparer: comparer.GenericComparer{},
 	}
 }
 
 // DetectVulnerabilities scans and returns the cargo vulnerabilities
-func (s *Advisory) DetectVulnerabilities(pkgName string, pkgVer *semver.Version) ([]types.DetectedVulnerability, error) {
+func (s *Advisory) DetectVulnerabilities(pkgName, pkgVer string) ([]types.DetectedVulnerability, error) {
 	advisories, err := s.vs.Get(pkgName)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to get cargo advisories: %w", err)
@@ -32,14 +35,18 @@ func (s *Advisory) DetectVulnerabilities(pkgName string, pkgVer *semver.Version)
 
 	var vulns []types.DetectedVulnerability
 	for _, advisory := range advisories {
-		if utils.MatchVersions(pkgVer, advisory.PatchedVersions) {
+		adv := dbTypes.Advisory{
+			UnaffectedVersions: advisory.UnaffectedVersions,
+			PatchedVersions:    advisory.PatchedVersions,
+		}
+		if !s.comparer.IsVulnerable(pkgVer, adv) {
 			continue
 		}
 
 		vuln := types.DetectedVulnerability{
 			VulnerabilityID:  advisory.VulnerabilityID,
 			PkgName:          strings.TrimSpace(pkgName),
-			InstalledVersion: pkgVer.String(),
+			InstalledVersion: pkgVer,
 			FixedVersion:     strings.Join(advisory.PatchedVersions, ", "),
 		}
 		vulns = append(vulns, vuln)
