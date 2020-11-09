2466fb0224d7ab996723626ffa15425ab8dcf0b6
knqyf263 <knqyf263@gmail.com>
2020-11-09 22:00:28 +0200

    feat(node): integrate comparer

--- pkg/detector/library/node/advisory.go
+++ pkg/detector/library/node/advisory.go
@@ -5,28 +5,28 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/Masterminds/semver/v3"
-
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/node"
-	"github.com/aquasecurity/trivy/pkg/scanner/utils"
+	"github.com/aquasecurity/trivy/pkg/detector/library/comparer"
+	"github.com/aquasecurity/trivy/pkg/log"
 	"github.com/aquasecurity/trivy/pkg/types"
 )
 
 // Advisory encapsulate Node vulnerability source
 type Advisory struct {
-	vs node.VulnSrc
+	vs       node.VulnSrc
+	comparer comparer.Comparer
 }
 
 // NewAdvisory is the factory method for Node Advisory
 func NewAdvisory() *Advisory {
 	return &Advisory{
-		vs: node.NewVulnSrc(),
+		vs:       node.NewVulnSrc(),
+		comparer: NpmComparer{},
 	}
 }
 
 // DetectVulnerabilities scans and return vulnerability using Node package scanner
-func (s *Advisory) DetectVulnerabilities(pkgName string, pkgVer *semver.Version) ([]types.DetectedVulnerability, error) {
-	replacer := strings.NewReplacer(".alpha", "-alpha", ".beta", "-beta", ".rc", "-rc", " <", ", <", " >", ", >")
+func (s *Advisory) DetectVulnerabilities(pkgName, pkgVer string) ([]types.DetectedVulnerability, error) {
 	advisories, err := s.vs.Get(pkgName)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to get node advisories: %w", err)
@@ -34,35 +34,37 @@ func (s *Advisory) DetectVulnerabilities(pkgName string, pkgVer *semver.Version)
 
 	var vulns []types.DetectedVulnerability
 	for _, advisory := range advisories {
-		// e.g. <= 2.15.0 || >= 3.0.0 <= 3.8.2
-		//  => {"<=2.15.0", ">= 3.0.0, <= 3.8.2"}
-		var vulnerableVersions []string
-		for _, version := range strings.Split(advisory.VulnerableVersions, " || ") {
-			version = strings.TrimSpace(version)
-			vulnerableVersions = append(vulnerableVersions, replacer.Replace(version))
-		}
-
-		if !utils.MatchVersions(pkgVer, vulnerableVersions) {
+		matched, err := s.comparer.MatchVersion(pkgVer, advisory.VulnerableVersions)
+		if err != nil {
+			log.Logger.Warn(err)
+			continue
+		} else if !matched {
 			continue
 		}
 
-		var patchedVersions []string
-		for _, version := range strings.Split(advisory.PatchedVersions, " || ") {
-			version = strings.TrimSpace(version)
-			patchedVersions = append(patchedVersions, replacer.Replace(version))
-		}
-
-		if utils.MatchVersions(pkgVer, patchedVersions) {
+		matched, err = s.comparer.MatchVersion(pkgVer, advisory.PatchedVersions)
+		if err != nil {
+			log.Logger.Warn(err)
+			continue
+		} else if matched {
 			continue
 		}
 
 		vuln := types.DetectedVulnerability{
 			VulnerabilityID:  advisory.VulnerabilityID,
 			PkgName:          pkgName,
-			InstalledVersion: pkgVer.String(),
-			FixedVersion:     strings.Join(patchedVersions, ", "),
+			InstalledVersion: pkgVer,
+			FixedVersion:     createFixedVersions(advisory.PatchedVersions),
 		}
 		vulns = append(vulns, vuln)
 	}
 	return vulns, nil
+}
+
+func createFixedVersions(patchedVersions string) string {
+	var fixedVersions []string
+	for _, s := range strings.Split(patchedVersions, "||") {
+		fixedVersions = append(fixedVersions, strings.TrimSpace(s))
+	}
+	return strings.Join(fixedVersions, ", ")
 }
