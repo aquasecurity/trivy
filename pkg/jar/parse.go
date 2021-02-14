2c78c68d5876e9ef48f503e18b4071560dfd89b2
Teppei Fukuda <knqyf263@gmail.com>
2021-02-14 13:31:28 +0200

    feat: support jar/war/ear (go-dep-parser#16)
    * feat: support jar/war/ear
    
    * feat(jar): support sha1 search
    
    * fix: do not return when MANIFEST.INF is invalid
    
    * feat: add log package
    
    * feat: search by artifactId
    
    * refactor: add runtime scope
    
    * feat: support multiple pom.properties
    
    * test: add test jar
    
    * fix: use fileProps
    
    * fix: replace '+' with ' '
    
    * fix: return when artifactId or version are empty
    
    * refactor(jar): add a debug message
    
    * fix(log): use zap.SugaredLogger

---
+++ pkg/jar/parse.go
@@ -0,0 +1,475 @@
+package jar
+
+import (
+	"archive/zip"
+	"bufio"
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+
+	"go.uber.org/zap"
+	"golang.org/x/xerrors"
+
+	"github.com/aquasecurity/go-dep-parser/pkg/log"
+	"github.com/aquasecurity/go-dep-parser/pkg/types"
+)
+
+const (
+	baseURL         = "http://search.maven.org/solrsearch/select"
+	idQuery         = `g:"%s" AND a:"%s"`
+	artifactIdQuery = `a:"%s" AND p:"jar"`
+	sha1Query       = `1:"%s"`
+)
+
+var (
+	jarFileRegEx = regexp.MustCompile(`^([a-zA-Z0-9\._-]*[^-*])-(\d\S*(?:-SNAPSHOT)?).jar$`)
+)
+
+type conf struct {
+	baseURL      string
+	rootFilePath string
+}
+
+type Option func(*conf)
+
+func WithURL(url string) Option {
+	return func(c *conf) {
+		c.baseURL = url
+	}
+}
+
+func WithFilePath(filePath string) Option {
+	return func(c *conf) {
+		c.rootFilePath = filePath
+	}
+}
+
+func Parse(r io.Reader, opts ...Option) ([]types.Library, error) {
+	c := conf{baseURL: baseURL}
+	for _, opt := range opts {
+		opt(&c)
+	}
+
+	return parseArtifact(c, c.rootFilePath, ioutil.NopCloser(r))
+}
+
+func parseArtifact(c conf, fileName string, r io.ReadCloser) ([]types.Library, error) {
+	defer r.Close()
+
+	log.Logger.Debugw("Parsing Java artifacts...", zap.String("file", fileName))
+
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, xerrors.Errorf("unable to read the jar file: %w", err)
+	}
+	zr, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
+	if err != nil {
+		return nil, xerrors.Errorf("zip error: %w", err)
+	}
+
+	// Try to extract artifactId and version from the file name
+	// e.g. spring-core-5.3.4-SNAPSHOT.jar => sprint-core, 5.3.4-SNAPSHOT
+	fileName = filepath.Base(fileName)
+	fileProps := parseFileName(fileName)
+
+	var libs []types.Library
+	var m manifest
+	var foundPomProps bool
+
+	for _, fileInJar := range zr.File {
+		switch {
+		case filepath.Base(fileInJar.Name) == "pom.properties":
+			props, err := parsePomProperties(fileInJar)
+			if err != nil {
+				return nil, xerrors.Errorf("failed to parse %s: %w", fileInJar.Name, err)
+			}
+			libs = append(libs, props.library())
+
+			// Check if the pom.properties is for the original JAR/WAR/EAR
+			if fileProps.artifactID == props.artifactID && fileProps.version == props.version {
+				foundPomProps = true
+			}
+		case filepath.Base(fileInJar.Name) == "MANIFEST.MF":
+			m, err = parseManifest(fileInJar)
+			if err != nil {
+				return nil, xerrors.Errorf("failed to parse MANIFEST.MF: %w", err)
+			}
+		case isArtifact(fileInJar.Name):
+			fr, err := fileInJar.Open()
+			if err != nil {
+				return nil, xerrors.Errorf("unable to open %s: %w", fileInJar.Name, err)
+			}
+
+			// parse jar/war/ear recursively
+			innerLibs, err := parseArtifact(c, fileInJar.Name, fr)
+			if err != nil {
+				return nil, xerrors.Errorf("failed to parse %s: %w", fileInJar.Name, err)
+			}
+			libs = append(libs, innerLibs...)
+		}
+	}
+
+	// If pom.properties is found, it should be preferred than MANIFEST.MF.
+	if foundPomProps {
+		return libs, nil
+	}
+
+	manifestProps := m.properties()
+	if manifestProps.valid() {
+		// Even if MANIFEST.MF is found, the groupId and artifactId might not be valid.
+		// We have to make sure that the artifact exists actually.
+		if ok, _ := exists(c, manifestProps); ok {
+			// If groupId and artifactId are valid, they will be returned.
+			return append(libs, manifestProps.library()), nil
+		}
+	}
+
+	// If groupId and artifactId are not found, call Maven Central's search API with SHA-1 digest.
+	p, err := searchBySHA1(c, b)
+	if err == nil {
+		return append(libs, p.library()), nil
+	}
+
+	log.Logger.Debugw("No such POM in the central repositories", zap.String("file", fileName))
+
+	// Return when artifactId or version from the file name are empty
+	if fileProps.artifactID == "" || fileProps.version == "" {
+		return libs, nil
+	}
+
+	// Try to search groupId by artifactId via sonatype API
+	// When some artifacts have the same groupIds, it might result in false detection.
+	fileProps.groupID, err = searchByArtifactID(c, fileProps.artifactID)
+	if err == nil {
+		log.Logger.Debugw("POM was determined in a heuristic way", zap.String("file", fileName),
+			zap.String("artifact", fileProps.String()))
+		libs = append(libs, fileProps.library())
+	}
+
+	return libs, nil
+}
+
+func isArtifact(name string) bool {
+	ext := filepath.Ext(name)
+	if ext == ".jar" || ext == ".ear" || ext == ".war" {
+		return true
+	}
+	return false
+}
+
+func parseFileName(fileName string) properties {
+	packageVersion := jarFileRegEx.FindStringSubmatch(fileName)
+	if len(packageVersion) != 3 {
+		return properties{}
+	}
+
+	return properties{
+		artifactID: packageVersion[1],
+		version:    packageVersion[2],
+	}
+}
+
+type properties struct {
+	groupID    string
+	artifactID string
+	version    string
+}
+
+func parsePomProperties(f *zip.File) (properties, error) {
+	file, err := f.Open()
+	if err != nil {
+		return properties{}, xerrors.Errorf("unable to open pom.properties: %w", err)
+	}
+	defer file.Close()
+
+	var p properties
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		switch {
+		case strings.HasPrefix(line, "groupId="):
+			p.groupID = strings.TrimPrefix(line, "groupId=")
+		case strings.HasPrefix(line, "artifactId="):
+			p.artifactID = strings.TrimPrefix(line, "artifactId=")
+		case strings.HasPrefix(line, "version="):
+			p.version = strings.TrimPrefix(line, "version=")
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		return properties{}, xerrors.Errorf("scan error: %w", err)
+	}
+	return p, nil
+}
+
+func (p properties) library() types.Library {
+	return types.Library{
+		Name:    fmt.Sprintf("%s:%s", p.groupID, p.artifactID),
+		Version: p.version,
+	}
+}
+
+func (p properties) valid() bool {
+	return p.groupID != "" && p.artifactID != "" && p.version != ""
+}
+
+func (p properties) String() string {
+	return fmt.Sprintf("%s:%s:%s", p.groupID, p.artifactID, p.version)
+}
+
+type manifest struct {
+	implementationVersion  string
+	implementationTitle    string
+	implementationVendorId string
+	specificationTitle     string
+	specificationVersion   string
+	bundleName             string
+	bundleVersion          string
+	bundleSymbolicName     string
+}
+
+func parseManifest(f *zip.File) (manifest, error) {
+	file, err := f.Open()
+	if err != nil {
+		return manifest{}, xerrors.Errorf("unable to open MANIFEST.MF: %w", err)
+	}
+	defer file.Close()
+
+	var m manifest
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// It is not determined which fields are present in each application.
+		// In some cases, none of them are included, in which case they cannot be detected.
+		switch {
+		case strings.HasPrefix(line, "Implementation-Version:"):
+			m.implementationVersion = strings.TrimPrefix(line, "Implementation-Version:")
+		case strings.HasPrefix(line, "Implementation-Title:"):
+			m.implementationTitle = strings.TrimPrefix(line, "Implementation-Title:")
+		case strings.HasPrefix(line, "Implementation-Vendor-Id:"):
+			m.implementationVendorId = strings.TrimPrefix(line, "Implementation-Vendor-Id:")
+		case strings.HasPrefix(line, "Specification-Version:"):
+			m.specificationVersion = strings.TrimPrefix(line, "Specification-Version:")
+		case strings.HasPrefix(line, "Specification-Title:"):
+			m.specificationTitle = strings.TrimPrefix(line, "Specification-Title:")
+		case strings.HasPrefix(line, "Bundle-Version:"):
+			m.bundleVersion = strings.TrimPrefix(line, "Bundle-Version:")
+		case strings.HasPrefix(line, "Bundle-Name:"):
+			m.bundleName = strings.TrimPrefix(line, "Bundle-Name:")
+		case strings.HasPrefix(line, "Bundle-SymbolicName:"):
+			m.bundleSymbolicName = strings.TrimPrefix(line, "Bundle-SymbolicName:")
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		return manifest{}, xerrors.Errorf("scan error: %w", err)
+	}
+	return m, nil
+}
+
+type apiResponse struct {
+	Response struct {
+		NumFound int `json:"numFound"`
+		Docs     []struct {
+			ID           string `json:"id"`
+			GroupID      string `json:"g"`
+			ArtifactID   string `json:"a"`
+			Version      string `json:"v"`
+			P            string `json:"p"`
+			VersionCount int    `json:versionCount`
+		} `json:"docs"`
+	} `json:"response"`
+}
+
+func (m manifest) properties() properties {
+	groupID, err := m.determineGroupID()
+	if err != nil {
+		return properties{}
+	}
+
+	artifactID, err := m.determineArtifactID()
+	if err != nil {
+		return properties{}
+	}
+
+	version, err := m.determineVersion()
+	if err != nil {
+		return properties{}
+	}
+
+	return properties{
+		groupID:    groupID,
+		artifactID: artifactID,
+		version:    version,
+	}
+}
+
+func (m manifest) determineGroupID() (string, error) {
+	var groupID string
+	switch {
+	case m.implementationVendorId != "":
+		groupID = m.implementationVendorId
+	case m.bundleSymbolicName != "":
+		groupID = m.bundleSymbolicName
+
+		// e.g. "com.fasterxml.jackson.core.jackson-databind" => "com.fasterxml.jackson.core"
+		idx := strings.LastIndex(m.bundleSymbolicName, ".")
+		if idx > 0 {
+			groupID = m.bundleSymbolicName[:idx]
+		}
+	default:
+		return "", xerrors.New("no groupID found")
+	}
+	return strings.TrimSpace(groupID), nil
+}
+
+func (m manifest) determineArtifactID() (string, error) {
+	var artifactID string
+	switch {
+	case m.implementationTitle != "":
+		artifactID = m.implementationTitle
+	case m.specificationTitle != "":
+		artifactID = m.specificationTitle
+	case m.bundleName != "":
+		artifactID = m.bundleName
+	default:
+		return "", xerrors.New("no artifactID found")
+	}
+	return strings.TrimSpace(artifactID), nil
+}
+
+func (m manifest) determineVersion() (string, error) {
+	var version string
+	switch {
+	case m.implementationVersion != "":
+		version = m.implementationVersion
+	case m.specificationVersion != "":
+		version = m.specificationVersion
+	case m.bundleVersion != "":
+		version = m.bundleVersion
+	default:
+		return "", xerrors.New("no version found")
+	}
+	return strings.TrimSpace(version), nil
+}
+
+func exists(c conf, p properties) (bool, error) {
+	req, err := http.NewRequest("GET", c.baseURL, nil)
+	if err != nil {
+		return false, xerrors.Errorf("unable to initialize HTTP client: %w", err)
+	}
+
+	q := req.URL.Query()
+	q.Set("q", fmt.Sprintf(idQuery, p.groupID, p.artifactID))
+	q.Set("rows", "1")
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, xerrors.Errorf("http error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var res apiResponse
+	if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return false, xerrors.Errorf("json decode error: %w", err)
+	}
+	return res.Response.NumFound > 0, nil
+}
+
+func searchBySHA1(c conf, data []byte) (properties, error) {
+	h := sha1.New()
+	_, err := h.Write(data)
+	if err != nil {
+		return properties{}, xerrors.Errorf("unable to calculate SHA-1: %w", err)
+	}
+	digest := hex.EncodeToString(h.Sum(nil))
+
+	req, err := http.NewRequest("GET", c.baseURL, nil)
+	if err != nil {
+		return properties{}, xerrors.Errorf("unable to initialize HTTP client: %w", err)
+	}
+
+	q := req.URL.Query()
+	q.Set("q", fmt.Sprintf(sha1Query, digest))
+	q.Set("rows", "1")
+	q.Set("wt", "json")
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return properties{}, xerrors.Errorf("sha1 search error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var res apiResponse
+	if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return properties{}, xerrors.Errorf("json decode error: %w", err)
+	}
+
+	if len(res.Response.Docs) == 0 {
+		return properties{}, xerrors.Errorf("no artifact found: %s", digest)
+	}
+
+	// Some artifacts might have the same SHA-1 digests.
+	// e.g. "javax.servlet:jstl" and "jstl:jstl"
+	docs := res.Response.Docs
+	sort.Slice(docs, func(i, j int) bool {
+		return docs[i].ID < docs[j].ID
+	})
+	d := docs[0]
+
+	return properties{
+		groupID:    d.GroupID,
+		artifactID: d.ArtifactID,
+		version:    d.Version,
+	}, nil
+}
+
+func searchByArtifactID(c conf, artifactID string) (string, error) {
+	req, err := http.NewRequest("GET", c.baseURL, nil)
+	if err != nil {
+		return "", xerrors.Errorf("unable to initialize HTTP client: %w", err)
+	}
+
+	q := req.URL.Query()
+	q.Set("q", fmt.Sprintf(artifactIdQuery, artifactID))
+	q.Set("rows", "20")
+	q.Set("wt", "json")
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", xerrors.Errorf("artifactID search error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var res apiResponse
+	if err = json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return "", xerrors.Errorf("json decode error: %w", err)
+	}
+
+	if len(res.Response.Docs) == 0 {
+		return "", xerrors.Errorf("no artifact found: %s", artifactID)
+	}
+
+	// Some artifacts might have the same artifactId.
+	// e.g. "javax.servlet:jstl" and "jstl:jstl"
+	docs := res.Response.Docs
+	sort.Slice(docs, func(i, j int) bool {
+		return docs[i].VersionCount > docs[j].VersionCount
+	})
+	d := docs[0]
+
+	return d.GroupID, nil
+}
---
+++ pkg/jar/parse_test.go
@@ -0,0 +1,171 @@
+package jar_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/go-dep-parser/pkg/jar"
+	"github.com/aquasecurity/go-dep-parser/pkg/types"
+)
+
+var (
+	// cd testdata/testimage/maven && docker build -t test .
+	// docker run --rm --name test -it test bash
+	// mvn dependency:tree -Dscope=compile -Dscope=runtime | awk '/:tree/,/BUILD SUCCESS/' | awk 'NR > 1 { print }' | head -n -2 | awk '{print $NF}' | awk -F":" '{printf("{\""$1":"$2"\", \""$4 "\"},\n")}'
+	wantMaven = []types.Library{
+		{"com.example:web-app", "1.0-SNAPSHOT"},
+		{"com.fasterxml.jackson.core:jackson-databind", "2.9.10.6"},
+		{"com.fasterxml.jackson.core:jackson-annotations", "2.9.10"},
+		{"com.fasterxml.jackson.core:jackson-core", "2.9.10"},
+		{"com.cronutils:cron-utils", "9.1.2"},
+		{"org.slf4j:slf4j-api", "1.7.30"},
+		{"org.glassfish:javax.el", "3.0.0"},
+		{"org.apache.commons:commons-lang3", "3.11"},
+	}
+
+	// cd testdata/testimage/gradle && docker build -t test .
+	// docker run --rm --name test -it test bash
+	// gradle app:dependencies --configuration implementation | grep "[+\]---" | cut -d" " -f2 | awk -F":" '{printf("{\""$1":"$2"\", \""$3"\"},\n")}'
+	wantGradle = []types.Library{
+		{"commons-dbcp:commons-dbcp", "1.4"},
+		{"commons-pool:commons-pool", "1.6"},
+		{"log4j:log4j", "1.2.17"},
+		{"org.apache.commons:commons-compress", "1.19"},
+	}
+
+	// manually created
+	wantSHA1 = []types.Library{
+		{"org.springframework:spring-core", "5.3.3"},
+	}
+
+	// manually created
+	wantHeuristic = []types.Library{
+		{"com.example:heuristic", "1.0.0-SNAPSHOT"},
+	}
+
+	// manually created
+	wantFatjar = []types.Library{
+		{"com.google.guava:failureaccess", "1.0.1"},
+		{"com.google.guava:guava", "29.0-jre"},
+		{"com.google.guava:listenablefuture", "9999.0-empty-to-avoid-conflict-with-guava"},
+		{"com.google.j2objc:j2objc-annotations", "1.3"},
+		{"org.apache.hadoop.thirdparty:hadoop-shaded-guava", "1.1.0-SNAPSHOT"},
+	}
+)
+
+type apiResponse struct {
+	Response response `json:"response"`
+}
+
+type response struct {
+	NumFound int   `json:"numFound"`
+	Docs     []doc `json:"docs"`
+}
+
+type doc struct {
+	ID           string `json:"id"`
+	GroupID      string `json:"g"`
+	ArtifactID   string `json:"a"`
+	Version      string `json:"v"`
+	P            string `json:"p"`
+	VersionCount int    `json:versionCount`
+}
+
+func TestParse(t *testing.T) {
+	vectors := []struct {
+		name string
+		file string // Test input file
+		want []types.Library
+	}{
+		{
+			name: "maven",
+			file: "testdata/maven.war",
+			want: wantMaven,
+		},
+		{
+			name: "gradle",
+			file: "testdata/gradle.war",
+			want: wantGradle,
+		},
+		{
+			name: "sha1 search",
+			file: "testdata/test.jar",
+			want: wantSHA1,
+		},
+		{
+			name: "artifactId search",
+			file: "testdata/heuristic-1.0.0-SNAPSHOT.jar",
+			want: wantHeuristic,
+		},
+		{
+			name: "fat jar",
+			file: "testdata/hadoop-shaded-guava-1.1.0-SNAPSHOT.jar",
+			want: wantFatjar,
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := apiResponse{
+			Response: response{
+				NumFound: 1,
+			},
+		}
+
+		switch {
+		case strings.Contains(r.URL.Query().Get("q"), "springframework"):
+			res.Response.NumFound = 0
+		case strings.Contains(r.URL.Query().Get("q"), "c666f5bc47eb64ed3bbd13505a26f58be71f33f0"):
+			res.Response.Docs = []doc{
+				{
+					ID:         "org.springframework.spring-core",
+					GroupID:    "org.springframework",
+					ArtifactID: "spring-core",
+					Version:    "5.3.3",
+				},
+			}
+		case strings.Contains(r.URL.Query().Get("q"), "heuristic"):
+			res.Response.Docs = []doc{
+				{
+					ID:           "org.springframework.heuristic",
+					GroupID:      "org.springframework",
+					ArtifactID:   "heuristic",
+					VersionCount: 10,
+				},
+				{
+					ID:           "com.example.heuristic",
+					GroupID:      "com.example",
+					ArtifactID:   "heuristic",
+					VersionCount: 100,
+				},
+			}
+		}
+		_ = json.NewEncoder(w).Encode(res)
+	}))
+
+	for _, v := range vectors {
+		t.Run(v.name, func(t *testing.T) {
+			f, err := os.Open(v.file)
+			require.NoError(t, err)
+
+			got, err := jar.Parse(f, jar.WithURL(ts.URL), jar.WithFilePath(v.file))
+			require.NoError(t, err)
+
+			sort.Slice(got, func(i, j int) bool {
+				return got[i].Name < got[j].Name
+			})
+			sort.Slice(v.want, func(i, j int) bool {
+				return v.want[i].Name < v.want[j].Name
+			})
+
+			assert.Equal(t, v.want, got)
+		})
+	}
+}
