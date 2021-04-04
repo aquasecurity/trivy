474be14d91a6dc049f3f358070ee8e8e74c602c9
knqyf263 <knqyf263@gmail.com>
2021-04-04 14:18:08 +0300

    feat(misconf): add status

--- pkg/types/misconfiguration.go
+++ pkg/types/misconfiguration.go
@@ -4,10 +4,20 @@ import ftypes "github.com/aquasecurity/fanal/types"
 
 // DetectedMisconfiguration holds detected misconfigurations
 type DetectedMisconfiguration struct {
-	Type       string       `json:",omitempty"`
-	ID         string       `json:",omitempty"`
-	Message    string       `json:",omitempty"`
-	Severity   string       `json:",omitempty"`
-	PrimaryURL string       `json:",omitempty"`
-	Layer      ftypes.Layer `json:",omitempty"`
+	Type       string        `json:",omitempty"`
+	ID         string        `json:",omitempty"`
+	Title      string        `json:",omitempty"`
+	Message    string        `json:",omitempty"`
+	Severity   string        `json:",omitempty"`
+	PrimaryURL string        `json:",omitempty"`
+	Status     MisconfStatus `json:",omitempty"`
+	Layer      ftypes.Layer  `json:",omitempty"`
 }
+
+type MisconfStatus string
+
+const (
+	StatusPassed    MisconfStatus = "PASS"
+	StatusFailure   MisconfStatus = "FAIL"
+	StatusException MisconfStatus = "EXCEPTION"
+)
