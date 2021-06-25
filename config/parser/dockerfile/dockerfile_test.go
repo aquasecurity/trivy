5ac9be612e3550e7e9d4e11d85f94ecaed7f79bb
Teppei Fukuda <knqyf263@gmail.com>
2021-06-25 17:17:26 +0300

    test(dockerfile): add multi-stage build (#187)
    * test(dockerfile): add multi-stage build
    
    * test(dockerfile): add multi-stage Dockerfile

--- config/parser/dockerfile/dockerfile_test.go
+++ config/parser/dockerfile/dockerfile_test.go
@@ -1,7 +1,6 @@
 package dockerfile_test
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"testing"
 
@@ -11,17 +10,177 @@ import (
 	"github.com/aquasecurity/fanal/config/parser/dockerfile"
 )
 
-func Test_dockerParser_Parse(t *testing.T) {
+func TestParser_Parse(t *testing.T) {
 	tests := []struct {
 		name      string
 		inputFile string
-		want      string
+		want      interface{}
 		wantErr   string
 	}{
 		{
 			name:      "happy path",
-			inputFile: "testdata/Dockerfile.deployment",
-			want:      `{"command":{"foo":[{"Cmd":"from","EndLine":1,"Flags":[],"JSON":false,"Original":"FROM foo","Stage":0,"StartLine":1,"SubCmd":"","Value":["foo"]},{"Cmd":"copy","EndLine":2,"Flags":[],"JSON":false,"Original":"COPY . /","Stage":0,"StartLine":2,"SubCmd":"","Value":[".","/"]},{"Cmd":"run","EndLine":3,"Flags":[],"JSON":false,"Original":"RUN echo hello","Stage":0,"StartLine":3,"SubCmd":"","Value":["echo hello"]}]}}`,
+			inputFile: "testdata/Dockerfile",
+			want: map[string]interface{}{
+				"command": map[string]interface{}{
+					"foo": []interface{}{
+						map[string]interface{}{
+							"Cmd":       "from",
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "FROM foo",
+							"Stage":     float64(0),
+							"StartLine": float64(1),
+							"EndLine":   float64(1),
+							"SubCmd":    "",
+							"Value":     []interface{}{"foo"},
+						},
+						map[string]interface{}{
+							"Cmd":       "copy",
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "COPY . /",
+							"Stage":     float64(0),
+							"StartLine": float64(2),
+							"EndLine":   float64(2),
+							"SubCmd":    "",
+							"Value":     []interface{}{".", "/"},
+						},
+						map[string]interface{}{
+							"Cmd":       "run",
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "RUN echo hello",
+							"Stage":     float64(0),
+							"StartLine": float64(3),
+							"EndLine":   float64(3),
+							"SubCmd":    "",
+							"Value":     []interface{}{"echo hello"},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:      "multi stage",
+			inputFile: "testdata/Dockerfile.multi",
+			want: map[string]interface{}{
+				"command": map[string]interface{}{
+					"golang:1.16 AS builder": []interface{}{
+						map[string]interface{}{
+							"Cmd":       "from",
+							"EndLine":   float64(1),
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "FROM golang:1.16 AS builder",
+							"Stage":     float64(0),
+							"StartLine": float64(1),
+							"SubCmd":    "",
+							"Value":     []interface{}{"golang:1.16", "AS", "builder"},
+						},
+						map[string]interface{}{
+							"Cmd":       "workdir",
+							"EndLine":   float64(2),
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "WORKDIR /go/src/github.com/alexellis/href-counter/",
+							"Stage":     float64(0),
+							"StartLine": float64(2),
+							"SubCmd":    "",
+							"Value":     []interface{}{"/go/src/github.com/alexellis/href-counter/"},
+						},
+						map[string]interface{}{
+							"Cmd":       "run",
+							"EndLine":   float64(3),
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "RUN go get -d -v golang.org/x/net/html",
+							"Stage":     float64(0),
+							"StartLine": float64(3),
+							"SubCmd":    "",
+							"Value":     []interface{}{"go get -d -v golang.org/x/net/html"},
+						},
+						map[string]interface{}{
+							"Cmd":       "copy",
+							"EndLine":   float64(4),
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "COPY app.go .",
+							"Stage":     float64(0),
+							"StartLine": float64(4),
+							"SubCmd":    "",
+							"Value":     []interface{}{"app.go", "."},
+						},
+						map[string]interface{}{
+							"Cmd":       "run",
+							"EndLine":   float64(5),
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .",
+							"Stage":     float64(0),
+							"StartLine": float64(5),
+							"SubCmd":    "",
+							"Value":     []interface{}{"CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app ."},
+						},
+					},
+					"alpine:latest": []interface{}{
+						map[string]interface{}{
+							"Cmd":       "from",
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "FROM alpine:latest",
+							"Stage":     float64(1),
+							"StartLine": float64(7),
+							"EndLine":   float64(7),
+							"SubCmd":    "",
+							"Value":     []interface{}{"alpine:latest"},
+						},
+						map[string]interface{}{
+							"Cmd":       "run",
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "RUN apk --no-cache add ca-certificates     && apk add --no-cache bash",
+							"Stage":     float64(1),
+							"StartLine": float64(8),
+							"EndLine":   float64(9),
+							"SubCmd":    "",
+							"Value":     []interface{}{"apk --no-cache add ca-certificates     && apk add --no-cache bash"},
+						},
+						map[string]interface{}{
+							"Cmd":       "workdir",
+							"Flags":     []interface{}{},
+							"JSON":      false,
+							"Original":  "WORKDIR /root/",
+							"Stage":     float64(1),
+							"StartLine": float64(10),
+							"EndLine":   float64(10),
+							"SubCmd":    "",
+							"Value":     []interface{}{"/root/"},
+						},
+						map[string]interface{}{
+							"Cmd":       "copy",
+							"Flags":     []interface{}{"--from=builder"},
+							"JSON":      false,
+							"Original":  "COPY --from=builder /go/src/github.com/alexellis/href-counter/app .",
+							"Stage":     float64(1),
+							"StartLine": float64(11),
+							"EndLine":   float64(11),
+							"SubCmd":    "",
+							"Value":     []interface{}{"/go/src/github.com/alexellis/href-counter/app", "."},
+						},
+						map[string]interface{}{
+							"Cmd":       "cmd",
+							"Flags":     []interface{}{},
+							"JSON":      true,
+							"Original":  `CMD ["./app"]`,
+							"Stage":     float64(1),
+							"StartLine": float64(12),
+							"EndLine":   float64(12),
+							"SubCmd":    "",
+							"Value":     []interface{}{"./app"},
+						},
+					},
+				},
+			},
 		},
 	}
 	for _, tt := range tests {
@@ -38,9 +197,7 @@ func Test_dockerParser_Parse(t *testing.T) {
 				return
 			}
 			assert.NoError(t, err)
-			gotJson, err := json.Marshal(got)
-			assert.NoError(t, err)
-			assert.Equal(t, tt.want, string(gotJson))
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
