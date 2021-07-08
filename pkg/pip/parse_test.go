cbaf5a3befac3a50f27bc2ffa51dd66dca07bc16
Sam Lane <samuel.lane@hotmail.com>
2021-07-08 16:39:20 +0100

    feat(python): support python requirements.txt (go-dep-parser#32)
    Co-authored-by: Ankush K <akhobragade@gmail.com>
    Co-authored-by: Ankush K <akhobragade42@gmail.com>
    Co-authored-by: Teppei Fukuda <knqyf263@gmail.com>

---
+++ pkg/pip/parse_test.go
@@ -0,0 +1,52 @@
+package pip
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/go-dep-parser/pkg/types"
+)
+
+func TestParse(t *testing.T) {
+	vectors := []struct {
+		file string
+		want []types.Library
+	}{
+		{
+			file: "testdata/requirements_flask.txt",
+			want: requirementsFlask,
+		},
+		{
+			file: "testdata/requirements_comments.txt",
+			want: requirementsComments,
+		},
+		{
+			file: "testdata/requirements_spaces.txt",
+			want: requirementsSpaces,
+		},
+		{
+			file: "testdata/requirements_no_version.txt",
+			want: requirementsNoVersion,
+		},
+		{
+			file: "testdata/requirements_operator.txt",
+			want: requirementsOperator,
+		},
+	}
+
+	for _, v := range vectors {
+		t.Run(path.Base(v.file), func(t *testing.T) {
+			f, err := os.Open(v.file)
+			require.NoError(t, err)
+
+			got, err := Parse(f)
+			require.NoError(t, err)
+
+			assert.Equal(t, v.want, got)
+		})
+	}
+}
