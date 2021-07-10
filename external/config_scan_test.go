0e8ab4f8113e4002714af2f4c6434a1999a09bba
Teppei Fukuda <knqyf263@gmail.com>
2021-07-10 11:07:53 +0300

    feat(config): add external scanner (fanal#206)

---
+++ external/config_scan_test.go
@@ -0,0 +1,88 @@
+package external_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/fanal/external"
+	"github.com/aquasecurity/fanal/types"
+)
+
+func TestConfigScanner_Scan(t *testing.T) {
+	type fields struct {
+		policyPaths []string
+		dataPaths   []string
+		namespaces  []string
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		inputDir string
+		want     []types.Misconfiguration
+	}{
+		{
+			name: "deny",
+			fields: fields{
+				policyPaths: []string{"testdata/deny"},
+				namespaces:  []string{"testdata"},
+			},
+			inputDir: "testdata/deny",
+			want: []types.Misconfiguration{
+				{
+					FileType: "dockerfile",
+					FilePath: "Dockerfile",
+					Failures: types.MisconfResults{
+						{
+							Namespace: "testdata.xyz_200",
+							Message:   "Old image",
+							PolicyMetadata: types.PolicyMetadata{
+								ID:       "XYZ-200",
+								Type:     "Docker Security Check",
+								Title:    "Old FROM",
+								Severity: "LOW",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "allow",
+			fields: fields{
+				policyPaths: []string{"testdata/allow"},
+				namespaces:  []string{"testdata"},
+			},
+			inputDir: "testdata/allow",
+			want: []types.Misconfiguration{
+				{
+					FileType: "dockerfile",
+					FilePath: "Dockerfile",
+					Successes: types.MisconfResults{
+						{
+							Namespace: "testdata.xyz_200",
+							PolicyMetadata: types.PolicyMetadata{
+								ID:       "XYZ-200",
+								Type:     "Docker Security Check",
+								Title:    "Old FROM",
+								Severity: "LOW",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, err := external.NewConfigScanner(t.TempDir(),
+				tt.fields.policyPaths, tt.fields.dataPaths, tt.fields.namespaces)
+			require.NoError(t, err)
+
+			got, err := s.Scan(tt.inputDir)
+			require.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
---
+++ external/testdata/deny/Dockerfile
@@ -0,0 +1,3 @@
+FROM alpine:3.10
+
+ADD foo.txt .
