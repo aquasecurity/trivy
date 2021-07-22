f4e32a2071e6c07fd06f329d5f47f83f73c5d246
Andrey Levchenko <levchenko.andrey@gmail.com>
2021-07-22 19:38:29 +0600

    feat(report): add end of service life flag to OS metadata
    PR fix

--- pkg/scanner/scan.go
+++ pkg/scanner/scan.go
@@ -102,10 +102,11 @@ func (s Scanner) ScanArtifact(ctx context.Context, options types.ScanOptions) (r
 	if err != nil {
 		return report.Report{}, xerrors.Errorf("scan failed: %w", err)
 	}
+	osFound.Eosl = eosl
+
 	if eosl {
 		log.Logger.Warnf("This OS version is no longer supported by the distribution: %s %s", osFound.Family, osFound.Name)
 		log.Logger.Warnf("The vulnerability detection may be insufficient because security updates are not provided")
-		osFound.Eosl = eosl
 	}
 
 	return report.Report{
