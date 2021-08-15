5220a45864d503cc7ad36dd18097f66a9fbabad0
Huang Huang <mozillazg101@gmail.com>
2021-08-15 20:53:12 +0800

    Update EOL of Debian 11
    https://bits.debian.org/2021/08/bullseye-released.html

--- pkg/detector/ospkg/debian/debian.go
+++ pkg/detector/ospkg/debian/debian.go
@@ -35,7 +35,7 @@ var (
 		"8":   time.Date(2020, 6, 30, 23, 59, 59, 0, time.UTC),
 		"9":   time.Date(2022, 6, 30, 23, 59, 59, 0, time.UTC),
 		"10":  time.Date(2024, 6, 30, 23, 59, 59, 0, time.UTC),
-		"11":  time.Date(3000, 1, 1, 23, 59, 59, 0, time.UTC),
+		"11":  time.Date(2026, 8, 14, 23, 59, 59, 0, time.UTC),
 		"12":  time.Date(3000, 1, 1, 23, 59, 59, 0, time.UTC),
 	}
 )
