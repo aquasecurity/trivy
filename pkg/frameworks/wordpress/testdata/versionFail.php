cdbcaf2f06e6897be0c45d3b2b021ba184723f81
Ankush K <akhobragade42@gmail.com>
2021-09-02 22:50:39 +0530

    feat: add wordpress parser (go-dep-parser#48)
    * wordpress file parser addition
    
    * adjusted as per new directory structure
    
    * updated for working with go 1.12 env
    
    * adjested directory stucture
    
    * fix(wordpress): handle comments properly
    
    Co-authored-by: knqyf263 <knqyf263@gmail.com>

---
+++ pkg/frameworks/wordpress/testdata/versionFail.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * WordPress Version
+ *
+ * Contains version information for the current WordPress release.
+ *
+ * @package WordPress
+ * @since 1.1.0
+ */
+
+ // $wp_version = '4.5.6-alpha';
+
+ /* 
+    $wp_version = '4.5.6-alpha';
+ */
+
+/**
+ * The WordPress version string.
+ *
+ * @global string $wp_version
+ */
+
+
+/**
+ * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
+ *
+ * @global int $wp_db_version
+ */
+$wp_db_version = 47597;
+
+/**
+ * Holds the TinyMCE version.
+ *
+ * @global string $tinymce_version
+ */
+$tinymce_version = '4960-20190918';
+
+/**
+ * Holds the required PHP version.
+ *
+ * @global string $required_php_version
+ */
+$required_php_version = '5.6.20';
+
+/**
+ * Holds the required MySQL version.
+ *
+ * @global string $required_mysql_version
+ */
+$required_mysql_version = '5.0';
