e8b007de0690f8ba7bca1a705b5b96b6b600344d
Ankush K <akhobragade42@gmail.com>
2021-09-05 11:01:55 +0530

    feat(nodejs): add package.json parser for npm resource (go-dep-parser#49)
    * package.json core package support
    
    * refactor(nodejs): return a single object
    
    * merge packagejson and lock parser
    
    * fixed as per comments. units tests are added as seperate package
    
    * updated license check impementation
    
    * updated test cases as per comment
    
    * refactor
    
    Co-authored-by: Teppei Fukuda <knqyf263@gmail.com>

---
+++ pkg/nodejs/packagejson/parse_test.go
@@ -0,0 +1,76 @@
+package packagejson_test
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/go-dep-parser/pkg/nodejs/packagejson"
+	"github.com/aquasecurity/go-dep-parser/pkg/types"
+)
+
+func TestParse(t *testing.T) {
+	vectors := []struct {
+		name      string
+		inputFile string
+		want      types.Library
+		wantErr   string
+	}{
+		{
+			name:      "happypath",
+			inputFile: "testdata/package.json",
+
+			// docker run --name composer --rm -it node:12-alpine sh
+			// npm init --force
+			// npm install --save promise jquery
+			// npm ls | grep -E -o "\S+@\S+" | awk -F@ 'NR>0 {printf("{\""$1"\", \""$2"\"},\n")}'
+			want: types.Library{
+				Name:    "bootstrap",
+				Version: "5.0.2",
+				License: "MIT",
+			},
+			wantErr: "",
+		},
+		{
+			name:      "happy path - legacy license",
+			inputFile: "testdata/legacy_package.json",
+			want: types.Library{
+				Name:    "angular",
+				Version: "4.1.2",
+				License: "ISC",
+			},
+			wantErr: "",
+		},
+		{
+			name:      "sad path",
+			inputFile: "testdata/invalid_package.json",
+
+			// docker run --name composer --rm -it node:12-alpine sh
+			// npm init --force
+			// npm install --save promise jquery
+			// npm ls | grep -E -o "\S+@\S+" | awk -F@ 'NR>0 {printf("{\""$1"\", \""$2"\"},\n")}'
+			want:    types.Library{},
+			wantErr: "JSON decode error",
+		},
+	}
+
+	for _, v := range vectors {
+		t.Run(path.Base(v.name), func(t *testing.T) {
+			f, err := os.Open(v.inputFile)
+			require.NoError(t, err)
+
+			got, err := packagejson.Parse(f)
+			if v.wantErr != "" {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), v.wantErr)
+				return
+			}
+
+			require.NoError(t, err)
+			assert.Equal(t, v.want, got)
+		})
+	}
+}
