1c877da6fdb472c4b49617b3c6dc13a713f031bc
Teppei Fukuda <knqyf263@gmail.com>
2021-09-07 13:43:44 +0300

    feat(image): revert size (fanal#261)

--- applier/docker.go
+++ applier/docker.go
@@ -71,7 +71,6 @@ func ApplyLayers(layers []types.BlobInfo) types.ArtifactDetail {
 	var mergedLayer types.ArtifactDetail
 
 	for _, layer := range layers {
-		mergedLayer.Size += layer.Size
 		for _, opqDir := range layer.OpaqueDirs {
 			_ = nestedMap.DeleteByString(opqDir, sep)
 		}
