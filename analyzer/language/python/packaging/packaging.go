7b1c02286f20fc9d10d87401fbfd46d66e253524
Teppei Fukuda <knqyf263@gmail.com>
2021-09-13 15:02:06 +0300

    feat(python): support egg zip (#267)

--- analyzer/language/python/packaging/packaging.go
+++ analyzer/language/python/packaging/packaging.go
@@ -1,7 +1,9 @@
 package packaging
 
 import (
+	"archive/zip"
 	"bytes"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -23,6 +25,7 @@ var (
 	requiredFiles = []string{
 		// .egg format
 		// https://setuptools.readthedocs.io/en/latest/deprecated/python_eggs.html#eggs-and-their-formats
+		".egg", // zip format
 		"EGG-INFO/PKG-INFO",
 
 		// .egg-info format: .egg-info can be a file or directory
@@ -39,7 +42,18 @@ type packagingAnalyzer struct{}
 
 // Analyze analyzes egg and wheel files.
 func (a packagingAnalyzer) Analyze(target analyzer.AnalysisTarget) (*analyzer.AnalysisResult, error) {
-	r := bytes.NewReader(target.Content)
+	content := target.Content
+
+	// .egg file is zip format and PKG-INFO needs to be extracted from the zip file.
+	if strings.HasSuffix(target.FilePath, ".egg") {
+		pkginfoInZip, err := a.analyzeEggZip(content)
+		if err != nil {
+			return nil, xerrors.Errorf("egg analysis error: %w", err)
+		}
+		content = pkginfoInZip
+	}
+
+	r := bytes.NewReader(content)
 	lib, err := packaging.Parse(r)
 	if err != nil {
 		return nil, xerrors.Errorf("unable to parse %s: %w", target.FilePath, err)
@@ -57,6 +71,37 @@ func (a packagingAnalyzer) Analyze(target analyzer.AnalysisTarget) (*analyzer.An
 			},
 		},
 	}}, nil
+}
+
+func (a packagingAnalyzer) analyzeEggZip(content []byte) ([]byte, error) {
+	zr, err := zip.NewReader(bytes.NewReader(content), int64(len(content)))
+	if err != nil {
+		return nil, xerrors.Errorf("zip reader error: %w", err)
+	}
+
+	for _, file := range zr.File {
+		if !a.Required(file.Name, nil) {
+			continue
+		}
+
+		return a.open(file)
+	}
+
+	return nil, nil
+}
+
+func (a packagingAnalyzer) open(file *zip.File) ([]byte, error) {
+	f, err := file.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		return nil, xerrors.Errorf("file read error: %w", err)
+	}
+	return b, nil
 }
 
 func (a packagingAnalyzer) Required(filePath string, _ os.FileInfo) bool {
