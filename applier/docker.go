32c35452c28700a4e7971c6d4fc13fde6956a479
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2021-11-28 12:44:30 +0600

    fix(applier): fixed layer applications update (#333)
    * Fix(Docker-image): fixed update applications
    Closes trivy issue #1407
    
    * Test: added tests for Docker ApplyLayers
    
    * test(applier): simplify the case
    
    Co-authored-by: knqyf263 <knqyf263@gmail.com>

--- applier/docker.go
+++ applier/docker.go
@@ -1,6 +1,7 @@
 package applier
 
 import (
+	"strings"
 	"time"
 
 	"github.com/aquasecurity/fanal/types"
@@ -62,6 +63,7 @@ func ApplyLayers(layers []types.BlobInfo) types.ArtifactDetail {
 
 	for _, layer := range layers {
 		for _, opqDir := range layer.OpaqueDirs {
+			opqDir = strings.TrimSuffix(opqDir, sep) //this is necessary so that an empty element is not contribute into the array of the DeleteByString function
 			_ = nestedMap.DeleteByString(opqDir, sep)
 		}
 		for _, whFile := range layer.WhiteoutFiles {
