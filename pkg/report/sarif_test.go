3d8cf3fc167b63b64536a15577944f6b77f094c0
AMF <work@afdesk.com>
2021-12-25 01:34:57 +0600

    add test for misconfigurations

--- pkg/report/sarif_test.go
+++ pkg/report/sarif_test.go
@@ -30,7 +30,7 @@ func TestReportWriter_Sarif(t *testing.T) {
 		expectedResults []*sarif.Result
 	}{
 		{
-			name: "happy path vulnerabilities",
+			name: "report with vulnerabilities",
 			results: report.Results{
 				{
 					Target: "test",
@@ -67,7 +67,7 @@ func TestReportWriter_Sarif(t *testing.T) {
 							"HIGH",
 						},
 						"precision":         "very-high",
-						"security-severity": 8.0,
+						"security-severity": "8.0",
 					},
 					Help: &sarif.MultiformatMessageString{
 						Text:     getStringPointer("Vulnerability CVE-2020-0001\nSeverity: HIGH\nPackage: foo\nFixed Version: 3.4.5\nLink: [CVE-2020-0001](https://avd.aquasec.com/nvd/cve-2020-0001)\nbaz"),
@@ -95,42 +95,123 @@ func TestReportWriter_Sarif(t *testing.T) {
 				},
 			},
 		},
-		/*
-			{
-				name: "happy path misconfigurations",
-				results: report.Results{
-					{
-						Target: "test",
-						Misconfigurations: []types.DetectedMisconfiguration{
-							{
-								Type:       "Kubernetes Security Check",
-								ID:         "KSV001",
-								Title:      "Image tag ':latest' used",
-								Message:    "Message",
-								Severity:   "HIGH",
-								PrimaryURL: "https://avd.aquasec.com/appshield/ksv001",
-								Status:     types.StatusFailure,
-							},
-							{
-								Type:       "Kubernetes Security Check",
-								ID:         "KSV002",
-								Title:      "SYS_ADMIN capability added",
-								Message:    "Message",
-								Severity:   "CRITICAL",
-								PrimaryURL: "https://avd.aquasec.com/appshield/ksv002",
-								Status:     types.StatusPassed,
+		{
+			name: "report with misconfigurations",
+			results: report.Results{
+				{
+					Target: "test",
+					Misconfigurations: []types.DetectedMisconfiguration{
+						{
+							Type:       "Kubernetes Security Check",
+							ID:         "KSV001",
+							Title:      "Image tag ':latest' used",
+							Message:    "Message",
+							Severity:   "HIGH",
+							PrimaryURL: "https://avd.aquasec.com/appshield/ksv001",
+							Status:     types.StatusFailure,
+						},
+						{
+							Type:       "Kubernetes Security Check",
+							ID:         "KSV002",
+							Title:      "SYS_ADMIN capability added",
+							Message:    "Message",
+							Severity:   "CRITICAL",
+							PrimaryURL: "https://avd.aquasec.com/appshield/ksv002",
+							Status:     types.StatusPassed,
+						},
+					},
+				},
+			},
+			expectedResults: []*sarif.Result{
+				{
+					RuleID:    getStringPointer("KSV001"),
+					RuleIndex: getUintPointer(0),
+					Level:     getStringPointer("error"),
+					Message:   sarif.Message{Text: getStringPointer("Artifact: test\nType: \nVulnerability KSV001\nSeverity: HIGH\nMessage: Message\nLink: [KSV001](https://avd.aquasec.com/appshield/ksv001)")},
+					Locations: []*sarif.Location{
+						{
+							PhysicalLocation: &sarif.PhysicalLocation{
+								ArtifactLocation: &sarif.ArtifactLocation{
+									URI:       getStringPointer("test"),
+									URIBaseId: getStringPointer("ROOTPATH"),
+								},
+								Region: &sarif.Region{StartLine: getIntPointer(1)},
 							},
 						},
 					},
 				},
-				expectedResults: []sarifResult{},
-				expectedRules:   []sarifRule{},
+				{
+					RuleID:    getStringPointer("KSV002"),
+					RuleIndex: getUintPointer(1),
+					Level:     getStringPointer("error"),
+					Message:   sarif.Message{Text: getStringPointer("Artifact: test\nType: \nVulnerability KSV002\nSeverity: CRITICAL\nMessage: Message\nLink: [KSV002](https://avd.aquasec.com/appshield/ksv002)")},
+					Locations: []*sarif.Location{
+						{
+							PhysicalLocation: &sarif.PhysicalLocation{
+								ArtifactLocation: &sarif.ArtifactLocation{
+									URI:       getStringPointer("test"),
+									URIBaseId: getStringPointer("ROOTPATH"),
+								},
+								Region: &sarif.Region{StartLine: getIntPointer(1)},
+							},
+						},
+					},
+				},
 			},
-
+			expectedRules: []*sarif.ReportingDescriptor{
+				{
+					ID:               "KSV001",
+					Name:             getStringPointer("OtherVulnerability"),
+					ShortDescription: &sarif.MultiformatMessageString{Text: getStringPointer("KSV001")},
+					FullDescription:  &sarif.MultiformatMessageString{Text: getStringPointer("")},
+					DefaultConfiguration: &sarif.ReportingConfiguration{
+						Level: "error",
+					},
+					HelpURI: getStringPointer("https://avd.aquasec.com/appshield/ksv001"),
+					Properties: map[string]interface{}{
+						"tags": []interface{}{
+							"vulnerability",
+							"security",
+							"HIGH",
+						},
+						"precision":         "very-high",
+						"security-severity": "8.0",
+					},
+					Help: &sarif.MultiformatMessageString{
+						Text:     getStringPointer("Misconfiguration KSV001\nType: Kubernetes Security Check\nSeverity: HIGH\nCheck: Image tag ':latest' used\nMessage: Message\nLink: [KSV001](https://avd.aquasec.com/appshield/ksv001)\n"),
+						Markdown: getStringPointer("**Misconfiguration KSV001**\n| Type | Severity | Check | Message | Link |\n| --- | --- | --- | --- | --- |\n|Kubernetes Security Check|HIGH|Image tag ':latest' used|Message|[KSV001](https://avd.aquasec.com/appshield/ksv001)|\n\n"),
+					},
+				},
+				{
+					ID:               "KSV002",
+					Name:             getStringPointer("OtherVulnerability"),
+					ShortDescription: &sarif.MultiformatMessageString{Text: getStringPointer("KSV002")},
+					FullDescription:  &sarif.MultiformatMessageString{Text: getStringPointer("")},
+					DefaultConfiguration: &sarif.ReportingConfiguration{
+						Level: "error",
+					},
+					HelpURI: getStringPointer("https://avd.aquasec.com/appshield/ksv002"),
+					Properties: map[string]interface{}{
+						"tags": []interface{}{
+							"vulnerability",
+							"security",
+							"CRITICAL",
+						},
+						"precision":         "very-high",
+						"security-severity": "9.0",
+					},
+					Help: &sarif.MultiformatMessageString{
+						Text:     getStringPointer("Misconfiguration KSV002\nType: Kubernetes Security Check\nSeverity: CRITICAL\nCheck: SYS_ADMIN capability added\nMessage: Message\nLink: [KSV002](https://avd.aquasec.com/appshield/ksv002)\n"),
+						Markdown: getStringPointer("**Misconfiguration KSV002**\n| Type | Severity | Check | Message | Link |\n| --- | --- | --- | --- | --- |\n|Kubernetes Security Check|CRITICAL|SYS_ADMIN capability added|Message|[KSV002](https://avd.aquasec.com/appshield/ksv002)|\n\n"),
+					},
+				},
+			},
+		},
+		/*
 			{
 				name:            "no vulns",
-				expectedResults: []sarifResult{},
-				expectedRules:   []sarifRule{},
+				expectedResults: []*sarif.Result{},
+				expectedRules:   []*sarif.ReportingDescriptor{},
 			},
 		*/
 	}
