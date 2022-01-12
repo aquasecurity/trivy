678df0f7d1cd1d9aa217ca1987a2159a8e412a00
masahiro331 <mur4m4s4.331@gmail.com>
2022-01-12 16:51:56 +0900

    feat(purl) add generate purl package utilities

---
+++ pkg/purl/purl.go
@@ -0,0 +1,110 @@
+package purl
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/aquasecurity/fanal/analyzer"
+	"github.com/aquasecurity/fanal/analyzer/os"
+	"github.com/aquasecurity/fanal/types"
+	"github.com/package-url/packageurl-go"
+)
+
+func NewPackageURL(t string, pkg types.Package) packageurl.PackageURL {
+	name := strings.ReplaceAll(pkg.Name, ":", "/")
+	index := strings.LastIndex(name, "/")
+
+	namespace := ""
+
+	pkgName := name
+	if index != -1 {
+		namespace = name[:index]
+		pkgName = name[index+1:]
+	}
+	purl := packageurl.NewPackageURL(purlType(t), namespace, pkgName, pkg.Version, parseQualifier(pkg), "")
+
+	return *purl
+}
+
+func purlType(t string) string {
+	switch t {
+	case string(analyzer.TypeJar):
+		return packageurl.TypeMaven
+	case string(analyzer.TypeBundler), string(analyzer.TypeGemSpec):
+		return packageurl.TypeGem
+	case string(analyzer.TypePythonPkg), string(analyzer.TypePip), string(analyzer.TypePipenv), string(analyzer.TypePoetry):
+		return packageurl.TypePyPi
+	case string(analyzer.TypeGoBinary), string(analyzer.TypeGoMod):
+		return packageurl.TypeGolang
+	case string(analyzer.TypeNpmPkgLock), string(analyzer.TypeNodePkg), string(analyzer.TypeYarn):
+		return packageurl.TypeNPM
+	case os.Alpine:
+		return string(analyzer.TypeApk)
+	case os.Debian, os.Ubuntu:
+		return string(analyzer.TypeDpkg)
+	case os.RedHat, os.CentOS, os.Rocky, os.Alma,
+		os.Amazon, os.Fedora, os.Oracle, os.OpenSUSE,
+		os.OpenSUSELeap, os.OpenSUSETumbleweed, os.SLES, os.Photon:
+		return packageurl.TypeRPM
+	}
+	return t
+}
+
+func parseQualifier(pkg types.Package) packageurl.Qualifiers {
+	qualifiers := packageurl.Qualifiers{}
+	if pkg.Release != "" {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "release",
+			Value: pkg.Release,
+		})
+	}
+	if pkg.Epoch != 0 {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "epoch",
+			Value: strconv.Itoa(pkg.Epoch),
+		})
+	}
+	if pkg.Arch != "" {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "arch",
+			Value: pkg.Arch,
+		})
+	}
+	if pkg.SrcName != "" {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "src_name",
+			Value: pkg.SrcName,
+		})
+	}
+	if pkg.SrcVersion != "" {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "src_version",
+			Value: pkg.SrcVersion,
+		})
+	}
+	if pkg.SrcRelease != "" {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "src_release",
+			Value: pkg.SrcRelease,
+		})
+	}
+	if pkg.SrcEpoch != 0 {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "src_epoch",
+			Value: strconv.Itoa(pkg.SrcEpoch),
+		})
+	}
+	if pkg.Modularitylabel != "" {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "modularitylabel",
+			Value: pkg.Modularitylabel,
+		})
+	}
+	if pkg.FilePath != "" {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "file_path",
+			Value: pkg.FilePath,
+		})
+	}
+	return qualifiers
+}
---
+++ pkg/purl/purl_test.go
@@ -0,0 +1,52 @@
+package purl
+
+import (
+	"testing"
+
+	"github.com/aquasecurity/fanal/types"
+	"gotest.tools/assert"
+)
+
+func TestNewPackageURL(t *testing.T) {
+
+	testCases := []struct {
+		name string
+		typ  string
+		pkg  types.Package
+		want string
+	}{
+		{
+			name: "language package",
+			typ:  "yarn",
+			pkg: types.Package{
+				Name:    "@xtuc/ieee754",
+				Version: "1.2.0",
+			},
+			want: "pkg:npm/%40xtuc/ieee754@1.2.0",
+		},
+		{
+			name: "os package",
+			typ:  "redhat",
+			pkg: types.Package{
+				Name:            "acl",
+				Version:         "2.2.53",
+				Release:         "1.el8",
+				Epoch:           0,
+				Arch:            "aarch64",
+				SrcName:         "acl",
+				SrcVersion:      "2.2.53",
+				SrcRelease:      "1.el8",
+				SrcEpoch:        0,
+				Modularitylabel: "",
+			},
+			want: "pkg:rpm/acl@2.2.53?release=1.el8&arch=aarch64&src_name=acl&src_version=2.2.53&src_release=1.el8",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			packageURL := NewPackageURL(tc.typ, tc.pkg)
+			assert.Equal(t, tc.want, packageURL.ToString(), tc.name)
+		})
+	}
+}
