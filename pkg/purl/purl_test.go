e504acd1cd55a0e63023a155ae0df1ca7d84c86b
masahiro331 <m_fujimura@r.recruit.co.jp>
2022-02-06 22:44:51 +0900

    feat(purl): delete report package dependency

--- pkg/purl/purl_test.go
+++ pkg/purl/purl_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/fanal/types"
-	"github.com/aquasecurity/trivy/pkg/report"
 
 	"gotest.tools/assert"
 )
@@ -12,17 +11,15 @@ import (
 func TestNewPackageURL(t *testing.T) {
 
 	testCases := []struct {
-		name  string
-		typ   string
-		pkg   types.Package
-		class report.ResultClass
-		fos   types.OS
-		want  string
+		name string
+		typ  string
+		pkg  types.Package
+		fos  *types.OS
+		want string
 	}{
 		{
-			name:  "maven package",
-			typ:   "jar",
-			class: report.ClassLangPkg,
+			name: "maven package",
+			typ:  "jar",
 			pkg: types.Package{
 				Name:    "org.springframework:spring-core",
 				Version: "5.3.14",
@@ -30,9 +27,8 @@ func TestNewPackageURL(t *testing.T) {
 			want: "pkg:maven/org.springframework/spring-core@5.3.14",
 		},
 		{
-			name:  "yarn package",
-			typ:   "yarn",
-			class: report.ClassLangPkg,
+			name: "yarn package",
+			typ:  "yarn",
 			pkg: types.Package{
 				Name:    "@xtuc/ieee754",
 				Version: "1.2.0",
@@ -40,9 +36,8 @@ func TestNewPackageURL(t *testing.T) {
 			want: "pkg:npm/%40xtuc/ieee754@1.2.0",
 		},
 		{
-			name:  "pypi package",
-			typ:   "pip",
-			class: report.ClassLangPkg,
+			name: "pypi package",
+			typ:  "pip",
 			pkg: types.Package{
 				Name:    "Django_test",
 				Version: "1.2.0",
@@ -50,9 +45,8 @@ func TestNewPackageURL(t *testing.T) {
 			want: "pkg:pypi/django-test@1.2.0",
 		},
 		{
-			name:  "composer package",
-			typ:   "composer",
-			class: report.ClassLangPkg,
+			name: "composer package",
+			typ:  "composer",
 			pkg: types.Package{
 				Name:    "symfony/contracts",
 				Version: "v1.0.2",
@@ -60,9 +54,8 @@ func TestNewPackageURL(t *testing.T) {
 			want: "pkg:composer/symfony/contracts@v1.0.2",
 		},
 		{
-			name:  "golang package",
-			typ:   "gomod",
-			class: report.ClassLangPkg,
+			name: "golang package",
+			typ:  "gomod",
 			pkg: types.Package{
 				Name:    "github.com/go-sql-driver/Mysql",
 				Version: "v1.5.0",
@@ -70,9 +63,8 @@ func TestNewPackageURL(t *testing.T) {
 			want: "pkg:golang/github.com/go-sql-driver/mysql@v1.5.0",
 		},
 		{
-			name:  "os package",
-			typ:   "redhat",
-			class: report.ClassOSPkg,
+			name: "os package",
+			typ:  "redhat",
 			pkg: types.Package{
 				Name:            "acl",
 				Version:         "2.2.53",
@@ -85,7 +77,7 @@ func TestNewPackageURL(t *testing.T) {
 				SrcEpoch:        0,
 				Modularitylabel: "",
 			},
-			fos: types.OS{
+			fos: &types.OS{
 				Family: "redhat",
 				Name:   "8",
 			},
@@ -95,7 +87,7 @@ func TestNewPackageURL(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			packageURL := NewPackageURL(tc.typ, tc.class, tc.fos, tc.pkg)
+			packageURL := NewPackageURL(tc.typ, tc.fos, tc.pkg)
 			assert.Equal(t, tc.want, packageURL.ToString(), tc.name)
 		})
 	}
