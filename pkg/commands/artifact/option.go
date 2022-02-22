c788676f878f949f943eef087da7f4c19e546730
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2022-02-22 22:41:25 +0600

    feat(option): warn "--list-all-pkgs" with "--format table" (#1632)
    Co-authored-by: knqyf263 <knqyf263@gmail.com>

--- pkg/commands/artifact/option.go
+++ pkg/commands/artifact/option.go
@@ -56,6 +56,12 @@ func (c *Option) Init() error {
 		return err
 	}
 
+	// "--list-all-pkgs" option is unavailable with "--format table".
+	// If user specifies "--list-all-pkgs" with "--format table", we should warn it.
+	if c.ListAllPkgs && c.Format != "json" {
+		c.Logger.Warn(`"--list-all-pkgs" cannot be used with "--format table". Try "--format json" or other formats.`)
+	}
+
 	return nil
 }
 
