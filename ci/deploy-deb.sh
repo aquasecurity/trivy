bc7150428661757e2d7bb9e9e313b59e2f9fdf68
AMF <work@afdesk.com>
2022-02-28 01:58:12 +0600

    Add conf generation for building deb packages

--- ci/deploy-deb.sh
+++ ci/deploy-deb.sh
@@ -12,6 +12,20 @@ for release in ${DEBIAN_RELEASES[@]} ${UBUNTU_RELEASES[@]}; do
   reprepro -A arm64 remove $release trivy
 done
 
+rm -r conf
+mkdir conf
+
+for release in ${UBUNTU_RELEASES[@]}; do
+  echo "Origin: knqyf263.github.io/trivy-repo/deb
+Label: github.io/knqyf263
+Codename: $release
+Architectures: i386 amd64 arm64
+Components: main
+Description: Trivy repository
+SignWith: 2E2D3567461632C84BB6CD6FE9D0A3616276FA6C
+" >> conf/distributions
+done
+
 for release in ${DEBIAN_RELEASES[@]} ${UBUNTU_RELEASES[@]}; do
   echo "Adding deb package to $release"
   reprepro includedeb $release ../../dist/*Linux-64bit.deb
