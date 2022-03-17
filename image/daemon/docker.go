e913433934152f1755c01150a7f69e52df40518e
afdesk <work@afdesk.com>
2022-03-18 00:10:13 +0600

    fix(docker): getting an image by ID or a name with tag (fanal#425)

--- image/daemon/docker.go
+++ image/daemon/docker.go
@@ -24,10 +24,17 @@ func DockerImage(ref name.Reference) (Image, func(), error) {
 		}
 	}()
 
-	imageID := ref.String()
+	// <image_name>:<tag> pattern like "alpine:3.15"
+	// or
+	// <image_name>@<digest> pattern like "alpine@sha256:21a3deaa0d32a8057914f36584b5288d2e5ecc984380bc0118285c70fa8c9300"
+	imageID := ref.Name()
 	inspect, _, err := c.ImageInspectWithRaw(context.Background(), imageID)
 	if err != nil {
-		return nil, cleanup, xerrors.Errorf("unable to inspect the image (%s): %w", imageID, err)
+		imageID = ref.String() // <image_id> pattern like `5ac716b05a9c`
+		inspect, _, err = c.ImageInspectWithRaw(context.Background(), imageID)
+		if err != nil {
+			return nil, cleanup, xerrors.Errorf("unable to inspect the image (%s): %w", imageID, err)
+		}
 	}
 
 	history, err := c.ImageHistory(context.Background(), imageID)
