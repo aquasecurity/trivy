de79d9424d65d6934e990c0521fec22da2b43db1
AMF <work@afdesk.com>
2022-03-23 13:53:04 +0600

    Mock tests

--- pkg/scanner/scan_test.go
+++ pkg/scanner/scan_test.go
@@ -194,7 +194,7 @@ func TestScanner_ScanArtifact(t *testing.T) {
 			mockArtifact.ApplyInspectExpectation(tt.inspectExpectation)
 
 			s := NewScanner(d, mockArtifact)
-			got, err := s.ScanArtifact(context.Background(), tt.args.options)
+			got, err := s.ScanArtifact(context.Background(), tt.args.options, nil)
 			if tt.wantErr != "" {
 				require.NotNil(t, err, tt.name)
 				require.Contains(t, err.Error(), tt.wantErr, tt.name)
