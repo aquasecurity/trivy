b5ed842dd9d0ba5e809f339bd7411d6cf5c74c1f
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-03-30 13:11:10 +0600

    feat(ubuntu): added support ubuntu ESM versions

--- integration/standalone_tar_test.go
+++ integration/standalone_tar_test.go
@@ -125,6 +125,14 @@ func TestTar(t *testing.T) {
 			golden: "testdata/ubuntu-1804-ignore-unfixed.json.golden",
 		},
 		{
+			name: "ubuntu 16.04-ESM",
+			testArgs: args{
+				Format: "json",
+				Input:  "testdata/fixtures/images/ubuntu-1604-esm.tar.gz",
+			},
+			golden: "testdata/ubuntu-1604-esm.json.golden",
+		},
+		{
 			name: "centos 7",
 			testArgs: args{
 				Format: "json",
