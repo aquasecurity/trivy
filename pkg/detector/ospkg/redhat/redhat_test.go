3b233edc1a318771c53f03d06baf13a061cf3a27
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-06-02 17:03:14 +0600

    test(redhat): removed repeated test

--- pkg/detector/ospkg/redhat/redhat_test.go
+++ pkg/detector/ospkg/redhat/redhat_test.go
@@ -197,51 +197,6 @@ func TestScanner_Detect(t *testing.T) {
 			},
 		},
 		{
-			name: "happy path: advisory without architecture",
-			fixtures: []string{
-				"testdata/fixtures/redhat.yaml",
-				"testdata/fixtures/cpe.yaml",
-			},
-			args: args{
-				osVer: "8.3",
-				pkgs: []ftypes.Package{
-					{
-						Name:            "php",
-						Version:         "7.2.10",
-						Release:         "1.module_el8.2.0+313+b04d0a66",
-						Arch:            "aarch64",
-						SrcName:         "php",
-						SrcVersion:      "7.2.10",
-						SrcRelease:      "1.module_el8.2.0+313+b04d0a66",
-						Modularitylabel: "php:7.2:8020020200507003613:2c7ca891",
-						Layer: ftypes.Layer{
-							DiffID: "sha256:3e968ecc016e1b9aa19023798229bf2d25c813d1bf092533f38b056aff820524",
-						},
-						BuildInfo: &ftypes.BuildInfo{
-							Nvr:  "ubi8-init-container-8.0-7",
-							Arch: "x86_64",
-						},
-					},
-				},
-			},
-			want: []types.DetectedVulnerability{
-				{
-					VulnerabilityID:  "CVE-2019-11043",
-					VendorIDs:        []string{"RHSA-2020:0322"},
-					PkgName:          "php",
-					InstalledVersion: "7.2.10-1.module_el8.2.0+313+b04d0a66",
-					FixedVersion:     "7.2.11-1.1.module+el8.0.0+4664+17bd8d65",
-					SeveritySource:   vulnerability.RedHat,
-					Vulnerability: dbTypes.Vulnerability{
-						Severity: dbTypes.SeverityCritical.String(),
-					},
-					Layer: ftypes.Layer{
-						DiffID: "sha256:3e968ecc016e1b9aa19023798229bf2d25c813d1bf092533f38b056aff820524",
-					},
-				},
-			},
-		},
-		{
 			name: "happy path: advisories have different arches",
 			fixtures: []string{
 				"testdata/fixtures/redhat.yaml",
