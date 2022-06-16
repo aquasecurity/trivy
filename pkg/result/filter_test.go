3d2fc78852703b53ee380bf561831ccb7f7a8985
maltemorgenstern <65773564+maltemorgenstern@users.noreply.github.com>
2022-06-16 12:04:19 +0200

    feat: allow expiration date for .trivyignore entries (#2332)
    Co-authored-by: Teppei Fukuda <knqyf263@gmail.com>

--- pkg/result/filter_test.go
+++ pkg/result/filter_test.go
@@ -245,6 +245,35 @@ func TestClient_Filter(t *testing.T) {
 							Severity: dbTypes.SeverityLow.String(),
 						},
 					},
+					{
+						VulnerabilityID:  "CVE-2022-0001",
+						PkgName:          "foo",
+						InstalledVersion: "1.2.3",
+						FixedVersion:     "1.2.4",
+						Vulnerability: dbTypes.Vulnerability{
+							Severity: dbTypes.SeverityLow.String(),
+						},
+					},
+					{
+						// this vulnerability is ignored
+						VulnerabilityID:  "CVE-2022-0002",
+						PkgName:          "foo",
+						InstalledVersion: "1.2.3",
+						FixedVersion:     "1.2.4",
+						Vulnerability: dbTypes.Vulnerability{
+							Severity: dbTypes.SeverityLow.String(),
+						},
+					},
+					{
+						// this vulnerability is ignored
+						VulnerabilityID:  "CVE-2022-0003",
+						PkgName:          "foo",
+						InstalledVersion: "1.2.3",
+						FixedVersion:     "1.2.4",
+						Vulnerability: dbTypes.Vulnerability{
+							Severity: dbTypes.SeverityLow.String(),
+						},
+					},
 				},
 				misconfs: []types.DetectedMisconfiguration{
 					{
@@ -263,6 +292,15 @@ func TestClient_Filter(t *testing.T) {
 			wantVulns: []types.DetectedVulnerability{
 				{
 					VulnerabilityID:  "CVE-2019-0003",
+					PkgName:          "foo",
+					InstalledVersion: "1.2.3",
+					FixedVersion:     "1.2.4",
+					Vulnerability: dbTypes.Vulnerability{
+						Severity: dbTypes.SeverityLow.String(),
+					},
+				},
+				{
+					VulnerabilityID:  "CVE-2022-0001",
 					PkgName:          "foo",
 					InstalledVersion: "1.2.3",
 					FixedVersion:     "1.2.4",
