419a4ab8f811cecf88319202659c799649ea955a
knqyf263 <knqyf263@gmail.com>
2022-06-23 12:21:38 +0300

    call dpkg license post handler before system file filter

--- pkg/fanal/types/handler.go
+++ pkg/fanal/types/handler.go
@@ -8,11 +8,14 @@ const (
 	MisconfPostHandler             HandlerType = "misconf"
 	DpkgLicensePostHandler         HandlerType = "dpkg-license"
 
+	// DpkgLicensePostHandlerPriority must be higher than SystemFileFilteringPostHandlerPriority
+	// so that copyright files will not be filtered by system-file-filter.
+	DpkgLicensePostHandlerPriority = 120
+
 	// SystemFileFilteringPostHandlerPriority should be higher than other handlers.
 	// Otherwise, other handlers need to process unnecessary files.
 	SystemFileFilteringPostHandlerPriority = 100
 
-	GoModMergePostHandlerPriority  = 50
-	MisconfPostHandlerPriority     = 50
-	DpkgLicensePostHandlerPriority = 50
+	GoModMergePostHandlerPriority = 50
+	MisconfPostHandlerPriority    = 50
 )
