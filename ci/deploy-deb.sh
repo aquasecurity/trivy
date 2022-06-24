f0715569a1d933df7c30193d430ab67c4c72b360
AMF <work@afdesk.com>
2022-06-24 19:24:01 +0600

    chore(ci): auto generation of configs for deb packages

--- ci/deploy-deb.sh
+++ ci/deploy-deb.sh
@@ -12,6 +12,23 @@ for release in ${DEBIAN_RELEASES[@]} ${UBUNTU_RELEASES[@]}; do
   reprepro -A arm64 remove $release trivy
 done
 
+rm -r conf
+mkdir conf
+
+DEBIAN_ALL_RELEASES=$(debian-distro-info --all)
+UBUNTU_ALL_RELEASES=$(ubuntu-distro-info --all)
+
+for release in ${DEBIAN_ALL_RELEASES[@]} ${UBUNTU_ALL_RELEASES[@]}; do
+  echo "Origin: aquasecurity.github.io/trivy-repo/deb
+Label: github.io/aquasecurity
+Codename: $release
+Architectures: i386 amd64 arm64
+Components: main
+Description: Trivy repository
+SignWith: 2E2D3567461632C84BB6CD6FE9D0A3616276FA6C
+" >> conf/distributions
+done
+
 for release in ${DEBIAN_RELEASES[@]} ${UBUNTU_RELEASES[@]}; do
   echo "Adding deb package to $release"
   reprepro includedeb $release ../../dist/*Linux-64bit.deb
