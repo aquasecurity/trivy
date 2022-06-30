56a7a5b928b72329b9415a3032459b4d624395b9
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-06-30 15:27:04 +0600

    test run canary build

--- pkg/fanal/analyzer/analyzer.go
+++ pkg/fanal/analyzer/analyzer.go
@@ -15,6 +15,7 @@ import (
 
 	dio "github.com/aquasecurity/go-dep-parser/pkg/io"
 	aos "github.com/aquasecurity/trivy/pkg/fanal/analyzer/os"
+
 	"github.com/aquasecurity/trivy/pkg/fanal/log"
 	"github.com/aquasecurity/trivy/pkg/fanal/types"
 )
