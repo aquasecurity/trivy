1e4fcef109bbd989b4bbc16320e3dd0fdef0658d
afdesk <work@afdesk.com>
2022-07-07 14:25:03 +0600

    test: add cache flag test (#2472)

--- pkg/flag/report_flags_test.go
+++ pkg/flag/report_flags_test.go
@@ -1,9 +1,197 @@
 package flag_test
 
 import (
+	"os"
 	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
+
+	dbTypes "github.com/aquasecurity/trivy-db/pkg/types"
+	"github.com/aquasecurity/trivy/pkg/flag"
+	"github.com/aquasecurity/trivy/pkg/log"
+	"github.com/aquasecurity/trivy/pkg/report"
 )
 
 func TestReportFlags_ToOptions(t *testing.T) {
-	// TODO
+	type fields struct {
+		format         string
+		template       string
+		dependencyTree bool
+		listAllPkgs    bool
+		ignoreUnfixed  bool
+		ignoreFile     string
+		exitCode       int
+		ignorePolicy   string
+		output         string
+		severities     string
+
+		debug bool
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		want     flag.ReportOptions
+		wantLogs []string
+	}{
+		{
+			name:   "happy default (without flags)",
+			fields: fields{},
+			want: flag.ReportOptions{
+				Output: os.Stdout,
+			},
+		},
+		{
+			name: "happy path with an unknown severity",
+			fields: fields{
+				severities: "CRITICAL,INVALID",
+			},
+			want: flag.ReportOptions{
+				Output: os.Stdout,
+				Severities: []dbTypes.Severity{
+					dbTypes.SeverityCritical,
+				},
+			},
+			wantLogs: []string{
+				"unknown severity option: unknown severity: INVALID",
+			},
+		},
+		{
+			name: "happy path with an cyclonedx",
+			fields: fields{
+				severities:  "CRITICAL",
+				format:      "cyclonedx",
+				listAllPkgs: true,
+			},
+			want: flag.ReportOptions{
+				Output:      os.Stdout,
+				Severities:  []dbTypes.Severity{dbTypes.SeverityCritical},
+				Format:      report.FormatCycloneDX,
+				ListAllPkgs: true,
+			},
+		},
+		{
+			name: "happy path with an cyclonedx option list-all-pkgs is false",
+			fields: fields{
+				severities:  "CRITICAL",
+				format:      "cyclonedx",
+				listAllPkgs: false,
+
+				debug: true,
+			},
+			wantLogs: []string{
+				`["cyclonedx" "spdx" "spdx-json" "github"] automatically enables '--list-all-pkgs'.`,
+				`Severities: ["CRITICAL"]`,
+			},
+			want: flag.ReportOptions{
+				Output: os.Stdout,
+				Severities: []dbTypes.Severity{
+					dbTypes.SeverityCritical,
+				},
+				Format:      report.FormatCycloneDX,
+				ListAllPkgs: true,
+			},
+		},
+		{
+			name: "invalid option combination: --template enabled without --format",
+			fields: fields{
+				template:   "@contrib/gitlab.tpl",
+				severities: "LOW",
+			},
+			wantLogs: []string{
+				"'--template' is ignored because '--format template' is not specified. Use '--template' option with '--format template' option.",
+			},
+			want: flag.ReportOptions{
+				Output:     os.Stdout,
+				Severities: []dbTypes.Severity{dbTypes.SeverityLow},
+				Template:   "@contrib/gitlab.tpl",
+			},
+		},
+		{
+			name: "invalid option combination: --template and --format json",
+			fields: fields{
+				format:     "json",
+				template:   "@contrib/gitlab.tpl",
+				severities: "LOW",
+			},
+			wantLogs: []string{
+				"'--template' is ignored because '--format json' is specified. Use '--template' option with '--format template' option.",
+			},
+			want: flag.ReportOptions{
+				Output:     os.Stdout,
+				Format:     "json",
+				Severities: []dbTypes.Severity{dbTypes.SeverityLow},
+				Template:   "@contrib/gitlab.tpl",
+			},
+		},
+		{
+			name: "invalid option combination: --format template without --template",
+			fields: fields{
+				format:     "template",
+				severities: "LOW",
+			},
+			wantLogs: []string{
+				"'--format template' is ignored because '--template' is not specified. Specify '--template' option when you use '--format template'.",
+			},
+			want: flag.ReportOptions{
+				Output:     os.Stdout,
+				Format:     "template",
+				Severities: []dbTypes.Severity{dbTypes.SeverityLow},
+			},
+		},
+		{
+			name: "invalid option combination: --list-all-pkgs with --format table",
+			fields: fields{
+				format:      "table",
+				severities:  "LOW",
+				listAllPkgs: true,
+			},
+			wantLogs: []string{
+				`"--list-all-pkgs" cannot be used with "--format table". Try "--format json" or other formats.`,
+			},
+			want: flag.ReportOptions{
+				Format:      "table",
+				Output:      os.Stdout,
+				Severities:  []dbTypes.Severity{dbTypes.SeverityLow},
+				ListAllPkgs: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			level := zap.WarnLevel
+			if tt.fields.debug {
+				level = zap.DebugLevel
+			}
+			core, obs := observer.New(level)
+			log.Logger = zap.New(core).Sugar()
+
+			viper.Set(flag.FormatFlag, tt.fields.format)
+			viper.Set(flag.TemplateFlag, tt.fields.template)
+			viper.Set(flag.DependencyTreeFlag, tt.fields.dependencyTree)
+			viper.Set(flag.ListAllPkgsFlag, tt.fields.listAllPkgs)
+			viper.Set(flag.IgnoreFileFlag, tt.fields.ignoreFile)
+			viper.Set(flag.IgnoreUnfixedFlag, tt.fields.ignoreUnfixed)
+			viper.Set(flag.ExitCodeFlag, tt.fields.exitCode)
+			viper.Set(flag.IgnorePolicyFlag, tt.fields.ignorePolicy)
+			viper.Set(flag.OutputFlag, tt.fields.output)
+			viper.Set(flag.SeverityFlag, tt.fields.severities)
+
+			// Assert options
+			f := &flag.ReportFlags{}
+
+			got, err := f.ToOptions(os.Stdout)
+			assert.NoError(t, err)
+			assert.Equalf(t, tt.want, got, "ToOptions()")
+
+			// Assert log messages
+			var gotMessages []string
+			for _, entry := range obs.AllUntimed() {
+				gotMessages = append(gotMessages, entry.Message)
+			}
+			assert.Equal(t, tt.wantLogs, gotMessages, tt.name)
+		})
+	}
 }
--- pkg/flag/scan_flags_test.go
+++ pkg/flag/scan_flags_test.go
@@ -2,8 +2,168 @@ package flag_test
 
 import (
 	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
+
+	"github.com/aquasecurity/trivy/pkg/flag"
+	"github.com/aquasecurity/trivy/pkg/log"
+	"github.com/aquasecurity/trivy/pkg/types"
 )
 
 func TestScanFlags_ToOptions(t *testing.T) {
-	// TODO
+	type fields struct {
+		skipDirs       []string
+		skipFiles      []string
+		offlineScan    bool
+		vulnType       string
+		securityChecks string
+	}
+	tests := []struct {
+		name     string
+		args     []string
+		fields   fields
+		want     flag.ScanOptions
+		wantLogs []string
+	}{
+		{
+			name:   "happy path",
+			args:   []string{"alpine:latest"},
+			fields: fields{},
+			want: flag.ScanOptions{
+				Target: "alpine:latest",
+			},
+		},
+		{
+			name: "happy path for OS vulnerabilities",
+			args: []string{"alpine:latest"},
+			fields: fields{
+				vulnType:       "os",
+				securityChecks: "vuln",
+			},
+			want: flag.ScanOptions{
+				Target:         "alpine:latest",
+				VulnType:       []string{types.VulnTypeOS},
+				SecurityChecks: []string{types.SecurityCheckVulnerability},
+			},
+		},
+		{
+			name: "happy path for library vulnerabilities",
+			args: []string{"alpine:latest"},
+			fields: fields{
+				vulnType:       "library",
+				securityChecks: "vuln",
+			},
+			want: flag.ScanOptions{
+				Target:         "alpine:latest",
+				VulnType:       []string{types.VulnTypeLibrary},
+				SecurityChecks: []string{types.SecurityCheckVulnerability},
+			},
+		},
+		{
+			name: "happy path for configs",
+			args: []string{"alpine:latest"},
+			fields: fields{
+				securityChecks: "config",
+			},
+			want: flag.ScanOptions{
+				Target:         "alpine:latest",
+				SecurityChecks: []string{types.SecurityCheckConfig},
+			},
+		},
+		{
+			name: "with wrong security check",
+			fields: fields{
+				securityChecks: "vuln,WRONG-CHECK",
+			},
+			want: flag.ScanOptions{
+				SecurityChecks: []string{types.SecurityCheckVulnerability},
+			},
+			wantLogs: []string{
+				`unknown security check: WRONG-CHECK`,
+			},
+		},
+		{
+			name: "with wrong vuln type",
+			fields: fields{
+				vulnType: "os,nonevuln",
+			},
+			want: flag.ScanOptions{
+				VulnType: []string{types.VulnTypeOS},
+			},
+			wantLogs: []string{
+				`unknown vulnerability type: nonevuln`,
+			},
+		},
+		{
+			name:   "without target (args)",
+			args:   []string{},
+			fields: fields{},
+			want:   flag.ScanOptions{},
+		},
+		{
+			name:   "with two or more targets (args)",
+			args:   []string{"alpine:latest", "nginx:latest"},
+			fields: fields{},
+			want:   flag.ScanOptions{},
+		},
+		{
+			name: "skip two files",
+			fields: fields{
+				skipFiles: []string{"file1", "file2"},
+			},
+			want: flag.ScanOptions{
+				SkipFiles: []string{"file1", "file2"},
+			},
+		},
+		{
+			name: "skip two folders",
+			fields: fields{
+				skipDirs: []string{"dir1", "dir2"},
+			},
+			want: flag.ScanOptions{
+				SkipDirs: []string{"dir1", "dir2"},
+			},
+		},
+		{
+			name: "offline scan",
+			fields: fields{
+				offlineScan: true,
+			},
+			want: flag.ScanOptions{
+				OfflineScan: true,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			level := zap.WarnLevel
+
+			core, obs := observer.New(level)
+			log.Logger = zap.New(core).Sugar()
+
+			viper.Set(flag.SkipDirsFlag, tt.fields.skipDirs)
+			viper.Set(flag.SkipFilesFlag, tt.fields.skipFiles)
+			viper.Set(flag.OfflineScanFlag, tt.fields.offlineScan)
+			viper.Set(flag.VulnTypeFlag, tt.fields.vulnType)
+			viper.Set(flag.SecurityChecksFlag, tt.fields.securityChecks)
+
+			// Assert options
+			f := &flag.ScanFlags{}
+
+			got := f.ToOptions(tt.args)
+			assert.Equalf(t, tt.want, got, "ToOptions()")
+
+			// Assert log messages
+			var gotMessages []string
+			for _, entry := range obs.AllUntimed() {
+				gotMessages = append(gotMessages, entry.Message)
+			}
+			assert.Equal(t, tt.wantLogs, gotMessages, tt.name)
+		})
+
+	}
 }
