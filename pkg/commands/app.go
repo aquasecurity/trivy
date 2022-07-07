df8056f2e505c593eb649953a2ff8755156d179e
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-07-07 18:45:47 +0600

    refactor

--- pkg/commands/app.go
+++ pkg/commands/app.go
@@ -698,7 +698,7 @@ func showVersion(cacheDir, outputFormat, version string, outputWriter io.Writer)
 
 func validateArgs(cmd *cobra.Command, args []string) error {
 	// '--clear-cache', '--download-db-only' and '--reset' don't conduct the scan
-	if viper.GetBool(flag.ClearCacheFlag.ConfigName) || viper.GetBool(flag.DownloadDBOnlyFlag) || viper.GetBool(flag.ResetFlag) {
+	if viper.GetBool(flag.ClearCacheFlag.ConfigName) || viper.GetBool(flag.DownloadDBOnlyFlag.ConfigName) || viper.GetBool(flag.ResetFlag.ConfigName) {
 		return nil
 	}
 
