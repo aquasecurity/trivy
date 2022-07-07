9d8b1ce10f47049e72a8f09cceff1c2edc220c9a
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-07-07 18:08:58 +0600

    refactor

--- pkg/flag/cache_flags.go
+++ pkg/flag/cache_flags.go
@@ -15,9 +15,9 @@ import (
 //   clear: true
 //   backend: "redis://localhost:6379"
 //   redis:
-//    ca: ca.pem
+//    ca: ca-cert.pem
 //    cert: cert.pem
-//    key: key
+//    key: key.pem
 var (
 	ClearCacheFlag = Flag{
 		Name:       "clear-cache",
