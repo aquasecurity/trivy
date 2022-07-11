602e6b9549aba1ca150dfe36054ed0a92badfe2c
knqyf263 <knqyf263@gmail.com>
2022-07-11 14:47:13 +0300

    leave todos

--- pkg/fanal/licensing/scanner.go
+++ pkg/fanal/licensing/scanner.go
@@ -1,17 +1,13 @@
 package licensing
 
 import (
-	"io/fs"
-
-	"github.com/aquasecurity/trivy/pkg/fanal/licensing/classification"
-	"github.com/aquasecurity/trivy/pkg/fanal/log"
 	"github.com/aquasecurity/trivy/pkg/fanal/types"
 
 	"golang.org/x/xerrors"
 )
 
 type Scanner struct {
-	classifier *classification.Classifier
+	classifier *Classifier
 }
 
 type ScanArgs struct {
@@ -20,7 +16,6 @@ type ScanArgs struct {
 }
 
 func NewScanner(ignoredLicenses []string) (Scanner, error) {
-
 	classifier, err := NewClassifier()
 	if err != nil {
 		return Scanner{}, xerrors.Errorf("classifier could not be created: %w", err)
@@ -28,6 +23,8 @@ func NewScanner(ignoredLicenses []string) (Scanner, error) {
 	return Scanner{classifier: classifier}, nil
 }
 
-func (s Scanner) Scan(scanArgs ScanArgs) types.LicenseFile {
+func (s Scanner) Scan() types.LicenseFile {
+	// TODO: license type detection
+	// https://github.com/google/licenseclassifier/blob/7c62d6fe8d3aa2f39c4affb58c9781d9dc951a2d/license_type.go#L377-L394
 	return types.LicenseFile{}
 }
