99cfb6ac1039e14be8566e876e51bdd6c6955c39
knqyf263 <knqyf263@gmail.com>
2022-07-11 14:38:30 +0300

    remove ScanFS

--- pkg/fanal/licensing/classifier_test.go
+++ pkg/fanal/licensing/classifier_test.go
@@ -1,143 +1,93 @@
-package licensing
+package licensing_test
 
 import (
-	"fmt"
-	"io/fs"
 	"os"
-	"path/filepath"
-	"sort"
 	"testing"
 
-	"github.com/aquasecurity/trivy/pkg/fanal/types"
-	"github.com/liamg/memoryfs"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/trivy/pkg/fanal/licensing"
+	"github.com/aquasecurity/trivy/pkg/fanal/types"
 )
 
-func Test_LicenseScanning(t *testing.T) {
-
+func TestClassifier_Classify(t *testing.T) {
 	tests := []struct {
-		name             string
-		filePath         string
-		expectLicense    bool
-		packageName      string
-		expectedFindings []types.LicenseFinding
+		name     string
+		filePath string
+		want     types.LicenseFile
+		wantErr  assert.ErrorAssertionFunc
 	}{
 		{
-			name:          "C file with AGPL-3.0",
-			filePath:      "testdata/licensed.c",
-			expectLicense: true,
-			expectedFindings: []types.LicenseFinding{
-				{
-					License:                     "AGPL-3.0",
-					GoogleLicenseClassification: "forbidden",
-					Confidence:                  0.98,
+			name:     "C file with AGPL-3.0",
+			filePath: "testdata/licensed.c",
+			want: types.LicenseFile{
+				FilePath: "testdata/licensed.c",
+				Findings: []types.LicenseFinding{
+					{
+						License:    "AGPL-3.0",
+						Confidence: 0.98,
+					},
 				},
 			},
 		},
 		{
-			name:          "C file with no license",
-			filePath:      "testdata/unlicensed.c",
-			expectLicense: false,
+			name:     "C file with no license",
+			filePath: "testdata/unlicensed.c",
+			want: types.LicenseFile{
+				FilePath: "testdata/unlicensed.c",
+			},
 		},
 		{
-			name:          "Creative commons License file",
-			filePath:      "testdata/LICENSE_creativecommons",
-			expectLicense: true,
-			expectedFindings: []types.LicenseFinding{
-				{
-					License:                     "Commons-Clause",
-					GoogleLicenseClassification: "forbidden",
-					Confidence:                  0.98,
+			name:     "Creative commons License file",
+			filePath: "testdata/LICENSE_creativecommons",
+			want: types.LicenseFile{
+				FilePath: "testdata/LICENSE_creativecommons",
+				Findings: []types.LicenseFinding{
+					{
+						License:    "Commons-Clause",
+						Confidence: 0.98,
+					},
 				},
 			},
 		},
 		{
-			name:          "Apache-2.0 CSS File",
-			filePath:      "testdata/styles.css",
-			expectLicense: true,
-			expectedFindings: []types.LicenseFinding{
-				{
-					License:                     "Apache-2.0",
-					GoogleLicenseClassification: "notice",
-					Confidence:                  1,
+			name:     "Apache-2.0 CSS File",
+			filePath: "testdata/styles.css",
+			want: types.LicenseFile{
+				FilePath: "testdata/styles.css",
+				Findings: []types.LicenseFinding{
+					{
+						License:    "Apache-2.0",
+						Confidence: 1,
+					},
 				},
 			},
 		},
 		{
-			name:          "Package folder identifies package",
-			filePath:      "testdata/callsites",
-			expectLicense: true,
-			packageName:   "callsites",
-			expectedFindings: []types.LicenseFinding{
-				{
-					License:                     "MIT",
-					GoogleLicenseClassification: "notice",
-					Confidence:                  0.98,
-				},
-			},
-		},
-		{
-			name:          "Apache 2 License file",
-			filePath:      "testdata/LICENSE_apache2",
-			expectLicense: true,
-			expectedFindings: []types.LicenseFinding{
-				{
-					License:                     "Apache-2.0",
-					GoogleLicenseClassification: "notice",
-					Confidence:                  1,
+			name:     "Apache 2 License file",
+			filePath: "testdata/LICENSE_apache2",
+			want: types.LicenseFile{
+				FilePath: "testdata/styles.css",
+				Findings: []types.LicenseFinding{
+					{
+						License:    "Apache-2.0",
+						Confidence: 1,
+					},
 				},
 			},
 		},
 	}
-
 	for _, tt := range tests {
-		t.Run(fmt.Sprintf("%#v", tt.name), func(t *testing.T) {
-
-			scanner, err := NewScanner([]string{})
+		t.Run(tt.name, func(t *testing.T) {
+			c, err := licensing.NewClassifier()
 			require.NoError(t, err)
 
-			var testFS fs.FS
-			f, err := os.Stat(tt.filePath)
-			require.NoError(t, err)
-			if f.IsDir() {
-				testFS = os.DirFS(tt.filePath)
-			} else {
-				memfs := memoryfs.New()
-				if filepath.Dir(tt.filePath) != "." {
-					err = memfs.MkdirAll(filepath.Dir(tt.filePath), os.ModePerm)
-					require.NoError(t, err)
-				}
-				content, err := os.ReadFile(tt.filePath)
-				require.NoError(t, err)
-				err = memfs.WriteFile(tt.filePath, content, os.ModePerm)
-				testFS = memfs
-			}
-
-			licenses, err := scanner.ScanFS(testFS)
+			contents, err := os.ReadFile(tt.filePath)
 			require.NoError(t, err)
 
-			if tt.expectLicense {
-				assert.NotNil(t, licenses)
-				require.GreaterOrEqual(t, len(licenses), 1)
-				license := licenses[0]
-				assert.Len(t, license.Findings, len(tt.expectedFindings))
-
-				// sort findings for consistent checking
-				sort.Slice(license.Findings, func(i, j int) bool {
-					return license.Findings[i].License < license.Findings[j].License
-				})
-
-				for i, f := range tt.expectedFindings {
-					lf := license.Findings[i]
-					assert.Equal(t, f.License, lf.License)
-					assert.Equal(t, f.GoogleLicenseClassification, lf.GoogleLicenseClassification)
-					assert.Greater(t, lf.Confidence, 0.8)
-				}
-			} else {
-				assert.Len(t, licenses, 0)
-			}
+			got := c.Classify(tt.filePath, contents)
+			assert.Equal(t, tt.want, got)
 		})
-
 	}
 }
--- pkg/fanal/licensing/scanner.go
+++ pkg/fanal/licensing/scanner.go
@@ -21,54 +21,13 @@ type ScanArgs struct {
 
 func NewScanner(ignoredLicenses []string) (Scanner, error) {
 
-	classifier, err := classification.NewClassifier(ignoredLicenses)
+	classifier, err := NewClassifier()
 	if err != nil {
 		return Scanner{}, xerrors.Errorf("classifier could not be created: %w", err)
 	}
 	return Scanner{classifier: classifier}, nil
 }
 
-func (s Scanner) ScanFS(filesystem fs.FS) ([]types.LicenseFile, error) {
-
-	var licenseFiles []types.LicenseFile
-
-	err := fs.WalkDir(filesystem, ".", func(path string, info fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		content, err := fs.ReadFile(filesystem, path)
-		if err != nil {
-			return err
-		}
-		licenseFile, err := s.classifier.Classify(path, content)
-		if err != nil {
-			return err
-		}
-
-		if len(licenseFile.Findings) > 0 {
-			licenseFiles = append(licenseFiles, licenseFile)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return licenseFiles, nil
-}
-
 func (s Scanner) Scan(scanArgs ScanArgs) types.LicenseFile {
-
-	license, err := s.classifier.Classify(scanArgs.FilePath, scanArgs.Content)
-	if err != nil {
-		log.Logger.Debugf("Name scan failed while scanning %s: %w", scanArgs.FilePath, err)
-	}
-
-	return license
+	return types.LicenseFile{}
 }
