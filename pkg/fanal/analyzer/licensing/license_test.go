4aa76beae96edaee26dee42eaa30044b027a108f
knqyf263 <knqyf263@gmail.com>
2022-07-11 15:31:45 +0300

    test: simplify cases

--- pkg/fanal/analyzer/licensing/license_test.go
+++ pkg/fanal/analyzer/licensing/license_test.go
@@ -5,78 +5,68 @@ import (
 	"os"
 	"testing"
 
-	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
+	"github.com/aquasecurity/trivy/pkg/fanal/types"
 )
 
-func Test_AnalyzeLicenses(t *testing.T) {
+func Test_licenseAnalyzer_Analyze(t *testing.T) {
 	tests := []struct {
-		name                       string
-		filePath                   string
-		expectedIdentifiedLicenses int
-		expectedLicense            string
+		name     string
+		filePath string
+		want     *analyzer.AnalysisResult
 	}{
 		{
-			name:                       "Licensed C file",
-			filePath:                   "testdata/licensed.c",
-			expectedLicense:            "AGPL-3.0",
-			expectedIdentifiedLicenses: 1,
+			name:     "Licensed C file",
+			filePath: "testdata/licensed.c",
+			want: &analyzer.AnalysisResult{
+				Licenses: []types.LicenseFile{
+					{
+						Type:     types.LicenseTypeHeader,
+						FilePath: "/testdata/licensed.c",
+						Findings: []types.LicenseFinding{
+							{
+								License:     "AGPL-3.0",
+								Confidence:  1,
+								LicenseLink: "https://spdx.org/licenses/AGPL-3.0.html",
+							},
+						},
+					},
+				},
+			},
 		},
 		{
-			name:                       "Another Licensed C file",
-			filePath:                   "testdata/another_licensed.c",
-			expectedLicense:            "BSL-1.0",
-			expectedIdentifiedLicenses: 1,
-		},
-		{
-			name:                       "CSS File",
-			filePath:                   "testdata/styles.css",
-			expectedLicense:            "Apache-2.0",
-			expectedIdentifiedLicenses: 1,
-		},
-		{
-			name:                       "Unlicensed C file",
-			filePath:                   "testdata/unlicensed.c",
-			expectedLicense:            "",
-			expectedIdentifiedLicenses: 0,
-		},
-		{
-			name:                       "Non human readable binary file",
-			filePath:                   "testdata/binaryfile",
-			expectedLicense:            "",
-			expectedIdentifiedLicenses: 0,
+			name:     "Non human readable binary file",
+			filePath: "testdata/binaryfile",
+			want:     nil,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			a, err := newLicenseScanner(ScannerOption{})
+			f, err := os.Open(tt.filePath)
 			require.NoError(t, err)
-			content, err := os.Open(tt.filePath)
-			require.NoError(t, err)
-			fi, err := content.Stat()
+			defer f.Close()
+
+			fi, err := f.Stat()
 			require.NoError(t, err)
 
+			a := licenseAnalyzer{}
 			got, err := a.Analyze(context.TODO(), analyzer.AnalysisInput{
 				FilePath: tt.filePath,
-				Content:  content,
+				Content:  f,
 				Info:     fi,
 			})
 			require.NoError(t, err)
-
-			if tt.expectedIdentifiedLicenses > 0 {
-				assert.Len(t, got.Licenses, tt.expectedIdentifiedLicenses)
-				assert.Equal(t, tt.expectedLicense, got.Licenses[0].Findings[0].License)
-			} else {
-				assert.Nil(t, got)
-			}
+			assert.Equal(t, tt.want, got)
 		})
 	}
 
 }
 
-func Test_LicenseAnalysisRequired(t *testing.T) {
+func Test_licenseAnalyzer_Required(t *testing.T) {
 	tests := []struct {
 		name     string
 		filePath string
@@ -97,21 +87,11 @@ func Test_LicenseAnalysisRequired(t *testing.T) {
 			filePath: "testdata/binaryfile",
 			want:     false,
 		},
-		{
-			name:     "Image file",
-			filePath: "testdata/picture.png",
-			want:     false,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			a, err := newLicenseScanner(ScannerOption{})
-			require.NoError(t, err)
-
-			fi, err := os.Stat(tt.filePath)
-			require.NoError(t, err)
-
-			got := a.Required(tt.filePath, fi)
+			a := licenseAnalyzer{}
+			got := a.Required(tt.filePath, nil)
 			assert.Equal(t, tt.want, got)
 		})
 	}
