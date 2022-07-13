ec39cf6cc39e04929c3bcbabf2ec98354c13f14b
knqyf263 <knqyf263@gmail.com>
2022-07-13 09:52:20 +0300

    test(table): fix signature

--- pkg/report/table/misconfig_test.go
+++ pkg/report/table/misconfig_test.go
@@ -4,41 +4,45 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/aquasecurity/trivy/pkg/report/table"
-
-	ftypes "github.com/aquasecurity/trivy/pkg/fanal/types"
-
-	"github.com/aquasecurity/trivy/pkg/types"
 	"github.com/stretchr/testify/assert"
+
+	dbTypes "github.com/aquasecurity/trivy-db/pkg/types"
+	ftypes "github.com/aquasecurity/trivy/pkg/fanal/types"
+	"github.com/aquasecurity/trivy/pkg/report/table"
+	"github.com/aquasecurity/trivy/pkg/types"
 )
 
 func TestMisconfigRenderer(t *testing.T) {
 
 	tests := []struct {
 		name               string
-		input              []types.DetectedMisconfiguration
-		want               string
+		input              types.Result
 		includeNonFailures bool
+		want               string
 	}{
 		{
 			name:               "no results",
-			input:              nil,
-			want:               "",
+			input:              types.Result{},
 			includeNonFailures: false,
+			want:               "",
 		},
 		{
 			name: "single result",
-			input: []types.DetectedMisconfiguration{
-				{
-					ID:          "AVD-XYZ-0123",
-					Title:       "Config file is bad",
-					Description: "Your config file is not good.",
-					Message:     "Oh no, a bad config.",
-					Severity:    "HIGH",
-					PrimaryURL:  "https://google.com/search?q=bad%20config",
-					Status:      "FAIL",
+			input: types.Result{
+				Target: "my-file",
+				Misconfigurations: []types.DetectedMisconfiguration{
+					{
+						ID:          "AVD-XYZ-0123",
+						Title:       "Config file is bad",
+						Description: "Your config file is not good.",
+						Message:     "Oh no, a bad config.",
+						Severity:    "HIGH",
+						PrimaryURL:  "https://google.com/search?q=bad%20config",
+						Status:      "FAIL",
+					},
 				},
 			},
+			includeNonFailures: false,
 			want: `HIGH: Oh no, a bad config.
 ════════════════════════════════════════
 Your config file is not good.
@@ -48,46 +52,49 @@ See https://google.com/search?q=bad%20config
 
 
 `,
-			includeNonFailures: false,
 		},
 		{
 			name: "single result with code",
-			input: []types.DetectedMisconfiguration{
-				{
-					ID:          "AVD-XYZ-0123",
-					Title:       "Config file is bad",
-					Description: "Your config file is not good.",
-					Message:     "Oh no, a bad config.",
-					Severity:    "HIGH",
-					PrimaryURL:  "https://google.com/search?q=bad%20config",
-					Status:      "FAIL",
-					CauseMetadata: ftypes.CauseMetadata{
-						Resource:  "",
-						Provider:  "",
-						Service:   "",
-						StartLine: 0,
-						EndLine:   0,
-						Code: ftypes.Code{
-							Lines: []ftypes.Line{
-								{
-									Number: 1,
-								},
-								{
-									Number:      2,
-									Content:     "bad: true",
-									Highlighted: "\x1b[37mbad:\x1b[0m true",
-									IsCause:     true,
-									FirstCause:  true,
-									LastCause:   true,
-								},
-								{
-									Number: 3,
+			input: types.Result{
+				Target: "my-file",
+				Misconfigurations: []types.DetectedMisconfiguration{
+					{
+						ID:          "AVD-XYZ-0123",
+						Title:       "Config file is bad",
+						Description: "Your config file is not good.",
+						Message:     "Oh no, a bad config.",
+						Severity:    "HIGH",
+						PrimaryURL:  "https://google.com/search?q=bad%20config",
+						Status:      "FAIL",
+						CauseMetadata: ftypes.CauseMetadata{
+							Resource:  "",
+							Provider:  "",
+							Service:   "",
+							StartLine: 0,
+							EndLine:   0,
+							Code: ftypes.Code{
+								Lines: []ftypes.Line{
+									{
+										Number: 1,
+									},
+									{
+										Number:      2,
+										Content:     "bad: true",
+										Highlighted: "\x1b[37mbad:\x1b[0m true",
+										IsCause:     true,
+										FirstCause:  true,
+										LastCause:   true,
+									},
+									{
+										Number: 3,
+									},
 								},
 							},
 						},
 					},
 				},
 			},
+			includeNonFailures: false,
 			want: `HIGH: Oh no, a bad config.
 ════════════════════════════════════════
 Your config file is not good.
@@ -103,52 +110,55 @@ See https://google.com/search?q=bad%20config
 
 
 `,
-			includeNonFailures: false,
 		},
 		{
 			name: "multiple results",
-			input: []types.DetectedMisconfiguration{
-				{
-					ID:          "AVD-XYZ-0123",
-					Title:       "Config file is bad",
-					Description: "Your config file is not good.",
-					Message:     "Oh no, a bad config.",
-					Severity:    "HIGH",
-					PrimaryURL:  "https://google.com/search?q=bad%20config",
-					Status:      "FAIL",
-					CauseMetadata: ftypes.CauseMetadata{
-						StartLine: 2,
-						EndLine:   2,
-						Code: ftypes.Code{
-							Lines: []ftypes.Line{
-								{
-									Number: 1,
-								},
-								{
-									Number:      2,
-									Content:     "bad: true",
-									Highlighted: "\x1b[37mbad:\x1b[0m true",
-									IsCause:     true,
-									FirstCause:  true,
-									LastCause:   true,
-								},
-								{
-									Number: 3,
+			input: types.Result{
+				Target: "my-file",
+				Misconfigurations: []types.DetectedMisconfiguration{
+					{
+						ID:          "AVD-XYZ-0123",
+						Title:       "Config file is bad",
+						Description: "Your config file is not good.",
+						Message:     "Oh no, a bad config.",
+						Severity:    "HIGH",
+						PrimaryURL:  "https://google.com/search?q=bad%20config",
+						Status:      "FAIL",
+						CauseMetadata: ftypes.CauseMetadata{
+							StartLine: 2,
+							EndLine:   2,
+							Code: ftypes.Code{
+								Lines: []ftypes.Line{
+									{
+										Number: 1,
+									},
+									{
+										Number:      2,
+										Content:     "bad: true",
+										Highlighted: "\x1b[37mbad:\x1b[0m true",
+										IsCause:     true,
+										FirstCause:  true,
+										LastCause:   true,
+									},
+									{
+										Number: 3,
+									},
 								},
 							},
 						},
 					},
-				},
-				{
-					ID:          "AVD-XYZ-0456",
-					Title:       "Config file is bad again",
-					Description: "Your config file is still not good.",
-					Message:     "Oh no, a bad config AGAIN.",
-					Severity:    "MEDIUM",
-					PrimaryURL:  "https://google.com/search?q=bad%20config",
-					Status:      "PASS",
+					{
+						ID:          "AVD-XYZ-0456",
+						Title:       "Config file is bad again",
+						Description: "Your config file is still not good.",
+						Message:     "Oh no, a bad config AGAIN.",
+						Severity:    "MEDIUM",
+						PrimaryURL:  "https://google.com/search?q=bad%20config",
+						Status:      "PASS",
+					},
 				},
 			},
+			includeNonFailures: true,
 			want: `FAIL: HIGH: Oh no, a bad config.
 ════════════════════════════════════════
 Your config file is not good.
@@ -172,13 +182,14 @@ See https://google.com/search?q=bad%20config
 
 
 `,
-			includeNonFailures: true,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			renderer := table.NewMisconfigRenderer("my-file", test.input, test.includeNonFailures, false)
+			severities := []dbTypes.Severity{dbTypes.SeverityLow, dbTypes.SeverityMedium, dbTypes.SeverityHigh,
+				dbTypes.SeverityCritical}
+			renderer := table.NewMisconfigRenderer(test.input, severities, false, test.includeNonFailures, false)
 			assert.Equal(t, test.want, strings.ReplaceAll(renderer.Render(), "\r\n", "\n"))
 		})
 	}
