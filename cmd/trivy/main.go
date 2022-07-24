0ac26dedc24d4eb99ebd140ccf98cbd7585f5805
knqyf263 <knqyf263@gmail.com>
2022-07-24 10:35:38 +0300

    fix: allow subcommands with TRIVY_RUN_AS_PLUGIN

--- cmd/trivy/main.go
+++ cmd/trivy/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+
 	"github.com/aquasecurity/trivy/pkg/commands"
 	"github.com/aquasecurity/trivy/pkg/log"
+	"github.com/aquasecurity/trivy/pkg/plugin"
 )
 
 var (
@@ -10,6 +15,16 @@ var (
 )
 
 func main() {
+	// Trivy behaves as the specified plugin.
+	if runAsPlugin := os.Getenv("TRIVY_RUN_AS_PLUGIN"); runAsPlugin != "" {
+		if !plugin.IsPredefined(runAsPlugin) {
+			log.Fatal(fmt.Errorf("unknown plugin: %s", runAsPlugin))
+		}
+		if err := plugin.RunWithArgs(context.Background(), runAsPlugin, os.Args); err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	app := commands.NewApp(version)
 	if err := app.Execute(); err != nil {
 		log.Fatal(err)
