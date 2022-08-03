3a10497a6fcdd197af895e1ba83086ee446685b0
MaineK00n <mainek00n.1229@gmail.com>
2022-08-03 16:40:07 +0900

    feat(alma): set AlmaLinux 9 EOL (#2653)

--- pkg/detector/ospkg/alma/alma.go
+++ pkg/detector/ospkg/alma/alma.go
@@ -18,8 +18,9 @@ import (
 var (
 	eolDates = map[string]time.Time{
 		// Source:
-		// https://wiki.almalinux.org/FAQ.html#how-long-will-cloudlinux-support-almalinux
-		"8": time.Date(2029, 12, 31, 23, 59, 59, 0, time.UTC),
+		// https://endoflife.date/almalinux
+		"8": time.Date(2029, 3, 1, 23, 59, 59, 0, time.UTC),
+		"9": time.Date(2032, 5, 31, 23, 59, 59, 0, time.UTC),
 	}
 )
 
