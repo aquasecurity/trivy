813759c02e177cbc72c2050055c1981b30a3a606
knqyf263 <knqyf263@gmail.com>
2022-09-15 13:23:43 +0300

    refactor: add a new file

--- pkg/fanal/artifact/image/image.go
+++ pkg/fanal/artifact/image/image.go
@@ -3,7 +3,7 @@ package image
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io"
 	"os"
 	"reflect"
@@ -17,14 +17,11 @@ import (
 
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
 	"github.com/aquasecurity/trivy/pkg/fanal/artifact"
-	"github.com/aquasecurity/trivy/pkg/fanal/artifact/sbom"
 	"github.com/aquasecurity/trivy/pkg/fanal/cache"
 	"github.com/aquasecurity/trivy/pkg/fanal/handler"
 	"github.com/aquasecurity/trivy/pkg/fanal/log"
 	"github.com/aquasecurity/trivy/pkg/fanal/types"
 	"github.com/aquasecurity/trivy/pkg/fanal/walker"
-	"github.com/aquasecurity/trivy/pkg/rekor"
-	ttypes "github.com/aquasecurity/trivy/pkg/types"
 )
 
 const (
@@ -69,19 +66,6 @@ func NewArtifact(img types.Image, c cache.ArtifactCache, opt artifact.Option) (a
 	}, nil
 }
 
-func repoDigest(img types.Image) (string, error) {
-	repoNameFull := img.Name()
-	repoName, _, _ := strings.Cut(repoNameFull, ":")
-
-	for _, rd := range img.RepoDigests() {
-		if name, d, found := strings.Cut(rd, "@"); found && name == repoName {
-			return d, nil
-		}
-	}
-	return "", fmt.Errorf("repo digest not found")
-
-}
-
 func (a Artifact) Inspect(ctx context.Context) (types.ArtifactReference, error) {
 	imageID, err := a.image.ID()
 	if err != nil {
@@ -102,15 +86,13 @@ func (a Artifact) Inspect(ctx context.Context) (types.ArtifactReference, error) 
 	log.Logger.Debugf("Image ID: %s", imageID)
 	log.Logger.Debugf("Diff IDs: %v", diffIDs)
 
-	for _, sbomFrom := range a.artifactOption.SbomFroms {
-		switch sbomFrom {
-		case ttypes.SbomFromTypeRekor:
-			results, err := a.inspectSbomAttestation(ctx)
-			if err == nil {
-				return results, nil
-			}
-			log.Logger.Debugf("Failed to inspect SBOM Attestation from rekor")
-		}
+	// Try fetching a remote SBOM document
+	if res, err := a.fetchRemoteSBOM(ctx); err == nil {
+		// Found SBOM
+		return res, nil
+	} else if !errors.Is(err, errSBOMNotFound) && !errors.Is(err, errNoRepoDigest) {
+		// Fail on unexpected error, otherwise it falls into the usual scanning.
+		return types.ArtifactReference{}, xerrors.Errorf("remote SBOM fetching error: %w", err)
 	}
 
 	// Try to detect base layers.
@@ -431,65 +413,4 @@ func (a Artifact) guessBaseLayers(diffIDs []string, configFile *v1.ConfigFile) [
 		diffIDIndex++
 	}
 	return baseDiffIDs
-}
-
-func (a Artifact) inspectRekorRecord(ctx context.Context, client *rekor.Client, entryID rekor.EntryID) (types.ArtifactReference, error) {
-	entry, err := client.GetEntry(ctx, entryID)
-	if err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to get rekor entry: %w", err)
-	}
-
-	f, err := os.CreateTemp("", "sbom-*")
-	if err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to create a temporary file: %w", err)
-	}
-	defer os.Remove(f.Name())
-
-	if _, err := f.Write(entry.Statement); err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to write statement: %w", err)
-	}
-	if err := f.Close(); err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to close: %w", err)
-	}
-
-	ar, err := sbom.NewArtifact(f.Name(), a.cache, a.artifactOption)
-	if err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to new artifact: %w", err)
-	}
-
-	results, err := ar.Inspect(ctx)
-	if err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to inspect: %w", err)
-	}
-	results.Name = a.image.Name()
-
-	return results, nil
-}
-
-func (a Artifact) inspectSbomAttestation(ctx context.Context) (types.ArtifactReference, error) {
-	d, err := repoDigest(a.image)
-	if err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to get repo digest: %w", err)
-	}
-
-	client, err := rekor.NewClient(a.artifactOption.RekorUrl)
-	if err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to create rekor client: %w", err)
-	}
-
-	entryIDs, err := client.Search(ctx, d)
-	if err != nil {
-		return types.ArtifactReference{}, xerrors.Errorf("failed to search rekor records: %w", err)
-	}
-
-	log.Logger.Debugf("Found matching entries: %s", entryIDs)
-
-	for _, id := range entryIDs {
-		log.Logger.Debugf("Inspecting rekor entry: %s", id)
-		results, err := a.inspectRekorRecord(ctx, client, id)
-		if err == nil {
-			return results, nil
-		}
-	}
-	return types.ArtifactReference{}, xerrors.Errorf("failed to inspect SBOM attestation: %w", err)
 }
---
+++ pkg/fanal/artifact/image/remote_sbom.go
@@ -0,0 +1,109 @@
+package image
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"golang.org/x/xerrors"
+
+	"github.com/aquasecurity/trivy/pkg/fanal/artifact/sbom"
+	"github.com/aquasecurity/trivy/pkg/fanal/log"
+	ftypes "github.com/aquasecurity/trivy/pkg/fanal/types"
+	"github.com/aquasecurity/trivy/pkg/rekor"
+	"github.com/aquasecurity/trivy/pkg/types"
+)
+
+var (
+	errSBOMNotFound = xerrors.New("remote SBOM not found")
+	errNoRepoDigest = xerrors.New("no repo digest")
+)
+
+func (a Artifact) fetchRemoteSBOM(ctx context.Context) (ftypes.ArtifactReference, error) {
+	for _, sbomFrom := range a.artifactOption.SbomFroms {
+		switch sbomFrom {
+		case types.SbomFromTypeRekor:
+			ref, err := a.inspectSbomAttestation(ctx)
+			if err == nil {
+				return ref, nil
+			}
+			log.Logger.Debugf("Failed to inspect SBOM Attestation from rekor")
+		}
+	}
+	return ftypes.ArtifactReference{}, errSBOMNotFound
+
+}
+
+func (a Artifact) inspectSbomAttestation(ctx context.Context) (ftypes.ArtifactReference, error) {
+	d, err := repoDigest(a.image)
+	if err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to get repo digest: %w", err)
+	}
+
+	client, err := rekor.NewClient(a.artifactOption.RekorUrl)
+	if err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to create rekor client: %w", err)
+	}
+
+	entryIDs, err := client.Search(ctx, d)
+	if err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to search rekor records: %w", err)
+	}
+
+	log.Logger.Debugf("Found matching entries: %s", entryIDs)
+
+	for _, id := range entryIDs {
+		log.Logger.Debugf("Inspecting rekor entry: %s", id)
+		results, err := a.inspectRekorRecord(ctx, client, id)
+		if err == nil {
+			return results, nil
+		}
+	}
+	return ftypes.ArtifactReference{}, xerrors.Errorf("failed to inspect SBOM attestation: %w", err)
+}
+
+func (a Artifact) inspectRekorRecord(ctx context.Context, client *rekor.Client, entryID rekor.EntryID) (ftypes.ArtifactReference, error) {
+	entry, err := client.GetEntry(ctx, entryID)
+	if err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to get rekor entry: %w", err)
+	}
+
+	f, err := os.CreateTemp("", "sbom-*")
+	if err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to create a temporary file: %w", err)
+	}
+	defer os.Remove(f.Name())
+
+	if _, err = f.Write(entry.Statement); err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to write statement: %w", err)
+	}
+	if err = f.Close(); err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to close: %w", err)
+	}
+
+	ar, err := sbom.NewArtifact(f.Name(), a.cache, a.artifactOption)
+	if err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to new artifact: %w", err)
+	}
+
+	results, err := ar.Inspect(ctx)
+	if err != nil {
+		return ftypes.ArtifactReference{}, xerrors.Errorf("failed to inspect: %w", err)
+	}
+	results.Name = a.image.Name()
+
+	return results, nil
+}
+
+func repoDigest(img ftypes.Image) (string, error) {
+	repoNameFull := img.Name()
+	repoName, _, _ := strings.Cut(repoNameFull, ":")
+
+	for _, rd := range img.RepoDigests() {
+		if name, d, found := strings.Cut(rd, "@"); found && name == repoName {
+			return d, nil
+		}
+	}
+	return "", errNoRepoDigest
+
+}
