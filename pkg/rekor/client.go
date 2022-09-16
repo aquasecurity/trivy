11d93d3baf75438c2d948801e17679409c451cff
Akira Saso <sasoakira6114@gmail.com>
2022-09-16 09:38:36 +0900

    fix: enable parsing of UUID-only rekor entry ID

--- pkg/rekor/client.go
+++ pkg/rekor/client.go
@@ -22,20 +22,28 @@ const (
 
 var ErrNoAttestation = xerrors.Errorf("Rekor attestations not found")
 
-// EntryID is a hex-format string. The length of the string is 80.
-// It consists of two elements, the TreeID and the UUID.
+// EntryID is a hex-format string. The length of the string is 80 or 64.
+// If the length is 80, it consists of two elements, the TreeID and the UUID. If the length is 64,
+// it consists only of the UUID.
 // cf. https://github.com/sigstore/rekor/blob/4923f60f4ae55ccd4baf28d182e8f55c2d8097d3/pkg/sharding/sharding.go#L25-L36
-type EntryID string
-
-func NewEntryID(entryID string) (EntryID, error) {
-	if len(entryID) == treeIDLen+uuidLen {
-		return EntryID(entryID), nil
-	}
-	return "", xerrors.Errorf("invalid Entry ID length")
+type EntryID struct {
+	TreeID string
+	UUID   string
 }
 
-func (e EntryID) UUID() string {
-	return string(e)[treeIDLen:]
+func NewEntryID(entryID string) (EntryID, error) {
+	switch len(entryID) {
+	case treeIDLen + uuidLen:
+		return EntryID{TreeID: entryID[:treeIDLen], UUID: entryID[treeIDLen:]}, nil
+	case uuidLen:
+		return EntryID{TreeID: "", UUID: entryID}, nil
+	default:
+		return EntryID{}, xerrors.Errorf("invalid Entry ID length")
+	}
+}
+
+func (e EntryID) String() string {
+	return e.TreeID + e.UUID
 }
 
 type Entry struct {
@@ -79,7 +87,7 @@ func (c *Client) Search(ctx context.Context, hash string) ([]EntryID, error) {
 }
 
 func (c *Client) GetEntry(ctx context.Context, entryID EntryID) (Entry, error) {
-	params := entries.NewGetLogEntryByUUIDParamsWithContext(ctx).WithEntryUUID(string(entryID))
+	params := entries.NewGetLogEntryByUUIDParamsWithContext(ctx).WithEntryUUID(entryID.String())
 
 	// TODO: bulk search
 	resp, err := c.Entries.GetLogEntryByUUID(params)
@@ -87,7 +95,7 @@ func (c *Client) GetEntry(ctx context.Context, entryID EntryID) (Entry, error) {
 		return Entry{}, xerrors.Errorf("failed to get log entry by UUID: %w", err)
 	}
 
-	entry, found := resp.Payload[entryID.UUID()]
+	entry, found := resp.Payload[entryID.UUID]
 	if !found {
 		return Entry{}, ErrNoAttestation
 	}
