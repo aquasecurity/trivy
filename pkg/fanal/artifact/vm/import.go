675342d5389f18e6c735bd299d34dfc16a131910
masahiro331 <m_fujimura@r.recruit.co.jp>
2022-10-15 03:09:58 +0900

    fix(vm): fix linter

--- pkg/fanal/artifact/vm/import.go
+++ pkg/fanal/artifact/vm/import.go
@@ -4,6 +4,7 @@ import (
 	// Register Filesystem
 	_ "github.com/aquasecurity/trivy/pkg/fanal/vm/filesystem/ext4"
 	_ "github.com/aquasecurity/trivy/pkg/fanal/vm/filesystem/xfs"
+
 	// Register VirtualMachineImage
 	_ "github.com/aquasecurity/trivy/pkg/fanal/vm/vmdk"
 )
