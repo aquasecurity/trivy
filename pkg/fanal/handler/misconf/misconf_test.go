af781f900c22441210c4f1e26c59a501a4ac3da5
Owen Rumney <owen.rumney@aquasec.com>
2022-10-18 17:23:20 +0100

    fix path concat for windows

--- pkg/fanal/handler/misconf/misconf_test.go
+++ pkg/fanal/handler/misconf/misconf_test.go
@@ -90,7 +90,7 @@ func Test_FindingFSTarget(t *testing.T) {
 		},
 		{
 			input:      []string{filepath.Join(string(os.PathSeparator), "home", "user")},
-			wantTarget: "/home/user",
+			wantTarget: filepath.Join("home", "user"),
 			wantPaths:  []string{"."},
 		},
 		{
@@ -98,7 +98,7 @@ func Test_FindingFSTarget(t *testing.T) {
 				filepath.Join(string(os.PathSeparator), "home", "user"),
 				filepath.Join(string(os.PathSeparator), "home", "user", "something"),
 			},
-			wantTarget: filepath.Join(string(os.PathSeparator), "home", "user"),
+			wantTarget: filepath.Join("home", "user"),
 			wantPaths:  []string{".", "/something"},
 		},
 		{
@@ -106,7 +106,7 @@ func Test_FindingFSTarget(t *testing.T) {
 				filepath.Join(string(os.PathSeparator), "home", "user"),
 				filepath.Join(string(os.PathSeparator), "home", "user", "something", "else"),
 			},
-			wantTarget: filepath.Join(string(os.PathSeparator), "home", "user"),
+			wantTarget: filepath.Join("home", "user"),
 			wantPaths:  []string{".", "/something/else"},
 		},
 		{
@@ -114,7 +114,7 @@ func Test_FindingFSTarget(t *testing.T) {
 				filepath.Join(string(os.PathSeparator), "home", "user"),
 				filepath.Join(string(os.PathSeparator), "home", "user2", "something", "else"),
 			},
-			wantTarget: filepath.Join(string(os.PathSeparator), "home"),
+			wantTarget: filepath.Join("home"),
 			wantPaths:  []string{"/user", "/user2/something/else"},
 		},
 		{
@@ -135,10 +135,14 @@ func Test_FindingFSTarget(t *testing.T) {
 		t.Run(fmt.Sprintf("%#v", test.input), func(t *testing.T) {
 
 			if runtime.GOOS == "windows" {
+				cwd, err := os.Getwd()
+				require.NoError(t, err)
+				vol := filepath.VolumeName(cwd)
+
 				if test.wantTarget != "" {
-					test.wantTarget = filepath.Join("C:", test.wantTarget)
+					test.wantTarget = filepath.Join(vol, string(os.PathSeparator), test.wantTarget)
 				} else {
-					test.wantTarget = "C:"
+					test.wantTarget = vol
 				}
 			}
 
