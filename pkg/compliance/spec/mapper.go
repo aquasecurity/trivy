3c9c792519f2037f1f605e5345fab94cc1e16e46
knqyf263 <knqyf263@gmail.com>
2022-10-25 19:48:27 +0300

    refactor: simplify generics

--- pkg/compliance/spec/mapper.go
+++ pkg/compliance/spec/mapper.go
@@ -1,60 +1,39 @@
 package spec
 
 import (
+	"golang.org/x/exp/slices"
+
 	"github.com/aquasecurity/trivy/pkg/types"
 )
 
-// TrivyCheck represent checks models : vulnerability , misconfiguration and secret
-type TrivyCheck interface {
-	GetID() string
-	CheckType() string
-	CheckPass() bool
-}
-
-// Mapper represent scan checks to spec check ids mapper
-type Mapper[T TrivyCheck] interface {
-	FilterScanResultsBySpecCheckIds(trivyChecks []T, scannerCheckIDs map[string][]string) []T
-	MapSpecCheckIDToFilteredResults(trivyChecks []TrivyCheck, target string, class types.ResultClass, typeN string, scannerCheckIDs map[string][]string) map[string]types.Results
-}
-
-type mapper[T TrivyCheck] struct {
-}
-
-// NewMapper instansiate new Mapper for specific scanner type
-func NewMapper[T TrivyCheck]() Mapper[T] {
-	return &mapper[T]{}
-}
-
-// FilterScanResultsBySpecCheckIds create a array of filtered security checks by spec checks ids
-func (m mapper[T]) FilterScanResultsBySpecCheckIds(trivyChecks []T, scannerCheckIDs map[string][]string) []T {
-	filteredSecurityCheck := make([]T, 0)
-	for _, tc := range trivyChecks {
-		for _, id := range scannerCheckIDs[tc.CheckType()] {
-			if tc.GetID() == id {
-				filteredSecurityCheck = append(filteredSecurityCheck, tc)
-			}
-		}
-	}
-	return filteredSecurityCheck
-}
-
-// MapSpecCheckIDToFilteredResults map spec check id to filterred scan results
-func (m mapper[T]) MapSpecCheckIDToFilteredResults(trivyChecks []TrivyCheck, target string, class types.ResultClass, typeN string, scannerCheckIDs map[string][]string) map[string]types.Results {
+// MapSpecCheckIDToFilteredResults map spec check id to filtered scan results
+func MapSpecCheckIDToFilteredResults(result types.Result, checkIDs map[types.SecurityCheck][]string) map[string]types.Results {
 	mapCheckByID := make(map[string]types.Results)
-	for _, tc := range trivyChecks {
-		if _, ok := mapCheckByID[tc.GetID()]; !ok {
-			mapCheckByID[tc.GetID()] = make(types.Results, 0)
+	for _, vuln := range result.Vulnerabilities {
+		// Skip irrelevant check IDs
+		if !slices.Contains(checkIDs[types.SecurityCheckVulnerability], vuln.GetID()) {
+			continue
 		}
-		for _, id := range scannerCheckIDs[tc.CheckType()] {
-			if tc.GetID() == id {
-				switch val := tc.(type) {
-				case types.DetectedMisconfiguration:
-					mapCheckByID[tc.GetID()] = append(mapCheckByID[tc.GetID()], types.Result{Target: target, Class: class, Type: typeN, MisconfSummary: misconfigSummary(val), Misconfigurations: []types.DetectedMisconfiguration{val}})
-				case types.DetectedVulnerability:
-					mapCheckByID[tc.GetID()] = append(mapCheckByID[tc.GetID()], types.Result{Target: target, Class: class, Type: typeN, Vulnerabilities: []types.DetectedVulnerability{val}})
-				}
-			}
+		mapCheckByID[vuln.GetID()] = append(mapCheckByID[vuln.GetID()], types.Result{
+			Target:          result.Target,
+			Class:           result.Class,
+			Type:            result.Type,
+			Vulnerabilities: []types.DetectedVulnerability{vuln},
+		})
+	}
+	for _, m := range result.Misconfigurations {
+		// Skip irrelevant check IDs
+		if !slices.Contains(checkIDs[types.SecurityCheckConfig], m.GetID()) {
+			continue
 		}
+
+		mapCheckByID[m.GetID()] = append(mapCheckByID[m.GetID()], types.Result{
+			Target:            result.Target,
+			Class:             result.Class,
+			Type:              result.Type,
+			MisconfSummary:    misconfigSummary(m),
+			Misconfigurations: []types.DetectedMisconfiguration{m},
+		})
 	}
 	return mapCheckByID
 }
@@ -65,66 +44,24 @@ func misconfigSummary(misconfig types.DetectedMisconfiguration) *types.MisconfSu
 	case types.StatusPassed:
 		rms.Successes = 1
 	case types.StatusFailure:
-		rms.Successes = 1
+		rms.Failures = 1
 	case types.StatusException:
 		rms.Exceptions = 1
 	}
 	return &rms
 }
 
-// FilterResults filter miconfiguration and vulnerabilities results by spec scanner check Ids
-func FilterResults(results types.Results, scannerCheckIDs map[string][]string) types.Results {
-	filteredResults := make(types.Results, 0)
-	for _, result := range results {
-		if len(result.Misconfigurations) > 0 {
-			filteredMisconfig := NewMapper[types.DetectedMisconfiguration]().FilterScanResultsBySpecCheckIds(result.Misconfigurations, scannerCheckIDs)
-			result.Misconfigurations = filteredMisconfig
-		}
-		if len(result.Vulnerabilities) > 0 {
-			filteredVuln := NewMapper[types.DetectedVulnerability]().FilterScanResultsBySpecCheckIds(result.Vulnerabilities, scannerCheckIDs)
-			result.Vulnerabilities = filteredVuln
-		}
-		filteredResults = append(filteredResults, result)
-	}
-	return filteredResults
-}
-
-// AggregateAllChecksBySpecID aggregate all scan results and map it to spec ids
-func AggregateAllChecksBySpecID(multiResults []types.Results, controls []Control) map[string]types.Results {
-	scannerCheckIDs := ScannerCheckIDs(controls)
+// AggregateAllChecksBySpecID aggregates all scan results and map it to spec ids
+func AggregateAllChecksBySpecID(multiResults []types.Results, cs ComplianceSpec) map[string]types.Results {
+	checkIDs := cs.CheckIDs()
 	complianceArr := make(map[string]types.Results, 0)
 	for _, resResult := range multiResults {
-		filteredResults := FilterResults(resResult, scannerCheckIDs)
-		for _, result := range filteredResults {
-			if len(result.Misconfigurations) > 0 {
-				cMapper := NewMapper[types.DetectedMisconfiguration]()
-				misconfigMap := cMapper.MapSpecCheckIDToFilteredResults(getTrivyChecks(result.Misconfigurations), result.Target, result.Class, result.Type, scannerCheckIDs)
-				for id, checks := range misconfigMap {
-					if _, ok := misconfigMap[id]; !ok {
-						complianceArr[id] = make(types.Results, 0)
-					}
-					complianceArr[id] = append(complianceArr[id], checks...)
-				}
-			}
-			if len(result.Vulnerabilities) > 0 {
-				vMapper := NewMapper[types.DetectedMisconfiguration]()
-				vulnsMap := vMapper.MapSpecCheckIDToFilteredResults(getTrivyChecks(result.Vulnerabilities), result.Target, result.Class, result.Type, scannerCheckIDs)
-				for id, checks := range vulnsMap {
-					if _, ok := vulnsMap[id]; !ok {
-						complianceArr[id] = make(types.Results, 0)
-					}
-					complianceArr[id] = append(complianceArr[id], checks...)
-				}
+		for _, result := range resResult {
+			m := MapSpecCheckIDToFilteredResults(result, checkIDs)
+			for id, checks := range m {
+				complianceArr[id] = append(complianceArr[id], checks...)
 			}
 		}
 	}
 	return complianceArr
-}
-
-func getTrivyChecks[T TrivyCheck](checks []T) []TrivyCheck {
-	tc := make([]TrivyCheck, 0)
-	for _, check := range checks {
-		tc = append(tc, check)
-	}
-	return tc
 }
--- pkg/compliance/spec/mapper_test.go
+++ pkg/compliance/spec/mapper_test.go
@@ -3,83 +3,98 @@ package spec_test
 import (
 	"testing"
 
-	"github.com/aquasecurity/trivy/pkg/compliance/spec"
-	"github.com/aquasecurity/trivy/pkg/types"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/aquasecurity/trivy/pkg/compliance/spec"
+	ftypes "github.com/aquasecurity/trivy/pkg/fanal/types"
+	"github.com/aquasecurity/trivy/pkg/types"
 )
 
-func TestFilterScanResultsBySpecCheckIds(t *testing.T) {
-	tests := []struct {
-		name        string
-		specPath    string
-		trivyCheck  spec.TrivyCheck
-		wantMapping map[string][]spec.TrivyCheck
-	}{
-		{name: "filter results by check ids for config", specPath: "./testdata/mapping_spec.yaml", trivyCheck: types.DetectedMisconfiguration{AVDID: "KSV012"},
-			wantMapping: map[string][]spec.TrivyCheck{"KSV012": {types.DetectedMisconfiguration{AVDID: "KSV012"}}}},
-		{name: "filter results by check ids for vulns", specPath: "./testdata/mapping_spec.yaml", trivyCheck: types.DetectedVulnerability{VulnerabilityID: "KSV014"},
-			wantMapping: map[string][]spec.TrivyCheck{"KSV014": {types.DetectedVulnerability{VulnerabilityID: "KSV014"}}}},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			cr, err := ReadSpecFile(tt.specPath)
-			assert.NoError(t, err)
-			scannerIDMap := spec.ScannerCheckIDs(cr.Spec.Controls)
-			m := spec.NewMapper[spec.TrivyCheck]()
-			filteredResults := m.FilterScanResultsBySpecCheckIds([]spec.TrivyCheck{tt.trivyCheck}, scannerIDMap)
-			for _, v := range filteredResults {
-				assert.Equal(t, len(tt.wantMapping[v.GetID()]), 1)
-			}
-		})
-	}
-}
-
-func TestFilterResults(t *testing.T) {
-	tests := []struct {
-		name         string
-		specPath     string
-		results      types.Results
-		wantFiltered types.Results
-	}{
-		{name: "filter results by check ids define in spec", specPath: "./testdata/mapping_spec.yaml",
-			results:      types.Results{{Misconfigurations: []types.DetectedMisconfiguration{{AVDID: "AVD-KSV012"}, {AVDID: "AVD-KSV017"}}, Vulnerabilities: []types.DetectedVulnerability{{VulnerabilityID: "CVE-KSV014"}}}},
-			wantFiltered: types.Results{{Misconfigurations: []types.DetectedMisconfiguration{{AVDID: "AVD-KSV012"}}, Vulnerabilities: []types.DetectedVulnerability{{VulnerabilityID: "CVE-KSV014"}}}}},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			cr, err := ReadSpecFile(tt.specPath)
-			assert.NoError(t, err)
-			scannerIDMap := spec.ScannerCheckIDs(cr.Spec.Controls)
-			r := spec.FilterResults(tt.results, scannerIDMap)
-			assert.Equal(t, r, tt.wantFiltered)
-		})
-	}
-}
-
-func TestMapSpecCheckIDtoFilteredResults(t *testing.T) {
-	tests := []struct {
-		name        string
-		specPath    string
-		trivyCheck  spec.TrivyCheck
-		wantMapping map[string]types.Results
-	}{
-		{name: "map Check by ID config", specPath: "./testdata/mapping_spec.yaml", trivyCheck: types.DetectedMisconfiguration{AVDID: "AVD-KSV012"},
-			wantMapping: map[string]types.Results{"AVD-KSV012": {types.Result{Target: "target", MisconfSummary: &types.MisconfSummary{Successes: 0, Failures: 0, Exceptions: 0}, Class: "class", Type: "typeN", Misconfigurations: []types.DetectedMisconfiguration{{AVDID: "AVD-KSV012"}}}}},
+func TestMapSpecCheckIDToFilteredResults(t *testing.T) {
+	checkIDs := map[types.SecurityCheck][]string{
+		types.SecurityCheckConfig: {
+			"AVD-KSV012",
+			"AVD-1.2.31",
+			"AVD-1.2.32",
+		},
+		types.SecurityCheckVulnerability: {
+			"CVE-9999-9999",
 		},
 	}
-
+	tests := []struct {
+		name     string
+		checkIDs map[types.SecurityCheck][]string
+		result   types.Result
+		want     map[string]types.Results
+	}{
+		{
+			name:     "misconfiguration",
+			checkIDs: checkIDs,
+			result: types.Result{
+				Target: "target",
+				Class:  types.ClassConfig,
+				Type:   ftypes.Kubernetes,
+				Misconfigurations: []types.DetectedMisconfiguration{
+					{AVDID: "AVD-KSV012", Status: types.StatusFailure},
+					{AVDID: "AVD-KSV013", Status: types.StatusFailure},
+					{AVDID: "AVD-1.2.31", Status: types.StatusFailure},
+				},
+			},
+			want: map[string]types.Results{
+				"AVD-KSV012": {
+					{
+						Target:         "target",
+						Class:          types.ClassConfig,
+						Type:           ftypes.Kubernetes,
+						MisconfSummary: &types.MisconfSummary{Successes: 0, Failures: 1, Exceptions: 0},
+						Misconfigurations: []types.DetectedMisconfiguration{
+							{AVDID: "AVD-KSV012", Status: types.StatusFailure},
+						},
+					},
+				},
+				"AVD-1.2.31": {
+					{
+						Target:         "target",
+						Class:          types.ClassConfig,
+						Type:           ftypes.Kubernetes,
+						MisconfSummary: &types.MisconfSummary{Successes: 0, Failures: 1, Exceptions: 0},
+						Misconfigurations: []types.DetectedMisconfiguration{
+							{AVDID: "AVD-1.2.31", Status: types.StatusFailure},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:     "vulnerability",
+			checkIDs: checkIDs,
+			result: types.Result{
+				Target: "target",
+				Class:  types.ClassLangPkg,
+				Type:   ftypes.GoModule,
+				Vulnerabilities: []types.DetectedVulnerability{
+					{VulnerabilityID: "CVE-9999-0001"},
+					{VulnerabilityID: "CVE-9999-9999"},
+				},
+			},
+			want: map[string]types.Results{
+				"CVE-9999-9999": {
+					{
+						Target: "target",
+						Class:  types.ClassLangPkg,
+						Type:   ftypes.GoModule,
+						Vulnerabilities: []types.DetectedVulnerability{
+							{VulnerabilityID: "CVE-9999-9999"},
+						},
+					},
+				},
+			},
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			cr, err := ReadSpecFile(tt.specPath)
-			assert.NoError(t, err)
-			scannerIDMap := spec.ScannerCheckIDs(cr.Spec.Controls)
-			m := spec.NewMapper[spec.TrivyCheck]()
-			mapResults := m.MapSpecCheckIDToFilteredResults([]spec.TrivyCheck{tt.trivyCheck}, "target", "class", "typeN", scannerIDMap)
-			for key, val := range tt.wantMapping {
-				assert.Equal(t, mapResults[key], val)
-			}
+			got := spec.MapSpecCheckIDToFilteredResults(tt.result, tt.checkIDs)
+			assert.Equalf(t, tt.want, got, "CheckIDs()")
 		})
 	}
 }
