824fd058a49e2b5fb650d5ade025e178d8a15e5d
AMF <work@afdesk.com>
2022-12-29 22:31:38 +0600

    removing old rpm files except the last 5

--- ci/deploy-rpm.sh
+++ ci/deploy-rpm.sh
@@ -4,18 +4,22 @@ function create_rpm_repo () {
         version=$1
         rpm_path=rpm/releases/${version}/x86_64
 
+        # removing all old rpm files except the last 5
+        files=$(ls -1 $rpm_path/trivy-* | head -n -5)
+        for old_version in ${files[@]}; do
+            rm $old_version
+        done
+
         RPM_EL=$(find ../dist/ -type f -name "*64bit.rpm" -printf "%f\n" | head -n1 | sed -e "s/_/-/g" -e "s/-Linux/.el$version/" -e "s/-64bit/.x86_64/")
         echo $RPM_EL
 
         mkdir -p $rpm_path
         cp ../dist/*64bit.rpm ${rpm_path}/${RPM_EL}
 
-        createrepo_c --update $rpm_path
+        createrepo_c --compatibility --retain-old-md=5 --update $rpm_path
 }
 
 cd trivy-repo
-
-rm -r rpm/releases/
 
 VERSIONS=(5 6 7 8 9)
 for version in ${VERSIONS[@]}; do
