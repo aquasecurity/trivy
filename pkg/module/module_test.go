47c94616eca9500026c378f2c384bffd9c2441b8
varanasikalyan <varanasikalyan@gmail.com>
2023-02-07 15:43:35 +0530

    Adding test for loading wasm modules via --module-files flag
    Adding test for loading wasm modules via --module-files flag

--- pkg/module/module_test.go
+++ pkg/module/module_test.go
@@ -109,3 +109,100 @@ func TestManager_Register(t *testing.T) {
 		})
 	}
 }
+
+func TestManager_CustomModuleRegister(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		// WASM tests difficult on Windows
+		t.Skip("Test satisfied adequately by Linux tests")
+	}
+	tests := []struct {
+		name                    string
+		noModuleDir             bool
+		moduleName              string
+		wantAnalyzerVersions    map[string]int
+		wantPostScannerVersions map[string]int
+		wantErr                 bool
+	}{
+		{
+			name:       "happy path",
+			moduleName: "happy",
+			wantAnalyzerVersions: map[string]int{
+				"happy": 1,
+			},
+			wantPostScannerVersions: map[string]int{
+				"happy": 1,
+			},
+		},
+		{
+			name:       "only analyzer",
+			moduleName: "analyzer",
+			wantAnalyzerVersions: map[string]int{
+				"analyzer": 1,
+			},
+			wantPostScannerVersions: map[string]int{},
+		},
+		{
+			name:                 "only post scanner",
+			moduleName:           "scanner",
+			wantAnalyzerVersions: map[string]int{},
+			wantPostScannerVersions: map[string]int{
+				"scanner": 2,
+			},
+		},
+		{
+			name:                    "no module dir",
+			noModuleDir:             true,
+			moduleName:              "happy",
+			wantAnalyzerVersions:    map[string]int{},
+			wantPostScannerVersions: map[string]int{},
+		},
+	}
+
+	// Loading and registering WASM file(s) via --module-files flag
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			modulePath := filepath.Join("testdata", tt.moduleName, tt.moduleName+".wasm")
+
+			// WASM modules must be generated before running this test.
+			if _, err := os.Stat(modulePath); os.IsNotExist(err) {
+				require.Fail(t, "missing WASM modules, try 'make test' or 'make generate-test-modules'")
+			}
+
+			// Set up a temp dir for modules
+			tmpDir := t.TempDir()
+			t.Setenv("XDG_DATA_HOME", tmpDir)
+			moduleDir := filepath.Join(tmpDir, module.RelativeDir)
+
+			if !tt.noModuleDir {
+				err := os.MkdirAll(moduleDir, 0777)
+				require.NoError(t, err)
+
+				// Copy the wasm module for testing
+				_, err = utils.CopyFile(modulePath, filepath.Join(moduleDir, filepath.Base(modulePath)))
+				require.NoError(t, err)
+			}
+
+			modulePaths := []string{filepath.Join(moduleDir, tt.moduleName+".wasm")}
+			m, err := module.NewManager(context.Background(), modulePaths...)
+			require.NoError(t, err)
+
+			// Register analyzer and post scanner from WASM module
+			m.Register()
+			defer func() {
+				analyzer.DeregisterAnalyzer(analyzer.Type(tt.moduleName))
+				post.DeregisterPostScanner(tt.moduleName)
+			}()
+
+			// Confirm the analyzer is registered
+			a, err := analyzer.NewAnalyzerGroup(analyzer.AnalyzerOptions{})
+			require.NoError(t, err)
+
+			got := a.AnalyzerVersions()
+			assert.Equal(t, tt.wantAnalyzerVersions, got)
+
+			// Confirm the post scanner is registered
+			got = post.ScannerVersions()
+			assert.Equal(t, tt.wantPostScannerVersions, got)
+		})
+	}
+}
