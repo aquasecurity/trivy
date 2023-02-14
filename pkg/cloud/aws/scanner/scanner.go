c1565a4f4579e6c025e63752df2b05ae3e2fc774
Gio Rodriguez <giovanni.rodriguez@aquasec.com>
2023-02-14 11:40:54 -0500

    adding a fix for update-cache that was not applied on AWS scans.

--- pkg/cloud/aws/scanner/scanner.go
+++ pkg/cloud/aws/scanner/scanner.go
@@ -93,10 +93,14 @@ func (s *AWSScanner) Scan(ctx context.Context, option flag.Options) (scan.Result
 			framework.CIS_AWS_1_2))
 	}
 
+	if option.CloudOptions.UpdateCache {
+		scannerOpts = append(scannerOpts, options.ScannerWithSpec(option.Compliance))
+	}
+
 	scanner := aws.New(scannerOpts...)
 
 	var freshState *state.State
-	if len(missing) > 0 {
+	if len(missing) > 0 || option.CloudOptions.UpdateCache {
 		var err error
 		freshState, err = scanner.CreateState(ctx)
 		if err != nil {
