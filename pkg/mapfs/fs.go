e2555eab0a2d7a95761516944f886584219b7ce5
knqyf263 <knqyf263@gmail.com>
2023-02-16 22:54:42 +0200

    feat: add mapfs

---
+++ pkg/mapfs/fs.go
@@ -0,0 +1,164 @@
+package mapfs
+
+import (
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"golang.org/x/exp/slices"
+	"golang.org/x/xerrors"
+
+	"github.com/aquasecurity/trivy/pkg/syncx"
+)
+
+var _ fs.FS = &FS{}
+
+// FS is an in-memory filesystem
+type FS struct {
+	root *file
+}
+
+// New creates a new filesystem
+func New() *FS {
+	return &FS{
+		root: &file{
+			stat: fileStat{
+				name:    ".",
+				size:    0x100,
+				modTime: time.Now(),
+				mode:    0o0700 | fs.ModeDir,
+			},
+			files: syncx.Map[string, *file]{},
+		},
+	}
+}
+
+// Filter removes the specified skippedFiles and returns a new FS
+func (m *FS) Filter(skippedFiles []string) (*FS, error) {
+	if len(skippedFiles) == 0 {
+		return m, nil
+	}
+	newFS := New()
+	err := fs.WalkDir(m, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return newFS.MkdirAll(path, d.Type().Perm())
+		}
+
+		if slices.Contains(skippedFiles, path) {
+			return nil
+		}
+
+		f, err := m.root.getFile(path)
+		if err != nil {
+			return xerrors.Errorf("unable to get %s: %w", path, err)
+		}
+		return newFS.WriteFile(path, f.path)
+	})
+	if err != nil {
+		return nil, xerrors.Errorf("walk error", err)
+	}
+
+	return newFS, nil
+
+}
+
+// Stat returns a FileInfo describing the file.
+func (m *FS) Stat(name string) (fs.FileInfo, error) {
+	name = filepath.Clean(name)
+	f, err := m.root.getFile(name)
+	if err != nil {
+		return nil, &fs.PathError{
+			Op:   "stat",
+			Path: name,
+			Err:  err,
+		}
+	}
+	if f.stat.IsDir() {
+		return &f.stat, nil
+	}
+	return os.Stat(f.path)
+}
+
+// ReadDir reads the named directory
+// and returns a list of directory entries sorted by filename.
+func (m *FS) ReadDir(name string) ([]fs.DirEntry, error) {
+	return m.root.ReadDir(filepath.Clean(name))
+}
+
+// Open opens the named file for reading.
+func (m *FS) Open(name string) (fs.File, error) {
+	return m.root.Open(filepath.Clean(name))
+}
+
+// WriteFile writes the specified bytes to the named file. If the file exists, it will be overwritten.
+func (m *FS) WriteFile(path, underlyingPath string) error {
+	return m.root.WriteFile(filepath.Clean(path), underlyingPath)
+}
+
+// MkdirAll creates a directory named path,
+// along with any necessary parents, and returns nil,
+// or else returns an error.
+// The permission bits perm (before umask) are used for all
+// directories that MkdirAll creates.
+// If path is already a directory, MkdirAll does nothing
+// and returns nil.
+func (m *FS) MkdirAll(path string, perm fs.FileMode) error {
+	return m.root.MkdirAll(filepath.Clean(path), perm)
+}
+
+// ReadFile reads the named file and returns its contents.
+// A successful call returns a nil error, not io.EOF.
+// (Because ReadFile reads the whole file, the expected EOF
+// from the final Read is not treated as an error to be reported.)
+//
+// The caller is permitted to modify the returned byte slice.
+// This method should return a copy of the underlying data.
+func (m *FS) ReadFile(name string) ([]byte, error) {
+	f, err := m.root.Open(filepath.Clean(name))
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }()
+	return io.ReadAll(f)
+}
+
+// Sub returns an FS corresponding to the subtree rooted at dir.
+func (m *FS) Sub(dir string) (fs.FS, error) {
+	d, err := m.root.getFile(filepath.Clean(dir))
+	if err != nil {
+		return nil, err
+	}
+	return &FS{
+		root: d,
+	}, nil
+}
+
+// Glob returns the names of all files matching pattern or nil
+// if there is no matching file. The syntax of patterns is the same
+// as in Match. The pattern may describe hierarchical names such as
+// /usr/*/bin/ed (assuming the Separator is '/').
+//
+// Glob ignores file system errors such as I/O errors reading directories.
+// The only possible returned error is ErrBadPattern, when pattern
+// is malformed.
+func (m *FS) Glob(pattern string) ([]string, error) {
+	pattern = strings.ReplaceAll(pattern, "/", separator)
+	return m.root.glob(pattern)
+}
+
+// Remove deletes a file or directory from the filesystem
+func (m *FS) Remove(path string) error {
+	return m.root.Remove(filepath.Clean(path))
+}
+
+// RemoveAll deletes a file or directory and any children if present from the filesystem
+func (m *FS) RemoveAll(path string) error {
+	return m.root.RemoveAll(filepath.Clean(path))
+}
