d777bb9fe083499a16759236cbabfcd14205584a
chenk <hen.keinan@gmail.com>
2023-02-16 11:52:46 +0200

    feat: docker multi credential support
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/types/docker_conf.go
+++ pkg/types/docker_conf.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"strings"
+
 	"github.com/caarlos0/env/v6"
 	"golang.org/x/xerrors"
 
@@ -9,8 +11,8 @@ import (
 
 // DockerConfig holds the config of Docker
 type DockerConfig struct {
-	UserName      string `env:"TRIVY_USERNAME"`
-	Password      string `env:"TRIVY_PASSWORD"`
+	UsersName     string `env:"TRIVY_USERNAME"`
+	Passwords     string `env:"TRIVY_PASSWORD"`
 	RegistryToken string `env:"TRIVY_REGISTRY_TOKEN"`
 	NonSSL        bool   `env:"TRIVY_NON_SSL" envDefault:"false"`
 }
@@ -21,10 +23,26 @@ func GetDockerOption(insecureTlsSkip bool, Platform string) (types.DockerOption,
 	if err := env.Parse(&cfg); err != nil {
 		return types.DockerOption{}, xerrors.Errorf("unable to parse environment variables: %w", err)
 	}
+	credentials := make([]types.Credential, 0)
+	users := strings.Split(cfg.UsersName, ",")
+	password := strings.Split(cfg.Passwords, ",")
+	for index, user := range users {
+		if len(user) > 0 && len(password[index]) > 0 {
+			credentials = append(credentials, types.Credential{UserName: user, Password: password[index]})
+		}
+	}
+	if len(credentials) == 0 {
+		credentials = append(credentials, types.Credential{}) // no credential use-case
+	}
+	if len(credentials) > 1 {  // backward competability maybe can be removed later
+		cfg.UsersName = credentials[0].UserName
+		cfg.Passwords = credentials[0].Password
+	}
 
 	return types.DockerOption{
-		UserName:              cfg.UserName,
-		Password:              cfg.Password,
+		UserName:              cfg.UsersName,
+		Password:              cfg.Passwords,
+		Credentials:           credentials,
 		RegistryToken:         cfg.RegistryToken,
 		InsecureSkipTLSVerify: insecureTlsSkip,
 		NonSSL:                cfg.NonSSL,
---
+++ pkg/types/docker_conf_test.go
@@ -0,0 +1,69 @@
+package types
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/aquasecurity/trivy/pkg/fanal/types"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetDockerOption(t *testing.T) {
+	tests := []struct {
+		name     string
+		userEnv  string
+		passEnv  string
+		platform string
+		want     types.DockerOption
+	}{
+		{name: "single credential value", userEnv: "user1", passEnv: "pass1", platform: "platform", want: types.DockerOption{
+			UserName: "user1",
+			Password: "pass1",
+			Credentials: []types.Credential{
+				{
+					UserName: "user1",
+					Password: "pass1",
+				},
+			},
+			Platform: "platform",
+		},
+		},
+		{name: "multi credential value", userEnv: "user1,user2", passEnv: "pass1,pass2", platform: "platform", want: types.DockerOption{
+			UserName: "user1",
+			Password: "pass1",
+			Credentials: []types.Credential{
+				{
+					UserName: "user1",
+					Password: "pass1",
+				},
+				{
+					UserName: "user2",
+					Password: "pass2",
+				},
+			},
+			Platform: "platform",
+		},
+		},
+		{name: "no credential value", platform: "platform", want: types.DockerOption{
+			UserName:    "",
+			Password:    "",
+			Credentials: []types.Credential{{}},
+			Platform:    "platform",
+		},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv("TRIVY_USERNAME", tt.userEnv)
+			os.Setenv("TRIVY_PASSWORD", tt.passEnv)
+			got, _ := GetDockerOption(false, tt.platform)
+			assert.True(t, reflect.DeepEqual(got, tt.want))
+			// reset
+			os.Setenv("TRIVY_USERNAME", "")
+			os.Setenv("TRIVY_PASSWORD", "")
+		})
+	}
+
+}
