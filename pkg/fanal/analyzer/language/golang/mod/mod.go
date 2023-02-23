1dc2b349c630da6742e64debd77c523c8c8d7645
Teppei Fukuda <knqyf263@gmail.com>
2023-02-23 13:35:08 +0200

    refactor: rewrite gomod analyzer as post-analyzer (#3674)

--- pkg/fanal/analyzer/language/golang/mod/mod.go
+++ pkg/fanal/analyzer/language/golang/mod/mod.go
@@ -2,14 +2,18 @@ package mod
 
 import (
 	"context"
+	"errors"
+	"io/fs"
 	"os"
 	"path/filepath"
 
+	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 	"golang.org/x/xerrors"
 
 	"github.com/aquasecurity/go-dep-parser/pkg/golang/mod"
 	"github.com/aquasecurity/go-dep-parser/pkg/golang/sum"
+	dio "github.com/aquasecurity/go-dep-parser/pkg/io"
 	godeptypes "github.com/aquasecurity/go-dep-parser/pkg/types"
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer/language"
@@ -17,42 +21,136 @@ import (
 )
 
 func init() {
-	analyzer.RegisterAnalyzer(&gomodAnalyzer{})
+	analyzer.RegisterPostAnalyzer(types.GoMod, newGoModAnalyzer)
 }
 
 const version = 2
 
-var requiredFiles = []string{types.GoMod, types.GoSum}
-
-type gomodAnalyzer struct{}
-
-func (a gomodAnalyzer) Analyze(_ context.Context, input analyzer.AnalysisInput) (*analyzer.AnalysisResult, error) {
-	var parser godeptypes.Parser
-	switch filepath.Base(input.FilePath) {
-	case types.GoMod:
-		parser = mod.NewParser()
-	case types.GoSum:
-		parser = sum.NewParser()
-	default:
-		return nil, nil
-	}
-
-	res, err := language.Analyze(types.GoModule, input.FilePath, input.Content, parser)
-	if err != nil {
-		return nil, xerrors.Errorf("failed to analyze %s: %w", input.FilePath, err)
-	}
-	return res, nil
+var requiredFiles = []string{
+	types.GoMod,
+	types.GoSum,
 }
 
-func (a gomodAnalyzer) Required(filePath string, _ os.FileInfo) bool {
+type gomodAnalyzer struct {
+	modParser godeptypes.Parser
+	sumParser godeptypes.Parser
+}
+
+func newGoModAnalyzer(_ analyzer.AnalyzerOptions) (analyzer.PostAnalyzer, error) {
+	return &gomodAnalyzer{
+		modParser: mod.NewParser(),
+		sumParser: sum.NewParser(),
+	}, nil
+}
+
+func (a *gomodAnalyzer) PostAnalyze(_ context.Context, input analyzer.PostAnalysisInput) (*analyzer.AnalysisResult, error) {
+	var apps []types.Application
+	err := fs.WalkDir(input.FS, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		} else if !d.Type().IsRegular() {
+			return nil
+		}
+
+		dir, file := filepath.Split(path)
+		if file != types.GoMod {
+			return nil
+		}
+
+		// Parse go.mod
+		gomod, err := parse(input.FS, path, a.modParser)
+		if err != nil {
+			return xerrors.Errorf("parse error: %w", err)
+		} else if gomod == nil {
+			return nil
+		}
+
+		if lessThanGo117(gomod) {
+			// e.g. /app/go.mod => /app/go.sum
+			sumPath := filepath.Join(dir, types.GoSum)
+			gosum, err := parse(input.FS, sumPath, a.sumParser)
+			if err != nil && !errors.Is(err, fs.ErrNotExist) {
+				return xerrors.Errorf("parse error: %w", err)
+			}
+			mergeGoSum(gomod, gosum)
+		}
+
+		apps = append(apps, *gomod)
+		return nil
+	})
+	if err != nil {
+		return nil, xerrors.Errorf("walk error: %w", err)
+	}
+
+	return &analyzer.AnalysisResult{
+		Applications: apps,
+	}, nil
+}
+
+func (a *gomodAnalyzer) Required(filePath string, _ os.FileInfo) bool {
 	fileName := filepath.Base(filePath)
 	return slices.Contains(requiredFiles, fileName)
 }
 
-func (a gomodAnalyzer) Type() analyzer.Type {
+func (a *gomodAnalyzer) Type() analyzer.Type {
 	return analyzer.TypeGoMod
 }
 
-func (a gomodAnalyzer) Version() int {
+func (a *gomodAnalyzer) Version() int {
 	return version
+}
+
+func parse(fsys fs.FS, path string, parser godeptypes.Parser) (*types.Application, error) {
+	f, err := fsys.Open(path)
+	if err != nil {
+		return nil, xerrors.Errorf("file open error: %w", err)
+	}
+	defer f.Close()
+
+	file, ok := f.(dio.ReadSeekCloserAt)
+	if !ok {
+		return nil, xerrors.Errorf("type assertion error: %w", err)
+	}
+
+	// Parse go.mod or go.sum
+	libs, deps, err := parser.Parse(file)
+	if err != nil {
+		return nil, xerrors.Errorf("%s parse error: %w", path, err)
+	}
+	return language.ToApplication(types.GoModule, path, "", libs, deps), nil
+}
+
+func lessThanGo117(gomod *types.Application) bool {
+	for _, lib := range gomod.Libraries {
+		// The indirect field is populated only in Go 1.17+
+		if lib.Indirect {
+			return false
+		}
+	}
+	return true
+}
+
+func mergeGoSum(gomod, gosum *types.Application) {
+	if gomod == nil || gosum == nil {
+		return
+	}
+	uniq := map[string]types.Package{}
+	for _, lib := range gomod.Libraries {
+		// It will be used for merging go.sum.
+		uniq[lib.Name] = lib
+	}
+
+	// For Go 1.16 or less, we need to merge go.sum into go.mod.
+	for _, lib := range gosum.Libraries {
+		// Skip dependencies in go.mod so that go.mod should be preferred.
+		if _, ok := uniq[lib.Name]; ok {
+			continue
+		}
+
+		// This dependency doesn't exist in go.mod, so it must be an indirect dependency.
+		lib.Indirect = true
+		uniq[lib.Name] = lib
+	}
+
+	gomod.Libraries = maps.Values(uniq)
 }
