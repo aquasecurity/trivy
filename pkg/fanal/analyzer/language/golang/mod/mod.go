c58fa623c02de818a452d328b421547b0a89e406
knqyf263 <knqyf263@gmail.com>
2023-02-23 22:54:58 +0200

    feat(go): license support

--- pkg/fanal/analyzer/language/golang/mod/mod.go
+++ pkg/fanal/analyzer/language/golang/mod/mod.go
@@ -3,10 +3,16 @@ package mod
 import (
 	"context"
 	"errors"
+	"fmt"
+	"go/build"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"regexp"
+	"unicode"
 
+	"github.com/samber/lo"
 	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 	"golang.org/x/xerrors"
@@ -18,6 +24,9 @@ import (
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer/language"
 	"github.com/aquasecurity/trivy/pkg/fanal/types"
+	"github.com/aquasecurity/trivy/pkg/licensing"
+	"github.com/aquasecurity/trivy/pkg/log"
+	"github.com/aquasecurity/trivy/pkg/utils/fsutils"
 )
 
 func init() {
@@ -26,10 +35,13 @@ func init() {
 
 const version = 2
 
-var requiredFiles = []string{
-	types.GoMod,
-	types.GoSum,
-}
+var (
+	requiredFiles = []string{
+		types.GoMod,
+		types.GoSum,
+	}
+	licenseRegexp = regexp.MustCompile(`^(?i)((UN)?LICEN(S|C)E|COPYING|README|NOTICE).*$`)
+)
 
 type gomodAnalyzer struct {
 	modParser godeptypes.Parser
@@ -80,6 +92,10 @@ func (a *gomodAnalyzer) PostAnalyze(_ context.Context, input analyzer.PostAnalys
 	})
 	if err != nil {
 		return nil, xerrors.Errorf("walk error: %w", err)
+	}
+
+	if err = fillLicenses(apps); err != nil {
+		return nil, xerrors.Errorf("unable to identify licenses: %w", err)
 	}
 
 	return &analyzer.AnalysisResult{
@@ -153,4 +169,101 @@ func mergeGoSum(gomod, gosum *types.Application) {
 	}
 
 	gomod.Libraries = maps.Values(uniq)
+}
+
+func fillLicenses(apps []types.Application) error {
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		gopath = build.Default.GOPATH
+	}
+
+	// $GOPATH/pkg/mod
+	modPath := filepath.Join(gopath, "pkg", "mod")
+	if !fsutils.DirExists(modPath) {
+		log.Logger.Debugf("GOPATH (%s) not found. Need 'go mod download' to fill license information", modPath)
+		return nil
+	}
+
+	licenses := map[string][]string{}
+	for i, app := range apps {
+		for j, lib := range app.Libraries {
+			libID := lib.Name + "@v" + lib.Version
+			if l, ok := licenses[libID]; ok {
+				// Fill licenses
+				apps[i].Libraries[j].Licenses = l
+				continue
+			}
+
+			// e.g. $GOPATH/pkg/mod/github.com/aquasecurity/go-dep-parser@v1.0.0
+			modDir := filepath.Join(modPath, fmt.Sprintf("%s@v%s", normalizeModName(lib.Name), lib.Version))
+			l, err := findLicense(modDir)
+			if err != nil {
+				return xerrors.Errorf("golang license error: %w", err)
+			} else if l == nil || len(l.Findings) == 0 {
+				continue
+			}
+			licenseNames := lo.Map(l.Findings, func(finding types.LicenseFinding, _ int) string {
+				return finding.Name
+			})
+			// Cache the detected licenses
+			licenses[libID] = licenseNames
+
+			// Fill licenses
+			apps[i].Libraries[j].Licenses = licenseNames
+		}
+	}
+	return nil
+}
+
+func findLicense(dir string) (*types.LicenseFile, error) {
+	var license *types.LicenseFile
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		} else if !d.Type().IsRegular() {
+			return nil
+		}
+		if !licenseRegexp.MatchString(filepath.Base(path)) {
+			return nil
+		}
+		// e.g. $GOPATH/pkg/mod/github.com/aquasecurity/go-dep-parser@v0.0.0-20220406074731-71021a481237/LICENSE
+		f, err := os.Open(path)
+		if err != nil {
+			return xerrors.Errorf("file (%s) open error: %w", path, err)
+		}
+		defer f.Close()
+
+		l, err := licensing.Classify(path, f)
+		if err != nil {
+			return xerrors.Errorf("license classify error: %w", err)
+		}
+		// License found
+		if l != nil && len(l.Findings) > 0 {
+			license = l
+			return io.EOF
+		}
+		return nil
+	})
+	// The module path may not exist
+	if errors.Is(err, os.ErrNotExist) {
+		return nil, nil
+	} else if err != nil && !errors.Is(err, io.EOF) {
+		return nil, fmt.Errorf("finding a known open source license: %w", err)
+	}
+	return license, nil
+}
+
+// normalizeModName escapes upper characters
+// e.g. 'github.com/BurntSushi/toml' => 'github.com/!burnt!sushi'
+func normalizeModName(name string) string {
+	var newName []rune
+	for _, c := range name {
+		if unicode.IsUpper(c) {
+			// 'A' => '!a'
+			newName = append(newName, '!', unicode.ToLower(c))
+		} else {
+			newName = append(newName, c)
+		}
+	}
+	return string(newName)
 }
