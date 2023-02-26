71adabefd764f7e1fee1cbc1ed8c46f13529988d
knqyf263 <knqyf263@gmail.com>
2023-02-26 19:37:50 +0200

    feat(python): parse pyproject.toml alongside poetry.lock

--- pkg/fanal/analyzer/language/python/poetry/poetry.go
+++ pkg/fanal/analyzer/language/python/poetry/poetry.go
@@ -2,42 +2,128 @@ package poetry
 
 import (
 	"context"
+	"errors"
+	"io/fs"
 	"os"
 	"path/filepath"
 
 	"golang.org/x/xerrors"
 
+	dio "github.com/aquasecurity/go-dep-parser/pkg/io"
 	"github.com/aquasecurity/go-dep-parser/pkg/python/poetry"
+	godeptypes "github.com/aquasecurity/go-dep-parser/pkg/types"
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer/language"
 	"github.com/aquasecurity/trivy/pkg/fanal/types"
+	"github.com/aquasecurity/trivy/pkg/log"
+	"github.com/aquasecurity/trivy/pkg/utils/fsutils"
 )
 
 func init() {
-	analyzer.RegisterAnalyzer(&poetryLibraryAnalyzer{})
+	analyzer.RegisterPostAnalyzer(analyzer.TypePoetry, newPoetryAnalyzer)
 }
 
 const version = 1
 
-type poetryLibraryAnalyzer struct{}
+type poetryAnalyzer struct {
+	pyprojectParser PyProjectParser
+	lockParser      godeptypes.Parser
+}
 
-func (a poetryLibraryAnalyzer) Analyze(_ context.Context, input analyzer.AnalysisInput) (*analyzer.AnalysisResult, error) {
-	res, err := language.Analyze(types.Poetry, input.FilePath, input.Content, poetry.NewParser())
-	if err != nil {
-		return nil, xerrors.Errorf("unable to parse poetry.lock: %w", err)
+func newPoetryAnalyzer(_ analyzer.AnalyzerOptions) (analyzer.PostAnalyzer, error) {
+	return &poetryAnalyzer{
+		pyprojectParser: PyProjectParser{},
+		lockParser:      poetry.NewParser(),
+	}, nil
+}
+
+func (a poetryAnalyzer) PostAnalyze(_ context.Context, input analyzer.PostAnalysisInput) (*analyzer.AnalysisResult, error) {
+	var apps []types.Application
+
+	required := func(path string, d fs.DirEntry) bool {
+		return filepath.Base(path) == types.PoetryLock
 	}
-	return res, nil
+
+	err := fsutils.WalkDir(input.FS, ".", required, func(path string, d fs.DirEntry, r dio.ReadSeekerAt) error {
+		// Parse poetry.lock
+		app, err := a.parsePoetryLock(path, r)
+		if err != nil {
+			return xerrors.Errorf("parse error: %w", err)
+		} else if app == nil {
+			return nil
+		}
+
+		// Parse pyproject.toml alongside poetry.lock to identify the direct dependencies
+		if err = a.mergePyProject(input.FS, filepath.Dir(path), app); err != nil {
+			return err
+		}
+		apps = append(apps, *app)
+
+		return nil
+	})
+	if err != nil {
+		return nil, xerrors.Errorf("poetry walk error: %w", err)
+	}
+
+	return &analyzer.AnalysisResult{
+		Applications: apps,
+	}, nil
 }
 
-func (a poetryLibraryAnalyzer) Required(filePath string, _ os.FileInfo) bool {
+func (a poetryAnalyzer) Required(filePath string, _ os.FileInfo) bool {
 	fileName := filepath.Base(filePath)
-	return fileName == types.PoetryLock
+	return fileName == types.PoetryLock || fileName == types.PyProject
 }
 
-func (a poetryLibraryAnalyzer) Type() analyzer.Type {
+func (a poetryAnalyzer) Type() analyzer.Type {
 	return analyzer.TypePoetry
 }
 
-func (a poetryLibraryAnalyzer) Version() int {
+func (a poetryAnalyzer) Version() int {
 	return version
+}
+
+func (a poetryAnalyzer) parsePoetryLock(path string, r dio.ReadSeekerAt) (*types.Application, error) {
+	libs, deps, err := a.lockParser.Parse(r)
+	if err != nil {
+		return nil, xerrors.Errorf("unable to parse poetry.lock: %w", err)
+	}
+	return language.ToApplication(types.Poetry, path, "", libs, deps), nil
+}
+
+func (a poetryAnalyzer) mergePyProject(fsys fs.FS, dir string, app *types.Application) error {
+	// Parse pyproject.toml to identify the direct dependencies
+	path := filepath.Join(dir, types.PyProject)
+	p, err := a.parsePyProject(fsys, path)
+	if errors.Is(err, fs.ErrNotExist) {
+		// Assume all the packages are direct dependencies as it cannot identify them from poetry.lock
+		log.Logger.Debugf("Poetry: %s not found", path)
+		return nil
+	} else if err != nil {
+		return xerrors.Errorf("unable to parse %s: %w", path, err)
+	}
+
+	for i, lib := range app.Libraries {
+		// Identify the direct/transitive dependencies
+		if _, ok := p.Tool.Poetry.Dependencies[lib.Name]; !ok {
+			app.Libraries[i].Indirect = true
+		}
+	}
+
+	return nil
+}
+
+func (a poetryAnalyzer) parsePyProject(fsys fs.FS, path string) (PyProject, error) {
+	// Parse pyproject.toml
+	f, err := fsys.Open(path)
+	if err != nil {
+		return PyProject{}, xerrors.Errorf("file open error: %w", err)
+	}
+	defer f.Close()
+
+	parsed, err := a.pyprojectParser.Parse(f)
+	if err != nil {
+		return PyProject{}, err
+	}
+	return parsed, nil
 }
---
+++ pkg/fanal/analyzer/language/python/poetry/pyproject.go
@@ -0,0 +1,31 @@
+package poetry
+
+import (
+	"github.com/BurntSushi/toml"
+	"golang.org/x/xerrors"
+	"io"
+)
+
+type PyProject struct {
+	Tool Tool `toml:"tool"`
+}
+
+type Tool struct {
+	Poetry Poetry `toml:"poetry"`
+}
+
+type Poetry struct {
+	Dependencies map[string]string `toml:"dependencies"`
+}
+
+// TODO: move under go-dep-parser
+type PyProjectParser struct {
+}
+
+func (p *PyProjectParser) Parse(r io.Reader) (PyProject, error) {
+	var conf PyProject
+	if _, err := toml.NewDecoder(r).Decode(&conf); err != nil {
+		return PyProject{}, xerrors.Errorf("toml decode error: %w", err)
+	}
+	return conf, nil
+}
