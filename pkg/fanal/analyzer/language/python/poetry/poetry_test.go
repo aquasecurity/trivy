719fdb1b11b079264c87339b85dda915434647b5
Teppei Fukuda <knqyf263@gmail.com>
2023-02-27 20:48:55 +0200

    feat(python): parse pyproject.toml alongside poetry.lock (#3695)
    Co-authored-by: DmitriyLewen <dmitriy.lewen@smartforce.io>

--- pkg/fanal/analyzer/language/python/poetry/poetry_test.go
+++ pkg/fanal/analyzer/language/python/poetry/poetry_test.go
@@ -5,27 +5,98 @@ import (
 	"os"
 	"testing"
 
-	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
-	"github.com/aquasecurity/trivy/pkg/fanal/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
+	"github.com/aquasecurity/trivy/pkg/fanal/types"
 )
 
 func Test_poetryLibraryAnalyzer_Analyze(t *testing.T) {
 	tests := []struct {
-		name      string
-		inputFile string
-		want      *analyzer.AnalysisResult
-		wantErr   string
+		name    string
+		dir     string
+		want    *analyzer.AnalysisResult
+		wantErr string
 	}{
 		{
-			name:      "happy path",
-			inputFile: "testdata/happy.lock",
+			name: "happy path",
+			dir:  "testdata/happy",
 			want: &analyzer.AnalysisResult{
 				Applications: []types.Application{
 					{
 						Type:     types.Poetry,
-						FilePath: "testdata/happy.lock",
+						FilePath: "poetry.lock",
+						Libraries: []types.Package{
+							{
+								ID:       "click@8.1.3",
+								Name:     "click",
+								Version:  "8.1.3",
+								Indirect: true,
+								DependsOn: []string{
+									"colorama@0.4.6",
+								},
+							},
+							{
+								ID:       "colorama@0.4.6",
+								Name:     "colorama",
+								Version:  "0.4.6",
+								Indirect: true,
+							},
+							{
+								ID:      "flask@1.0.3",
+								Name:    "flask",
+								Version: "1.0.3",
+								DependsOn: []string{
+									"click@8.1.3",
+									"itsdangerous@2.1.2",
+									"jinja2@3.1.2",
+									"werkzeug@2.2.3",
+								},
+							},
+							{
+								ID:       "itsdangerous@2.1.2",
+								Name:     "itsdangerous",
+								Version:  "2.1.2",
+								Indirect: true,
+							},
+							{
+								ID:       "jinja2@3.1.2",
+								Name:     "jinja2",
+								Version:  "3.1.2",
+								Indirect: true,
+								DependsOn: []string{
+									"markupsafe@2.1.2",
+								},
+							},
+							{
+								ID:       "markupsafe@2.1.2",
+								Name:     "markupsafe",
+								Version:  "2.1.2",
+								Indirect: true,
+							},
+							{
+								ID:       "werkzeug@2.2.3",
+								Name:     "werkzeug",
+								Version:  "2.2.3",
+								Indirect: true,
+								DependsOn: []string{
+									"markupsafe@2.1.2",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "no pyproject.toml",
+			dir:  "testdata/no-pyproject",
+			want: &analyzer.AnalysisResult{
+				Applications: []types.Application{
+					{
+						Type:     types.Poetry,
+						FilePath: "poetry.lock",
 						Libraries: []types.Package{
 							{
 								ID:      "click@8.1.3",
@@ -40,65 +111,25 @@ func Test_poetryLibraryAnalyzer_Analyze(t *testing.T) {
 								Name:    "colorama",
 								Version: "0.4.6",
 							},
-							{
-								ID:      "flask@1.0.3",
-								Name:    "flask",
-								Version: "1.0.3",
-								DependsOn: []string{
-									"click@8.1.3",
-									"itsdangerous@2.1.2",
-									"jinja2@3.1.2",
-									"werkzeug@2.2.3",
-								},
-							},
-							{
-								ID:      "itsdangerous@2.1.2",
-								Name:    "itsdangerous",
-								Version: "2.1.2",
-							},
-							{
-								ID:      "jinja2@3.1.2",
-								Name:    "jinja2",
-								Version: "3.1.2",
-								DependsOn: []string{
-									"markupsafe@2.1.2",
-								},
-							},
-							{
-								ID:      "markupsafe@2.1.2",
-								Name:    "markupsafe",
-								Version: "2.1.2",
-							},
-							{
-								ID:      "werkzeug@2.2.3",
-								Name:    "werkzeug",
-								Version: "2.2.3",
-								DependsOn: []string{
-									"markupsafe@2.1.2",
-								},
-							},
 						},
 					},
 				},
 			},
 		},
 		{
-			name:      "wrong path",
-			inputFile: "testdata/wrong.lock",
-			wantErr:   "unable to parse poetry.lock",
+			name:    "broken poetry.lock",
+			dir:     "testdata/sad",
+			wantErr: "unable to parse poetry.lock",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			f, err := os.Open(tt.inputFile)
+			a, err := newPoetryAnalyzer(analyzer.AnalyzerOptions{})
 			require.NoError(t, err)
-			defer func() { _ = f.Close() }()
 
-			a := poetryLibraryAnalyzer{}
-			got, err := a.Analyze(context.Background(), analyzer.AnalysisInput{
-				FilePath: tt.inputFile,
-				Content:  f,
+			got, err := a.PostAnalyze(context.Background(), analyzer.PostAnalysisInput{
+				FS: os.DirFS(tt.dir),
 			})
 
 			if tt.wantErr != "" {
