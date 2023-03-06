ae413cb08e5ace1854ca5f5362bd00c8fb7d1d5f
chenk <hen.keinan@gmail.com>
2023-03-06 11:31:12 +0200

    fix: exclude node collector scanning
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/k8s/commands/cluster.go
+++ pkg/k8s/commands/cluster.go
@@ -3,10 +3,12 @@ package commands
 import (
 	"context"
 
+	"github.com/aquasecurity/trivy-kubernetes/pkg/artifacts"
 	"github.com/aquasecurity/trivy-kubernetes/pkg/k8s"
 	"github.com/aquasecurity/trivy-kubernetes/pkg/trivyk8s"
 	"github.com/aquasecurity/trivy/pkg/flag"
 	"github.com/aquasecurity/trivy/pkg/log"
+	"github.com/aquasecurity/trivy/pkg/types"
 
 	"golang.org/x/xerrors"
 )
@@ -16,10 +18,18 @@ func clusterRun(ctx context.Context, opts flag.Options, cluster k8s.Cluster) err
 	if err := validateReportArguments(opts); err != nil {
 		return err
 	}
-
-	artifacts, err := trivyk8s.New(cluster, log.Logger).ListArtifactAndNodeInfo(ctx)
-	if err != nil {
-		return xerrors.Errorf("get k8s artifacts error: %w", err)
+	var artifacts []*artifacts.Artifact
+	var err error
+	if opts.Scanners.AnyEnabled(types.MisconfigScanner) {
+		artifacts, err = trivyk8s.New(cluster, log.Logger).ListArtifactAndNodeInfo(ctx)
+		if err != nil {
+			return xerrors.Errorf("get k8s artifacts with node info error: %w", err)
+		}
+	} else {
+		artifacts, err = trivyk8s.New(cluster, log.Logger).ListArtifacts(ctx)
+		if err != nil {
+			return xerrors.Errorf("get k8s artifacts error: %w", err)
+		}
 	}
 
 	runner := newRunner(opts, cluster.GetCurrentContext())
