70829195b3cf3affde6d1bad6d9e084d1b146912
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-03-07 15:51:22 +0600

    fix typo

--- pkg/commands/artifact/run.go
+++ pkg/commands/artifact/run.go
@@ -495,9 +495,9 @@ func disabledAnalyzers(opts flag.Options) []analyzer.Type {
 	}
 
 	// Parsing jar files requires Java-db client
-	// But we don't create client if vulnerability analysis is disabled and sbom format is not used
+	// But we don't create client if vulnerability analysis is disabled and SBOM format is not used
 	// We need to disable jar analyzer to avoid errors
-	// TODO disable all languages that don't contains license information for this case
+	// TODO disable all languages that don't contain license information for this case
 	if opts.Scanners.Enabled(types.LicenseScanner) && !opts.Scanners.Enabled(types.VulnerabilityScanner) &&
 		!slices.Contains(report.SupportedSBOMFormats, opts.Format) {
 		analyzers = append(analyzers, analyzer.TypeJar)
