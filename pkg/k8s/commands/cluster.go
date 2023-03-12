648be2d959a84e8494befadc10ac6129c1e96a2e
chenk <hen.keinan@gmail.com>
2023-03-12 09:43:28 +0200

    feat: exclude node-collector scanning
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/k8s/commands/cluster.go
+++ pkg/k8s/commands/cluster.go
@@ -3,14 +3,15 @@ package commands
 import (
 	"context"
 
+	"golang.org/x/exp/slices"
+	"golang.org/x/xerrors"
+
 	"github.com/aquasecurity/trivy-kubernetes/pkg/artifacts"
 	"github.com/aquasecurity/trivy-kubernetes/pkg/k8s"
 	"github.com/aquasecurity/trivy-kubernetes/pkg/trivyk8s"
 	"github.com/aquasecurity/trivy/pkg/flag"
 	"github.com/aquasecurity/trivy/pkg/log"
 	"github.com/aquasecurity/trivy/pkg/types"
-
-	"golang.org/x/xerrors"
 )
 
 // clusterRun runs scan on kubernetes cluster
@@ -20,7 +21,7 @@ func clusterRun(ctx context.Context, opts flag.Options, cluster k8s.Cluster) err
 	}
 	var artifacts []*artifacts.Artifact
 	var err error
-	if opts.Scanners.AnyEnabled(types.MisconfigScanner) {
+	if opts.Scanners.AnyEnabled(types.MisconfigScanner) && slices.Contains(opts.Components, "infra") {
 		artifacts, err = trivyk8s.New(cluster, log.Logger).ListArtifactAndNodeInfo(ctx)
 		if err != nil {
 			return xerrors.Errorf("get k8s artifacts with node info error: %w", err)
