b45e0728b09ca7046519ce8beaaac8d23bae6e82
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-03-16 12:41:46 +0600

    try to guess direct deps

--- pkg/report/table/table_test.go
+++ pkg/report/table/table_test.go
@@ -162,7 +162,7 @@ Total: 1 (MEDIUM: 0, HIGH: 1)
 			expectedOutput: ``,
 		},
 		{
-			name: "happy path with vulnerability origin graph",
+			name: "happy path with vulnerability origin graph with",
 			results: types.Results{
 				{
 					Target: "package-lock.json",
@@ -202,6 +202,104 @@ Total: 1 (MEDIUM: 0, HIGH: 1)
 							ID:      "styled-components@3.1.3",
 							Name:    "styled-components",
 							Version: "3.1.3",
+							DependsOn: []string{
+								"fbjs@0.8.18",
+							},
+						},
+					},
+					Vulnerabilities: []types.DetectedVulnerability{
+						{
+							VulnerabilityID: "CVE-2022-0235",
+							PkgID:           "node-fetch@1.7.3",
+							PkgName:         "node-fetch",
+							Vulnerability: dbTypes.Vulnerability{
+								Title:       "foobar",
+								Description: "baz",
+								Severity:    "HIGH",
+							},
+							InstalledVersion: "1.7.3",
+							FixedVersion:     "2.6.7, 3.1.1",
+						},
+						{
+							VulnerabilityID: "CVE-2021-26539",
+							PkgID:           "sanitize-html@1.20.0",
+							PkgName:         "sanitize-html",
+							Vulnerability: dbTypes.Vulnerability{
+								Title:       "foobar",
+								Description: "baz",
+								Severity:    "MEDIUM",
+							},
+							InstalledVersion: "1.20.0",
+							FixedVersion:     "2.3.1",
+						},
+					},
+				},
+			},
+			expectedOutput: `
+package-lock.json (npm)
+=======================
+Total: 2 (MEDIUM: 1, HIGH: 1)
+
+┌───────────────┬────────────────┬──────────┬───────────────────┬───────────────┬────────┐
+│    Library    │ Vulnerability  │ Severity │ Installed Version │ Fixed Version │ Title  │
+├───────────────┼────────────────┼──────────┼───────────────────┼───────────────┼────────┤
+│ node-fetch    │ CVE-2022-0235  │ HIGH     │ 1.7.3             │ 2.6.7, 3.1.1  │ foobar │
+├───────────────┼────────────────┼──────────┼───────────────────┼───────────────┤        │
+│ sanitize-html │ CVE-2021-26539 │ MEDIUM   │ 1.20.0            │ 2.3.1         │        │
+└───────────────┴────────────────┴──────────┴───────────────────┴───────────────┴────────┘
+
+Dependency Origin Tree (Reversed)
+=================================
+package-lock.json
+├── node-fetch@1.7.3, (MEDIUM: 0, HIGH: 1)
+│   └── ...(omitted)...
+│       └── styled-components@3.1.3
+└── sanitize-html@1.20.0, (MEDIUM: 1, HIGH: 0)
+`,
+		},
+		{
+			name: "happy path with vulnerability origin graph(no info about direct deps)",
+			results: types.Results{
+				{
+					Target: "package-lock.json",
+					Class:  types.ClassLangPkg,
+					Type:   "npm",
+					Packages: []ftypes.Package{
+						{
+							ID:       "node-fetch@1.7.3",
+							Name:     "node-fetch",
+							Version:  "1.7.3",
+							Indirect: true,
+						},
+						{
+							ID:       "isomorphic-fetch@2.2.1",
+							Name:     "isomorphic-fetch",
+							Version:  "2.2.1",
+							Indirect: true,
+							DependsOn: []string{
+								"node-fetch@1.7.3",
+							},
+						},
+						{
+							ID:       "fbjs@0.8.18",
+							Name:     "fbjs",
+							Version:  "0.8.18",
+							Indirect: true,
+							DependsOn: []string{
+								"isomorphic-fetch@2.2.1",
+							},
+						},
+						{
+							ID:       "sanitize-html@1.20.0",
+							Name:     "sanitize-html",
+							Version:  "1.20.0",
+							Indirect: true,
+						},
+						{
+							ID:       "styled-components@3.1.3",
+							Name:     "styled-components",
+							Version:  "3.1.3",
+							Indirect: true,
 							DependsOn: []string{
 								"fbjs@0.8.18",
 							},
