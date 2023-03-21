b247a894aacb91e19e20b84d54381c8bb92f6548
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2023-03-21 23:13:02 +0600

    feat(nodejs): parse package.json alongside yarn.lock (#3757)
    Co-authored-by: knqyf263 <knqyf263@gmail.com>

--- pkg/fanal/analyzer/language/nodejs/pkg/pkg.go
+++ pkg/fanal/analyzer/language/nodejs/pkg/pkg.go
@@ -8,6 +8,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/aquasecurity/go-dep-parser/pkg/nodejs/packagejson"
+	godeptypes "github.com/aquasecurity/go-dep-parser/pkg/types"
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer"
 	"github.com/aquasecurity/trivy/pkg/fanal/analyzer/language"
 	"github.com/aquasecurity/trivy/pkg/fanal/types"
@@ -27,13 +28,15 @@ type nodePkgLibraryAnalyzer struct{}
 // Analyze analyzes package.json for node packages
 func (a nodePkgLibraryAnalyzer) Analyze(_ context.Context, input analyzer.AnalysisInput) (*analyzer.AnalysisResult, error) {
 	p := packagejson.NewParser()
-	libs, deps, err := p.Parse(input.Content)
+	pkg, err := p.Parse(input.Content)
 	if err != nil {
 		return nil, xerrors.Errorf("unable to parse %s: %w", input.FilePath, err)
 	}
 
-	return language.ToAnalysisResult(types.NodePkg, input.FilePath, input.FilePath, libs, deps), nil
-
+	// package.json may contain version range in `dependencies` fields
+	// e.g.   "devDependencies": { "mocha": "^5.2.0", }
+	// so we get only information about project
+	return language.ToAnalysisResult(types.NodePkg, input.FilePath, input.FilePath, []godeptypes.Library{pkg.Library}, nil), nil
 }
 
 func (a nodePkgLibraryAnalyzer) Required(filePath string, _ os.FileInfo) bool {
