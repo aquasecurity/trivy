b1187ae6c7e6b4299ca6cd6714754104d0bd37c5
AndreyLevchenko <levchenko.andrey@gmail.com>
2023-03-24 10:39:52 +0600

    feat(rust): dependency tree and line numbers support for cargo lock file (go-dep-parser#179) (go-dep-parser#180)

--- pkg/rust/cargo/parse.go
+++ pkg/rust/cargo/parse.go
@@ -1,6 +1,9 @@
 package cargo
 
 import (
+	"io"
+	"strings"
+
 	"github.com/BurntSushi/toml"
 	dio "github.com/aquasecurity/go-dep-parser/pkg/io"
 	"github.com/aquasecurity/go-dep-parser/pkg/types"
@@ -9,14 +12,14 @@ import (
 	"golang.org/x/xerrors"
 )
 
+type cargoPkg struct {
+	Name         string   `toml:"name"`
+	Version      string   `toml:"version"`
+	Source       string   `toml:"source,omitempty"`
+	Dependencies []string `toml:"dependencies,omitempty"`
+}
 type Lockfile struct {
-	Packages []struct {
-		Name         string   `toml:"name"`
-		Version      string   `toml:"version"`
-		Source       string   `toml:"source,omitempty"`
-		Dependencies []string `toml:"dependencies,omitempty"`
-	} `toml:"package"`
-	Metadata interface{}
+	Packages []cargoPkg `toml:"package"`
 }
 
 type Parser struct{}
@@ -27,18 +30,62 @@ func NewParser() types.Parser {
 
 func (p *Parser) Parse(r dio.ReadSeekerAt) ([]types.Library, []types.Dependency, error) {
 	var lockfile Lockfile
-	if _, err := toml.DecodeReader(r, &lockfile); err != nil {
+	decoder := toml.NewDecoder(r)
+	if _, err := decoder.Decode(&lockfile); err != nil {
 		return nil, nil, xerrors.Errorf("decode error: %w", err)
 	}
 
+	if _, err := r.Seek(0, io.SeekStart); err != nil {
+		return nil, nil, xerrors.Errorf("seek error: %w", err)
+	}
+
+	// naive parser to get line numbers by package from lock file
+	pkgParser := naivePkgParser{r: r}
+	lineNumIdx := pkgParser.parse()
+
 	var libs []types.Library
+	var deps []types.Dependency
 	for _, pkg := range lockfile.Packages {
 		pkgID := utils.PackageID(pkg.Name, pkg.Version)
-		libs = append(libs, types.Library{
+		lib := types.Library{
 			ID:      pkgID,
 			Name:    pkg.Name,
 			Version: pkg.Version,
-		})
+		}
+		if pos, ok := lineNumIdx[pkgID]; ok {
+			lib.Locations = []types.Location{{StartLine: pos.start, EndLine: pos.end}}
+		}
+
+		libs = append(libs, lib)
+		dep := parseDependencies(pkgID, pkg)
+		if dep != nil {
+			deps = append(deps, *dep)
+		}
 	}
-	return libs, nil, nil //TODO add actual dependencies
+	return libs, deps, nil
+}
+func parseDependencies(pkgId string, pkg cargoPkg) *types.Dependency {
+	dependOn := []string{}
+	for _, pkgDep := range pkg.Dependencies {
+		/*
+			Dependency entries look like:
+			"unsafe-any 0.4.2 (registry+https://github.com/rust-lang/crates.io-index)"
+		*/
+
+		fields := strings.Fields(pkgDep)
+		if len(fields) != 3 {
+			continue
+		}
+		name := fields[0]
+		version := fields[1]
+		dependOn = append(dependOn, utils.PackageID(name, version))
+	}
+	if len(dependOn) > 0 {
+		return &types.Dependency{
+			ID:        pkgId,
+			DependsOn: dependOn,
+		}
+	} else {
+		return nil
+	}
 }
--- pkg/rust/cargo/parse_test.go
+++ pkg/rust/cargo/parse_test.go
@@ -1,6 +1,7 @@
 package cargo
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"sort"
@@ -13,22 +14,64 @@ import (
 	"github.com/aquasecurity/go-dep-parser/pkg/types"
 )
 
+var (
+	cargoNormalLibs = []types.Library{
+		{ID: "normal@0.1.0", Name: "normal", Version: "0.1.0", Locations: []types.Location{{StartLine: 8, EndLine: 13}}},
+		{ID: "libc@0.2.54", Name: "libc", Version: "0.2.54", Locations: []types.Location{{StartLine: 3, EndLine: 6}}},
+		{ID: "typemap@0.3.3", Name: "typemap", Version: "0.3.3", Locations: []types.Location{{StartLine: 20, EndLine: 26}}},
+		{ID: "url@1.7.2", Name: "url", Version: "1.7.2", Locations: []types.Location{{StartLine: 43, EndLine: 51}}},
+		{ID: "unsafe-any@0.4.2", Name: "unsafe-any", Version: "0.4.2", Locations: []types.Location{{StartLine: 15, EndLine: 18}}},
+		{ID: "matches@0.1.8", Name: "matches", Version: "0.1.8", Locations: []types.Location{{StartLine: 33, EndLine: 36}}},
+		{ID: "idna@0.1.5", Name: "idna", Version: "0.1.5", Locations: []types.Location{{StartLine: 28, EndLine: 31}}},
+		{ID: "percent-encoding@1.0.1", Name: "percent-encoding", Version: "1.0.1", Locations: []types.Location{{StartLine: 38, EndLine: 41}}},
+	}
+	cargoNormalDeps = []types.Dependency{
+		{
+			ID:        "normal@0.1.0",
+			DependsOn: []string{"libc@0.2.54"}},
+		{
+			ID:        "typemap@0.3.3",
+			DependsOn: []string{"unsafe-any@0.4.2"},
+		},
+		{
+			ID:        "url@1.7.2",
+			DependsOn: []string{"idna@0.1.5", "matches@0.1.8", "percent-encoding@1.0.1"},
+		},
+	}
+	cargoMixedLibs = []types.Library{
+		{ID: "normal@0.1.0", Name: "normal", Version: "0.1.0", Locations: []types.Location{{StartLine: 17, EndLine: 22}}},
+		{ID: "libc@0.2.54", Name: "libc", Version: "0.2.54", Locations: []types.Location{{StartLine: 3, EndLine: 6}}},
+		{ID: "typemap@0.3.3", Name: "typemap", Version: "0.3.3", Locations: []types.Location{{StartLine: 55, EndLine: 61}}},
+		{ID: "url@1.7.2", Name: "url", Version: "1.7.2", Locations: []types.Location{{StartLine: 26, EndLine: 34}}},
+		{ID: "unsafe-any@0.4.2", Name: "unsafe-any", Version: "0.4.2", Locations: []types.Location{{StartLine: 9, EndLine: 12}}},
+		{ID: "matches@0.1.8", Name: "matches", Version: "0.1.8", Locations: []types.Location{{StartLine: 41, EndLine: 44}}},
+		{ID: "idna@0.1.5", Name: "idna", Version: "0.1.5", Locations: []types.Location{{StartLine: 36, EndLine: 39}}},
+		{ID: "percent-encoding@1.0.1", Name: "percent-encoding", Version: "1.0.1", Locations: []types.Location{{StartLine: 46, EndLine: 49}}},
+	}
+)
+
 func TestParse(t *testing.T) {
 	vectors := []struct {
-		file string // Test input file
-		want []types.Library
+		file     string // Test input file
+		wantLibs []types.Library
+		wantDeps []types.Dependency
+		wantErr  assert.ErrorAssertionFunc
 	}{
 		{
-			file: "testdata/cargo_normal.lock",
-			want: cargoNormal,
+			file:     "testdata/cargo_normal.lock",
+			wantLibs: cargoNormalLibs,
+			wantDeps: cargoNormalDeps,
+			wantErr:  assert.NoError,
 		},
 		{
-			file: "testdata/cargo_many.lock",
-			want: cargoMany,
+			file:     "testdata/cargo_mixed.lock",
+			wantLibs: cargoMixedLibs,
+			wantDeps: cargoNormalDeps,
+			wantErr:  assert.NoError,
 		},
 		{
-			file: "testdata/cargo_nickel.lock",
-			want: cargoNickel,
+			file:    "testdata/cargo_invalid.lock",
+			wantErr: assert.Error,
 		},
 	}
 
@@ -37,27 +80,36 @@ func TestParse(t *testing.T) {
 			f, err := os.Open(v.file)
 			require.NoError(t, err)
 
-			got, _, err := NewParser().Parse(f)
+			libs, deps, err := NewParser().Parse(f)
 
-			require.NoError(t, err)
+			if !v.wantErr(t, err, fmt.Sprintf("Parse(%v)", v.file)) {
+				return
+			}
 
-			sort.Slice(got, func(i, j int) bool {
-				ret := strings.Compare(got[i].Name, got[j].Name)
-				if ret == 0 {
-					return got[i].Version < got[j].Version
-				}
-				return ret < 0
-			})
+			if err != nil {
+				return
+			}
 
-			sort.Slice(v.want, func(i, j int) bool {
-				ret := strings.Compare(v.want[i].Name, v.want[j].Name)
-				if ret == 0 {
-					return v.want[i].Version < v.want[j].Version
-				}
-				return ret < 0
-			})
+			sortLibs(libs)
+			sortLibs(v.wantLibs)
 
-			assert.Equal(t, v.want, got)
+			sortDeps(deps)
+			sortDeps(v.wantDeps)
+
+			assert.Equalf(t, v.wantLibs, libs, "Parse libraries(%v)", v.file)
+			assert.Equalf(t, v.wantDeps, deps, "Parse dependencies(%v)", v.file)
 		})
 	}
+}
+
+func sortLibs(libs []types.Library) {
+	sort.Slice(libs, func(i, j int) bool {
+		return strings.Compare(libs[i].ID, libs[j].ID) < 0
+	})
+}
+
+func sortDeps(deps []types.Dependency) {
+	sort.Slice(deps, func(i, j int) bool {
+		return strings.Compare(deps[i].ID, deps[j].ID) < 0
+	})
 }
