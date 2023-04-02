916eecd0f22348570aa0dd8b23c7929d6af28a71
knqyf263 <knqyf263@gmail.com>
2023-04-02 20:14:40 +0300

    test: rewrite scripts in Go

---
+++ magefiles/fixture.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/google/go-containerregistry/pkg/crane"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+)
+
+func fixtureContainerImages() error {
+	const (
+		testImages = "ghcr.io/aquasecurity/trivy-test-images"
+		dir        = "integration/testdata/fixtures/images/"
+	)
+	tags, err := crane.ListTags(testImages)
+	if err != nil {
+		return err
+	}
+	for _, tag := range tags {
+		fileName := tag + ".tar.gz"
+		filePath := filepath.Join(dir, fileName)
+		if exists(filePath) {
+			continue
+		}
+		fmt.Printf("Downloading %s...\n", tag)
+		img, err := crane.Pull(fmt.Sprintf("%s:%s", testImages, tag))
+		if err != nil {
+			return err
+		}
+		if err = crane.Save(img, tag, filePath); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func fixtureVMImages() error {
+	const (
+		testVMImages    = "ghcr.io/aquasecurity/trivy-test-vm-images"
+		titleAnnotation = "org.opencontainers.image.title"
+	)
+	tags, err := crane.ListTags(testVMImages)
+	if err != nil {
+		return err
+	}
+	for _, tag := range tags {
+		img, err := crane.Pull(fmt.Sprintf("%s:%s", testVMImages, tag))
+		if err != nil {
+			return err
+		}
+
+		manifest, err := img.Manifest()
+		if err != nil {
+			return err
+		}
+
+		layers, err := img.Layers()
+		if err != nil {
+			return err
+		}
+
+		for i, layer := range layers {
+			fileName, ok := manifest.Layers[i].Annotations[titleAnnotation]
+			if !ok {
+				continue
+			}
+			if err = saveLayer(layer, fileName); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func saveLayer(layer v1.Layer, fileName string) error {
+	const dir = "integration/testdata/fixtures/vm-images/"
+
+	filePath := filepath.Join(dir, fileName)
+	if exists(filePath) {
+		return nil
+	}
+	fmt.Printf("Downloading %s...\n", fileName)
+
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	c, err := layer.Compressed()
+	if err != nil {
+		return err
+	}
+	if _, err = io.Copy(f, c); err != nil {
+		return err
+	}
+	return nil
+}
