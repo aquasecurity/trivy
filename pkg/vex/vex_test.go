be51c1ec882720a7c3543a396925a81da6f0dc58
knqyf263 <knqyf263@gmail.com>
2023-04-13 15:15:38 +0300

    test(vex): add more tests

--- pkg/vex/vex_test.go
+++ pkg/vex/vex_test.go
@@ -1,13 +1,14 @@
 package vex
 
 import (
+	"testing"
+	"time"
+
 	cdx "github.com/CycloneDX/cyclonedx-go"
 	openvex "github.com/openvex/go-vex/pkg/vex"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
-	"time"
 )
 
 func TestOpen(t *testing.T) {
@@ -107,6 +108,158 @@ func TestOpen(t *testing.T) {
 			}
 			require.NoError(t, err)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestOpenVEX_Statement(t *testing.T) {
+	type fields struct {
+		VEX *openvex.VEX
+	}
+	type args struct {
+		vulnID string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   Statement
+	}{
+		{
+			name: "happy",
+			fields: fields{
+				VEX: &openvex.VEX{
+					Statements: []openvex.Statement{
+						{
+							Vulnerability: "CVE-2021-44228",
+							Products: []string{
+								"pkg:maven/org.springframework.boot/spring-boot@2.6.0",
+							},
+							Status:        openvex.StatusNotAffected,
+							Justification: "vulnerable_code_not_in_execute_path",
+						},
+					},
+				},
+			},
+			args: args{
+				vulnID: "CVE-2021-44228",
+			},
+			want: Statement{
+				VulnerabilityID: "CVE-2021-44228",
+				Affects:         []string{"pkg:maven/org.springframework.boot/spring-boot@2.6.0"},
+				Status:          StatusNotAffected,
+				Justification:   "vulnerable_code_not_in_execute_path",
+			},
+		},
+		{
+			name: "not match",
+			fields: fields{
+				VEX: &openvex.VEX{
+					Statements: []openvex.Statement{
+						{
+							Vulnerability: "CVE-2021-44228",
+							Products: []string{
+								"pkg:maven/org.springframework.boot/spring-boot@2.6.0",
+							},
+							Status:        openvex.StatusNotAffected,
+							Justification: "vulnerable_code_not_in_execute_path",
+						},
+					},
+				},
+			},
+			args: args{
+				vulnID: "CVE-3000-0001",
+			},
+			want: Statement{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := &OpenVEX{
+				VEX: tt.fields.VEX,
+			}
+			assert.Equal(t, tt.want, v.Statement(tt.args.vulnID))
+		})
+	}
+}
+
+func TestCycloneDX_Statement(t *testing.T) {
+	type fields struct {
+		BOM *cdx.BOM
+	}
+	type args struct {
+		vulnID string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   Statement
+	}{
+		{
+			name: "happy",
+			fields: fields{
+				BOM: &cdx.BOM{
+					Vulnerabilities: &[]cdx.Vulnerability{
+						{
+							ID: "CVE-2018-7489",
+							Analysis: &cdx.VulnerabilityAnalysis{
+								State:         "not_affected",
+								Justification: "code_not_reachable",
+							},
+							Affects: &[]cdx.Affects{
+								{
+									Ref: "urn:cdx:3e671687-395b-41f5-a30f-a58921a69b79/1#jackson-databind-2.8.0",
+								},
+							},
+						},
+					},
+				},
+			},
+			args: args{
+				vulnID: "CVE-2018-7489",
+			},
+			want: Statement{
+				VulnerabilityID: "CVE-2018-7489",
+				Affects:         []string{"urn:cdx:3e671687-395b-41f5-a30f-a58921a69b79/1#jackson-databind-2.8.0"},
+				Status:          StatusNotAffected,
+				Justification:   "code_not_reachable",
+			},
+		},
+		{
+			name: "not match",
+			fields: fields{
+				BOM: &cdx.BOM{
+					Vulnerabilities: &[]cdx.Vulnerability{
+						{
+							ID: "CVE-2018-7489",
+							Analysis: &cdx.VulnerabilityAnalysis{
+								State:         "not_affected",
+								Justification: "code_not_reachable",
+							},
+							Affects: &[]cdx.Affects{
+								{
+									Ref: "urn:cdx:3e671687-395b-41f5-a30f-a58921a69b79/1#jackson-databind-2.8.0",
+								},
+							},
+						},
+					},
+				},
+			},
+			args: args{
+				vulnID: "CVE-3000-0001",
+			},
+			want: Statement{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := &CycloneDX{
+				BOM: tt.fields.BOM,
+			}
+			assert.Equal(t, tt.want, v.Statement(tt.args.vulnID))
 		})
 	}
 }
