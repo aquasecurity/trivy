524875df04e75213e43589aede4309d653c1475f
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-05-18 15:30:41 +0600

    update testcases

--- pkg/fanal/analyzer/pkg/rpm/rpm.go
+++ pkg/fanal/analyzer/pkg/rpm/rpm.go
@@ -131,6 +131,13 @@ func (a rpmPkgAnalyzer) parsePkgInfo(rc io.Reader) (types.Packages, []string, er
 			}
 		}
 
+		// RPM DB uses MD5 digest
+		// https://rpm-software-management.github.io/rpm/manual/tags.html#signatures-and-digests
+		var d digest.Digest
+		if pkg.SigMD5 != "" {
+			d = digest.NewDigestFromString(digest.MD5, pkg.SigMD5)
+		}
+
 		p := types.Package{
 			ID:              fmt.Sprintf("%s@%s-%s.%s", pkg.Name, pkg.Version, pkg.Release, pkg.Arch),
 			Name:            pkg.Name,
@@ -146,7 +153,7 @@ func (a rpmPkgAnalyzer) parsePkgInfo(rc io.Reader) (types.Packages, []string, er
 			Licenses:        []string{pkg.License},
 			DependsOn:       pkg.Requires, // Will be replaced with package IDs
 			Maintainer:      pkg.Vendor,
-			Digest:          digest.NewDigestFromString(digest.MD5, pkg.PackageDigest),
+			Digest:          d,
 		}
 		pkgs = append(pkgs, p)
 		installedFiles = append(installedFiles, files...)
