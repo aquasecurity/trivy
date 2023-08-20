d442176405260caa04fce9ac093a62c52ab361aa
Nikita Pivkin <nikita.pivkin@smartforce.io>
2023-08-20 13:12:31 +0700

    feat(server): add version endpoint (#4869)
    * feat(server): add version endpoint
    
    * fix panic and test
    
    * move version.go
    
    * move version variable
    
    * add docs about endpoints
    
    * move testdata
    
    * refactor
    
    * update build command
    
    * refactor

--- magefiles/docs.go
+++ magefiles/docs.go
@@ -12,17 +12,13 @@ import (
 
 // Generate CLI references
 func main() {
-	ver, err := version()
-	if err != nil {
-		log.Fatal(err)
-	}
 	// Set a dummy path for the documents
 	flag.CacheDirFlag.Default = "/path/to/cache"
 	flag.ModuleDirFlag.Default = "$HOME/.trivy/modules"
 
-	cmd := commands.NewApp(ver)
+	cmd := commands.NewApp()
 	cmd.DisableAutoGenTag = true
-	if err = doc.GenMarkdownTree(cmd, "./docs/docs/references/configuration/cli"); err != nil {
+	if err := doc.GenMarkdownTree(cmd, "./docs/docs/references/configuration/cli"); err != nil {
 		log.Fatal(err)
 	}
 }
