7d6429c852df5a5ed2fdfcdeee3b81d038fda642
Itay Shakury <itay@itaysk.com>
2023-09-27 17:45:27 +0300

    chore: auto label discussions

---
+++ .github/actions/trivy-triage/test/labelDiscussion.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# add a label to a discussion
+# requires authenticated gh cli, assumes repo but current git repository
+# args:
+#   $1: discussion ID (not number!), e.g DIC_kwDOE0GiPM4CXnDc, required
+#   $2: label ID, e.g. MDU6TGFiZWwzNjIzNjY0MjQ=, required
+# TODO
