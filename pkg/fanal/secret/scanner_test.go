e1c6af40a462b681aa2e245aaf348db7eace264f
Rachid Azgaou <rachid.azgaou_ext@sonepar.com>
2023-09-29 18:32:31 +0200

    revert spaces

--- pkg/fanal/secret/scanner_test.go
+++ pkg/fanal/secret/scanner_test.go
@@ -809,7 +809,7 @@ func TestSecretScanner(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {	
+		t.Run(tt.name, func(t *testing.T) {
 			content, err := os.ReadFile(tt.inputFilePath)
 			require.NoError(t, err)
 
