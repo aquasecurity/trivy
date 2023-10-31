d0d3315894935873657ef10a5568353b85a5b66e
very-doge-wow <95224950+very-doge-wow@users.noreply.github.com>
2023-10-31 14:08:06 +0100

    refactor(secret): more specific rule for JWT tokens
    As used by gitleaks: https://github.com/gitleaks/gitleaks/blob/master/cmd/generate/config/rules/jwt.go

--- pkg/fanal/secret/builtin-rules.go
+++ pkg/fanal/secret/builtin-rules.go
@@ -567,7 +567,7 @@ var builtinRules = []Rule{
 		ID:       "jwt-token",
 		Category: CategoryJWT,
 		Title:    "JWT token",
-		Regex:    MustCompile(`^([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_\-\+\/=]*)`),
+		Regex:    MustCompile(`ey[a-zA-Z0-9]{17,}\.ey[a-zA-Z0-9\/\\_-]{17,}\.(?:[a-zA-Z0-9\/\\_-]{10,}={0,2})?`),
 		Keywords: []string{"jwt"},
 	},
 	{
