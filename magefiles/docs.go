1daca55df39c545d8bd4124c2157b5e91a05d578
knqyf263 <knqyf263@gmail.com>
2023-11-14 13:45:12 +0900

    chore: not load plugins for auto-generating docs
    Signed-off-by: knqyf263 <knqyf263@gmail.com>

--- magefiles/docs.go
+++ magefiles/docs.go
@@ -16,6 +16,9 @@ func main() {
 	flag.CacheDirFlag.Default = "/path/to/cache"
 	flag.ModuleDirFlag.Default = "$HOME/.trivy/modules"
 
+	// Set a dummy path not to load plugins
+	os.Setenv("XDG_DATA_HOME", os.TempDir())
+
 	cmd := commands.NewApp()
 	cmd.DisableAutoGenTag = true
 	if err := doc.GenMarkdownTree(cmd, "./docs/docs/references/configuration/cli"); err != nil {
