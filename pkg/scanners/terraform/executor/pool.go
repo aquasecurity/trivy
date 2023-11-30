e0ddc7187e729ce522df057966d4ddc46d759902
Simar <simar@linux.com>
2023-11-29 21:00:59 -0700

    fix imports

--- pkg/scanners/terraform/executor/pool.go
+++ pkg/scanners/terraform/executor/pool.go
@@ -13,7 +13,8 @@ import (
 	"github.com/aquasecurity/defsec/pkg/scan"
 	"github.com/aquasecurity/defsec/pkg/state"
 	"github.com/aquasecurity/defsec/pkg/terraform"
-	"github.com/aquasecurity/trivy/pkg/types"
+	//"github.com/aquasecurity/trivy/pkg/types"
+	types "github.com/aquasecurity/defsec/pkg/types/rules"
 )
 
 type Pool struct {
