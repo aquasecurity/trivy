24a8aff15747520e3803a68a1b8c674781178dc7
DmitriyLewen <dmitriy.lewen@smartforce.io>
2024-01-16 12:22:05 +0600

    fix(amazon): save system files for pkgs containing `amzn` in src

--- pkg/fanal/analyzer/pkg/rpm/rpm.go
+++ pkg/fanal/analyzer/pkg/rpm/rpm.go
@@ -133,7 +133,7 @@ func (a rpmPkgAnalyzer) listPkgs(db RPMDB) (types.Packages, []string, error) {
 		// Check if the package is vendor-provided.
 		// If the package is not provided by vendor, the installed files should not be skipped.
 		var files []string
-		if packageProvidedByVendor(pkg.Vendor) {
+		if packageProvidedByVendor(pkg.Vendor, srcRel) {
 			files, err = pkg.InstalledFileNames()
 			if err != nil {
 				return nil, nil, xerrors.Errorf("unable to get installed files: %w", err)
@@ -235,13 +235,17 @@ func splitFileName(filename string) (name, ver, rel string, err error) {
 	return name, ver, rel, nil
 }
 
-func packageProvidedByVendor(pkgVendor string) bool {
-	for _, vendor := range osVendors {
-		if strings.HasPrefix(pkgVendor, vendor) {
-			return true
+func packageProvidedByVendor(pkgVendor, srcRelease string) bool {
+	if pkgVendor != "" {
+		for _, vendor := range osVendors {
+			if strings.HasPrefix(pkgVendor, vendor) {
+				return true
+			}
 		}
 	}
-	return false
+	// Official Amazon packages may not contain `Vendor` field:
+	// https://github.com/aquasecurity/trivy/issues/5887
+	return strings.Contains(srcRelease, "amzn")
 }
 
 func writeToTempFile(rc io.Reader) (string, error) {
