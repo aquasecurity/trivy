2b5df0341efbeac49944d825083e4db28d66453a
adam.carruthers1 <adam.carruthers1@nhs.net>
2024-02-22 14:59:18 +0000

    Write table test

--- pkg/iac/scanners/terraform/parser/resolvers/registry_test.go
+++ pkg/iac/scanners/terraform/parser/resolvers/registry_test.go
@@ -6,45 +6,51 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_getPrivateRegistryTokenFromEnvVars(t *testing.T) {
-	t.Run("returns empty string when no env var set", func(t *testing.T) {
-		token, err := getPrivateRegistryTokenFromEnvVars("registry.example.com")
-		assert.Equal(t, "", token)
-		assert.Equal(t, "No token was found for the registry at registry.example.com", err.Error())
-	})
+func Test_getPrivateRegistryTokenFromEnvVars_ErrorsWithNoEnvVarSet(t *testing.T) {
+	token, err := getPrivateRegistryTokenFromEnvVars("registry.example.com")
+	assert.Equal(t, "", token)
+	assert.Equal(t, "no token was found for the registry at registry.example.com", err.Error())
+}
 
-	t.Run("returns string when simple env var set", func(t *testing.T) {
-		t.Setenv("TF_TOKEN_registry_example_com", "abcd")
-		token, err := getPrivateRegistryTokenFromEnvVars("registry.example.com")
-		assert.Equal(t, "abcd", token)
-		assert.Equal(t, nil, err)
-	})
+func Test_getPrivateRegistryTokenFromEnvVars_ConvertsSiteNameToEnvVar(t *testing.T) {
+	tests := []struct {
+		name      string
+		siteName  string
+		tokenName string
+	}{
+		{
+			name:      "returns string when simple env var set",
+			siteName:  "registry.example.com",
+			tokenName: "TF_TOKEN_registry_example_com",
+		},
+		{
+			name:      "allows dashes in hostname to be dashes",
+			siteName:  "my-registry.example.com",
+			tokenName: "TF_TOKEN_my-registry_example_com",
+		},
+		{
+			name:      "allows dashes in hostname to be double underscores",
+			siteName:  "my-registry.example.com",
+			tokenName: "TF_TOKEN_my__registry_example_com",
+		},
+		{
+			name:      "handles utf8 to punycode correctly",
+			siteName:  "例えば.com",
+			tokenName: "TF_TOKEN_xn--r8j3dr99h_com",
+		},
+		{
+			name:      "handles punycode with dash to underscore conversion",
+			siteName:  "café.fr",
+			tokenName: "TF_TOKEN_xn____caf__dma_fr",
+		},
+	}
 
-	t.Run("allows dashes in hostname to be dashes", func(t *testing.T) {
-		t.Setenv("TF_TOKEN_my-registry_example_com", "1111")
-		token, err := getPrivateRegistryTokenFromEnvVars("my-registry.example.com")
-		assert.Equal(t, "1111", token)
-		assert.Equal(t, nil, err)
-	})
-
-	t.Run("allows dashes in hostname to be double underscores", func(t *testing.T) {
-		t.Setenv("TF_TOKEN_my__registry_example_com", "1234")
-		token, err := getPrivateRegistryTokenFromEnvVars("my-registry.example.com")
-		assert.Equal(t, "1234", token)
-		assert.Equal(t, nil, err)
-	})
-
-	t.Run("handles utf8 to punycode correctly", func(t *testing.T) {
-		t.Setenv("TF_TOKEN_xn--r8j3dr99h_com", "9999")
-		token, err := getPrivateRegistryTokenFromEnvVars("例えば.com")
-		assert.Equal(t, "9999", token)
-		assert.Equal(t, nil, err)
-	})
-
-	t.Run("handles punycode with dash to underscore conversion", func(t *testing.T) {
-		t.Setenv("TF_TOKEN_xn____caf__dma_fr", "9875")
-		token, err := getPrivateRegistryTokenFromEnvVars("café.fr")
-		assert.Equal(t, "9875", token)
-		assert.Equal(t, nil, err)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(tt.tokenName, "abcd")
+			token, err := getPrivateRegistryTokenFromEnvVars(tt.siteName)
+			assert.Equal(t, "abcd", token)
+			assert.Equal(t, nil, err)
+		})
+	}
 }
