4e0f136236c4589a934a92da5be4c88d365ef997
chenk <hen.keinan@gmail.com>
2024-03-14 14:11:04 +0200

    feat: support exclude kinds/namespaces and include kinds/namespaces
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/flag/kubernetes_flags.go
+++ pkg/flag/kubernetes_flags.go
@@ -74,6 +74,27 @@ var (
 		ConfigName: "kubernetes.exclude.nodes",
 		Usage:      "indicate the node labels that the node-collector job should exclude from scanning (example: kubernetes.io/arch:arm64,team:dev)",
 	}
+
+	ExcludeKinds = Flag[[]string]{
+		Name:       "exclude-kinds",
+		ConfigName: "kubernetes.exclude.kinds",
+		Usage:      "indicate the kinds exclude from scanning (example: node)",
+	}
+	IncludeKinds = Flag[[]string]{
+		Name:       "include-kinds",
+		ConfigName: "kubernetes.include.kinds",
+		Usage:      "indicate the kinds included in scanning (example: node)",
+	}
+	ExcludeNamespaces = Flag[[]string]{
+		Name:       "exclude-namespaces",
+		ConfigName: "kubernetes.exclude.namespaces",
+		Usage:      "indicate the namespaces excluded from scanning (example: kube-system)",
+	}
+	IncludeNamespaces = Flag[[]string]{
+		Name:       "include-namespaces",
+		ConfigName: "kubernetes.include.namespaces",
+		Usage:      "indicate the namespaces included in scanning (example: kube-system)",
+	}
 	NodeCollectorImageRef = Flag[string]{
 		Name:       "node-collector-imageref",
 		ConfigName: "kubernetes.node.collector.imageref",
@@ -106,6 +127,10 @@ type K8sFlagGroup struct {
 	NodeCollectorNamespace *Flag[string]
 	ExcludeOwned           *Flag[bool]
 	ExcludeNodes           *Flag[[]string]
+	ExcludeKinds           *Flag[[]string]
+	IncludeKinds           *Flag[[]string]
+	ExcludeNamespaces      *Flag[[]string]
+	IncludeNamespaces      *Flag[[]string]
 	QPS                    *Flag[float64]
 	Burst                  *Flag[int]
 }
@@ -122,6 +147,10 @@ type K8sOptions struct {
 	NodeCollectorNamespace string
 	ExcludeOwned           bool
 	ExcludeNodes           map[string]string
+	ExcludeKinds           []string
+	IncludeKinds           []string
+	ExcludeNamespaces      []string
+	IncludeNamespaces      []string
 	QPS                    float32
 	Burst                  int
 }
@@ -138,6 +167,10 @@ func NewK8sFlagGroup() *K8sFlagGroup {
 		NodeCollectorNamespace: NodeCollectorNamespace.Clone(),
 		ExcludeOwned:           ExcludeOwned.Clone(),
 		ExcludeNodes:           ExcludeNodes.Clone(),
+		ExcludeKinds:           ExcludeKinds.Clone(),
+		IncludeKinds:           IncludeKinds.Clone(),
+		ExcludeNamespaces:      ExcludeNamespaces.Clone(),
+		IncludeNamespaces:      IncludeNamespaces.Clone(),
 		NodeCollectorImageRef:  NodeCollectorImageRef.Clone(),
 		QPS:                    QPS.Clone(),
 		Burst:                  Burst.Clone(),
@@ -161,6 +194,10 @@ func (f *K8sFlagGroup) Flags() []Flagger {
 		f.ExcludeOwned,
 		f.ExcludeNodes,
 		f.NodeCollectorImageRef,
+		f.ExcludeKinds,
+		f.IncludeKinds,
+		f.ExcludeNamespaces,
+		f.IncludeNamespaces,
 		f.QPS,
 		f.Burst,
 	}
@@ -185,6 +222,12 @@ func (f *K8sFlagGroup) ToOptions() (K8sOptions, error) {
 		}
 		exludeNodeLabels[excludeNodeParts[0]] = excludeNodeParts[1]
 	}
+	if len(f.ExcludeNamespaces.Value()) > 0 && len(f.IncludeNamespaces.Value()) > 0 {
+		return K8sOptions{}, fmt.Errorf("include-namespaces and exclude-namespaces flags cannot be used together")
+	}
+	if len(f.ExcludeKinds.Value()) > 0 && len(f.IncludeKinds.Value()) > 0 {
+		return K8sOptions{}, fmt.Errorf("include-kinds and exclude-kinds flags cannot be used together")
+	}
 
 	return K8sOptions{
 		ClusterContext:         f.ClusterContext.Value(),
@@ -199,6 +242,10 @@ func (f *K8sFlagGroup) ToOptions() (K8sOptions, error) {
 		ExcludeNodes:           exludeNodeLabels,
 		NodeCollectorImageRef:  f.NodeCollectorImageRef.Value(),
 		QPS:                    float32(f.QPS.Value()),
+		ExcludeKinds:           f.ExcludeKinds.Value(),
+		IncludeKinds:           f.IncludeKinds.Value(),
+		ExcludeNamespaces:      f.ExcludeNamespaces.Value(),
+		IncludeNamespaces:      f.IncludeNamespaces.Value(),
 		Burst:                  f.Burst.Value(),
 	}, nil
 }
