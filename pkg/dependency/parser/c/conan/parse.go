7dae00b6c1ff4a062163671a58335e1d01a42cd9
Damian Ellwart <d.ellwart@falconvsystems.com>
2024-03-20 11:53:16 +0100

    feat(vuln): handle scanning conan v2.x lockfiles

--- pkg/dependency/parser/c/conan/parse.go
+++ pkg/dependency/parser/c/conan/parse.go
@@ -15,7 +15,7 @@ import (
 	xio "github.com/aquasecurity/trivy/pkg/x/io"
 )
 
-type LockFile struct {
+type LockFileV1 struct {
 	GraphLock GraphLock `json:"graph_lock"`
 }
 
@@ -30,9 +30,14 @@ type Node struct {
 	EndLine   int
 }
 
+type LockFileV2 struct {
+	Requires []string `json:"requires"`
+}
+
 type Parser struct {
 	logger *log.Logger
 }
+
 
 func NewParser() types.Parser {
 	return &Parser{
@@ -40,17 +45,9 @@ func NewParser() types.Parser {
 	}
 }
 
-func (p *Parser) Parse(r xio.ReadSeekerAt) ([]types.Library, []types.Dependency, error) {
-	var lock LockFile
-	input, err := io.ReadAll(r)
-	if err != nil {
-		return nil, nil, xerrors.Errorf("failed to read canon lock file: %w", err)
-	}
-	if err := jfather.Unmarshal(input, &lock); err != nil {
-		return nil, nil, xerrors.Errorf("failed to decode canon lock file: %w", err)
-	}
-
-	// Get a list of direct dependencies
+func (p *Parser) parseRequirementsV1(lock LockFileV1) ([]types.Library, []types.Dependency, error) {
+	var libs []types.Library
+	var deps []types.Dependency
 	var directDeps []string
 	if root, ok := lock.GraphLock.Nodes["0"]; ok {
 		directDeps = root.Requires
@@ -62,7 +59,7 @@ func (p *Parser) Parse(r xio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 		if node.Ref == "" {
 			continue
 		}
-		lib, err := parseRef(node)
+		lib, err := parseRefV1(node)
 		if err != nil {
 			p.logger.Debug("Parse ref error", log.Err(err))
 			continue
@@ -76,8 +73,6 @@ func (p *Parser) Parse(r xio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 	}
 
 	// Parse dependency graph
-	var libs []types.Library
-	var deps []types.Dependency
 	for i, node := range lock.GraphLock.Nodes {
 		lib, ok := parsed[i]
 		if !ok {
@@ -102,27 +97,82 @@ func (p *Parser) Parse(r xio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 	return libs, deps, nil
 }
 
-func parseRef(node Node) (types.Library, error) {
+func (p *Parser) parseRequirementsV2(lock LockFileV2) ([]types.Library, []types.Dependency, error) {
+	var libs []types.Library
+
+	for _, req := range lock.Requires {
+		lib, _ := parseRefV2(req)
+		libs = append(libs, lib)
+	}
+	return libs, []types.Dependency{}, nil
+}
+
+func (p *Parser) Parse(r xio.ReadSeekerAt) ([]types.Library, []types.Dependency, error) {
+	var lockV1 LockFileV1
+	var lockV2 LockFileV2
+	input, err := io.ReadAll(r)
+	if err != nil {
+		return nil, nil, xerrors.Errorf("failed to read conan lock file: %w", err)
+	}
+
+	// try to parse requirements as conan v1.x
+	if err := jfather.Unmarshal(input, &lockV1); err != nil {
+		return nil, nil, xerrors.Errorf("failed to decode conan lock file: %w", err)
+	}
+	if lockV1.GraphLock.Nodes != nil {
+		log.Logger.Debug("Handling conan lockfile as v1.x")
+		return p.parseRequirementsV1(lockV1)
+	} else {
+		// try to parse requirements as conan v2.x
+		log.Logger.Debug("Handling conan lockfile as v2.x")
+		if err := jfather.Unmarshal(input, &lockV2); err != nil {
+			return nil, nil, xerrors.Errorf("failed to decode conan lock file: %w", err)
+		}
+		return p.parseRequirementsV2(lockV2)
+	}
+}
+
+func parsePackage(text string) (string, string, error) {
 	// full ref format: package/version@user/channel#rrev:package_id#prev
 	// various examples:
 	// 'pkga/0.1@user/testing'
 	// 'pkgb/0.1.0'
 	// 'pkgc/system'
 	// 'pkgd/0.1.0#7dcb50c43a5a50d984c2e8fa5898bf18'
-	ss := strings.Split(strings.Split(strings.Split(node.Ref, "@")[0], "#")[0], "/")
+	ss := strings.Split(strings.Split(strings.Split(text, "@")[0], "#")[0], "/")
 	if len(ss) != 2 {
-		return types.Library{}, xerrors.Errorf("Unable to determine conan dependency: %q", node.Ref)
+		return "", "", xerrors.Errorf("Unable to determine conan dependency: %q", text)
+	}
+	return ss[0], ss[1], nil
+}
+
+func parseRefV1(node Node) (types.Library, error) {
+	name, version, err := parsePackage(node.Ref)
+	if err != nil {
+		return types.Library{}, err
 	}
 	return types.Library{
-		ID:      dependency.ID(ftypes.Conan, ss[0], ss[1]),
-		Name:    ss[0],
-		Version: ss[1],
+		ID:      dependency.ID(ftypes.Conan, name, version),
+		Name:    name,
+		Version: version,
 		Locations: []types.Location{
 			{
 				StartLine: node.StartLine,
 				EndLine:   node.EndLine,
 			},
 		},
+	}, nil
+}
+
+func parseRefV2(req string) (types.Library, error) {
+	name, version, err := parsePackage(req)
+	if err != nil {
+		return types.Library{}, err
+	}
+	return types.Library{
+		ID:      dependency.ID(ftypes.Conan, name, version),
+		Name:    name,
+		Version: version,
 	}, nil
 }
 
