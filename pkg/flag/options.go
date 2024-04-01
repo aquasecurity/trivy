07e978359107f0be95f0b700b94768e7beefc1e3
chenk <hen.keinan@gmail.com>
2024-04-01 09:09:49 +0300

    feat: k8s intrusive flag support
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/flag/options.go
+++ pkg/flag/options.go
@@ -360,13 +360,8 @@ func (o *Options) Align() {
 	}
 
 	// Vulnerability scanning is disabled by default for CycloneDX.
-	if o.Format == types.FormatCycloneDX && !viper.IsSet(ScannersFlag.ConfigName) { // remove K8sOptions.Components validation check when vuln scan is supported for k8s report with cycloneDX
+	if o.Format == types.FormatCycloneDX && !viper.IsSet(ScannersFlag.ConfigName) {
 		log.Logger.Info(`"--format cyclonedx" disables security scanning. Specify "--scanners vuln" explicitly if you want to include vulnerabilities in the CycloneDX report.`)
-		o.Scanners = nil
-	}
-
-	if o.Format == types.FormatCycloneDX {
-		log.Logger.Info(`"k8s with --format cyclonedx" disable security scanning`)
 		o.Scanners = nil
 	}
 }
