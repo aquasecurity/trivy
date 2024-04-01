36bcbeac659d6542d4dd8685feabe69ba68f4671
nikpivkin <nikita.pivkin@smartforce.io>
2024-04-01 23:31:27 +0700

    feat(cloudformation): add support for logging and endpoint access for EKS

--- pkg/iac/adapters/cloudformation/aws/eks/eks_test.go
+++ pkg/iac/adapters/cloudformation/aws/eks/eks_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aquasecurity/trivy/pkg/iac/adapters/cloudformation/testutil"
 	"github.com/aquasecurity/trivy/pkg/iac/providers/aws/eks"
+	"github.com/aquasecurity/trivy/pkg/iac/types"
 )
 
 func TestAdapt(t *testing.T) {
@@ -19,9 +20,44 @@ func TestAdapt(t *testing.T) {
 Resources:
   EKSCluster:
       Type: AWS::EKS::Cluster
+      Properties:
+        Logging:
+          ClusterLogging:
+            EnabledTypes:
+              - Type: api
+              - Type: audit
+              - Type: authenticator
+              - Type: controllerManager
+              - Type: scheduler
+        EncryptionConfig:
+          - Provider:
+              KeyArn: alias/mykey
+            Resources: [secrets]
+        ResourcesVpcConfig:
+          EndpointPublicAccess: True
+          PublicAccessCidrs:
+            - 0.0.0.0/0
 `,
 			expected: eks.EKS{
-				Clusters: []eks.Cluster{{}},
+				Clusters: []eks.Cluster{
+					{
+						Logging: eks.Logging{
+							API:               types.BoolTest(true),
+							Audit:             types.BoolTest(true),
+							Authenticator:     types.BoolTest(true),
+							ControllerManager: types.BoolTest(true),
+							Scheduler:         types.BoolTest(true),
+						},
+						Encryption: eks.Encryption{
+							KMSKeyID: types.StringTest("alias/mykey"),
+							Secrets:  types.BoolTest(true),
+						},
+						PublicAccessEnabled: types.BoolTest(true),
+						PublicAccessCIDRs: []types.StringValue{
+							types.StringTest("0.0.0.0/0"),
+						},
+					},
+				},
 			},
 		},
 		{
