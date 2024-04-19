68624ea14514e503f95d2a91a0629c58981ab452
Levente Kovacs <levente.kovacs@real-digital.de>
2024-04-19 12:34:03 +0200

    Add parse test for Python binary

---
+++ pkg/dependency/parser/python/binary/parse_test.go
@@ -0,0 +1,72 @@
+package pythonbinary
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/go-dep-parser/pkg/types"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputFile string
+		want      []types.Library
+		wantErr   string
+	}{
+		{
+			name:      "ELF2.7",
+			inputFile: "testdata/python2.7.elf",
+			want: []types.Library{
+				{
+					Name:    "python",
+					Version: "2.7.18",
+				},
+			},
+		},
+		{
+			name:      "ELF3.9",
+			inputFile: "testdata/python3.9.elf",
+			want: []types.Library{
+				{
+					Name:    "python",
+					Version: "3.9.19",
+				},
+			},
+		},
+		{
+			name:      "ELF3.10",
+			inputFile: "testdata/python3.10.elf",
+			want: []types.Library{
+				{
+					Name:    "python",
+					Version: "3.10.12",
+				},
+			},
+		},
+		{
+			name:      "sad path",
+			inputFile: "testdata/dummy",
+			wantErr:   "unrecognized executable format",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f, err := os.Open(tt.inputFile)
+			require.NoError(t, err)
+
+			got, err := Parse(f)
+			if tt.wantErr != "" {
+				require.NotNil(t, err)
+				assert.Contains(t, err.Error(), tt.wantErr)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
