a708e38243654c020bf727cf59cf9f12ce1a0f2d
Levente Kovacs <levente.kovacs@real-digital.de>
2024-04-19 12:19:08 +0200

    Add binary version parsing for java, nodejs, php and python

---
+++ pkg/dependency/parser/java/binary/parse_test.go
@@ -0,0 +1,62 @@
+package javabinary
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/go-dep-parser/pkg/types"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputFile string
+		want      []types.Library
+		wantErr   string
+	}{
+		{
+			name:      "ELF",
+			inputFile: "testdata/java.11.elf",
+			want: []types.Library{
+				{
+					Name:    "openjdk",
+					Version: "11.0.10+9",
+				},
+			},
+		},
+		{
+			name:      "ELF",
+			inputFile: "testdata/java.8.elf",
+			want: []types.Library{
+				{
+					Name:    "openjdk",
+					Version: "1.8.0_212-b04",
+				},
+			},
+		},
+		{
+			name:      "sad path",
+			inputFile: "testdata/dummy",
+			wantErr:   "unrecognized executable format",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f, err := os.Open(tt.inputFile)
+			require.NoError(t, err)
+
+			got, err := Parse(f)
+			if tt.wantErr != "" {
+				require.NotNil(t, err)
+				assert.Contains(t, err.Error(), tt.wantErr)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
---
+++ pkg/dependency/parser/nodejs/binary/parse.go
@@ -0,0 +1,55 @@
+// Ported from https://github.com/golang/go/blob/e9c96835971044aa4ace37c7787de231bbde05d9/src/cmd/go/internal/version/version.go
+
+package nodejsbinary
+
+import (
+	"bytes"
+	"io"
+	"regexp"
+
+	"github.com/aquasecurity/go-dep-parser/pkg/types"
+)
+
+// Parse scans file to try to report the NodeJS version.
+func Parse(r io.Reader) ([]types.Library, error) {
+	x, err := openExe(r)
+	if err != nil {
+		return nil, err
+	}
+
+	vers, mod := findVers(x)
+	if vers == "" {
+		return nil, nil
+	}
+
+	var libs []types.Library
+	libs = append(libs, types.Library{
+		Name:    mod,
+		Version: vers,
+	})
+
+	return libs, nil
+}
+
+// findVers finds and returns the NodeJS version in the executable x.
+func findVers(x exe) (vers string, mod string) {
+	text, size := x.DataStart()
+	data, err := x.ReadData(text, size)
+	if err != nil {
+		return
+	}
+
+	re := regexp.MustCompile(`node\.js\/v(\d{1,3}\.\d{1,3}\.\d{1,3})`)
+	// split by null characters
+	items := bytes.Split(data, []byte("\000"))
+	for _, s := range items {
+		// Extract the version number
+		match := re.FindSubmatch(s)
+		if match != nil {
+			vers = string(match[1])
+			break
+		}
+	}
+
+	return vers, "node.js"
+}
