0bce2d72953f32cb57b0f2e0bf95002d5a8a3120
knqyf263 <knqyf263@gmail.com>
2024-04-24 23:28:11 +0400

    feat(php/composr): add unknown relationship
    Signed-off-by: knqyf263 <knqyf263@gmail.com>

--- pkg/dependency/parser/php/composer/parse.go
+++ pkg/dependency/parser/php/composer/parse.go
@@ -52,11 +52,12 @@ func (p *Parser) Parse(r xio.ReadSeekerAt) ([]types.Library, []types.Dependency,
 	foundDeps := make(map[string][]string)
 	for _, pkg := range lockFile.Packages {
 		lib := types.Library{
-			ID:       dependency.ID(ftypes.Composer, pkg.Name, pkg.Version),
-			Name:     pkg.Name,
-			Version:  pkg.Version,
-			Indirect: false, // composer.lock file doesn't have info about Direct/Indirect deps. Will think that all dependencies are Direct
-			License:  strings.Join(pkg.License, ", "),
+			ID:           dependency.ID(ftypes.Composer, pkg.Name, pkg.Version),
+			Name:         pkg.Name,
+			Version:      pkg.Version,
+			Indirect:     false, // composer.lock file doesn't have info about Direct/Indirect deps. Will think that all dependencies are Direct
+			Relationship: types.RelationshipUnknown,
+			License:      strings.Join(pkg.License, ", "),
 			Locations: []types.Location{
 				{
 					StartLine: pkg.StartLine,
