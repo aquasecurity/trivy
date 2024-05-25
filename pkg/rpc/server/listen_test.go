3a95359e63d13eb64ed8344c2c8cd3ebb3b976e8
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-05-25 10:36:40 +0200

    ci(deps): fix ineffassign in ".*_test.go$"
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- pkg/rpc/server/listen_test.go
+++ pkg/rpc/server/listen_test.go
@@ -262,6 +262,7 @@ func Test_newServeMux(t *testing.T) {
 			url := ts.URL + tt.path
 			if tt.header == nil {
 				resp, err = http.Get(url)
+				require.NoError(t, err)
 			} else {
 				req, err := http.NewRequest(http.MethodPost, url, http.NoBody)
 				require.NoError(t, err)
@@ -269,9 +270,8 @@ func Test_newServeMux(t *testing.T) {
 				req.Header = tt.header
 				client := new(http.Client)
 				resp, err = client.Do(req)
+				require.NoError(t, err)
 			}
-
-			require.NoError(t, err)
 			assert.Equal(t, tt.want, resp.StatusCode)
 			defer resp.Body.Close()
 		})
