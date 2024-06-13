cd360dde202769b884c094326e07ef0332eb0daf
Nikita Pivkin <nikita.pivkin@smartforce.io>
2024-06-13 20:30:09 +0600

    BREAKING(misconf): flatten recursive types (#6862)

--- go.mod
+++ go.mod
@@ -26,7 +26,7 @@ require (
 	github.com/aquasecurity/table v1.8.0
 	github.com/aquasecurity/testdocker v0.0.0-20240613070307-2c3868d658ac
 	github.com/aquasecurity/tml v0.6.1
-	github.com/aquasecurity/trivy-aws v0.9.0
+	github.com/aquasecurity/trivy-aws v0.9.1-0.20240607040622-8a7f09cd891f
 	github.com/aquasecurity/trivy-checks v0.11.0
 	github.com/aquasecurity/trivy-db v0.0.0-20231005141211-4fc651f7ac8d
 	github.com/aquasecurity/trivy-java-db v0.0.0-20240109071736-184bd7481d48
--- pkg/iac/providers/google/iam/iam.go
+++ pkg/iac/providers/google/iam/iam.go
@@ -7,20 +7,18 @@ import (
 type IAM struct {
 	Organizations                 []Organization
 	WorkloadIdentityPoolProviders []WorkloadIdentityPoolProvider
+	Projects                      []Project
+	Folders                       []Folder
 }
 
 type Organization struct {
 	Metadata iacTypes.Metadata
-	Folders  []Folder
-	Projects []Project
 	Members  []Member
 	Bindings []Binding
 }
 
 type Folder struct {
 	Metadata iacTypes.Metadata
-	Folders  []Folder
-	Projects []Project
 	Members  []Member
 	Bindings []Binding
 }
@@ -54,35 +52,9 @@ type WorkloadIdentityPoolProvider struct {
 }
 
 func (p *IAM) AllProjects() []Project {
-	var projects []Project
-	for _, org := range p.Organizations {
-		projects = append(projects, org.Projects...)
-		for _, folder := range org.Folders {
-			projects = append(projects, folder.Projects...)
-			for _, desc := range folder.AllFolders() {
-				projects = append(projects, desc.Projects...)
-			}
-		}
-	}
-	return projects
+	return p.Projects
 }
 
 func (p *IAM) AllFolders() []Folder {
-	var folders []Folder
-	for _, org := range p.Organizations {
-		folders = append(folders, org.Folders...)
-		for _, folder := range org.Folders {
-			folders = append(folders, folder.AllFolders()...)
-		}
-	}
-	return folders
-}
-
-func (f *Folder) AllFolders() []Folder {
-	var folders []Folder
-	for _, folder := range f.Folders {
-		folders = append(folders, folder)
-		folders = append(folders, folder.AllFolders()...)
-	}
-	return folders
+	return p.Folders
 }
