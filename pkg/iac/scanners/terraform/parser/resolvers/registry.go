b5616d03c93908e8b4966027d2fbc8d077ea78a9
nikpivkin <nikita.pivkin@smartforce.io>
2024-07-08 14:52:13 +0700

    fix(misconf): load only submodule if it is specified in source
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>

--- pkg/iac/scanners/terraform/parser/resolvers/registry.go
+++ pkg/iac/scanners/terraform/parser/resolvers/registry.go
@@ -45,7 +45,7 @@ func (r *registryResolver) Resolve(ctx context.Context, target fs.FS, opt Option
 	}
 
 	inputVersion := opt.Version
-	source := removeSubdirFromSource(opt.Source)
+	source, _ := splitPackageSubdirRaw(opt.Source)
 	parts := strings.Split(source, "/")
 	if len(parts) < 3 || len(parts) > 4 {
 		return
