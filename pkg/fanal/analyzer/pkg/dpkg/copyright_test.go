6ec91a571d696a606c656c56b70954de69a19e4f
Pierre Baumard <pierre.baumard@cnav.fr>
2024-07-16 19:00:37 +0200

    Fix copyright_test

--- pkg/fanal/analyzer/pkg/dpkg/copyright_test.go
+++ pkg/fanal/analyzer/pkg/dpkg/copyright_test.go
@@ -29,7 +29,7 @@ func Test_dpkgLicenseAnalyzer_Analyze(t *testing.T) {
 						Type:     types.LicenseTypeDpkg,
 						FilePath: "usr/share/doc/zlib1g/copyright",
 						Findings: []types.LicenseFinding{
-							{Name: "GPL-1.0-only"},
+							{Name: "GPL-1.0-or-later"},
 							{Name: "Artistic-2.0"},
 							{Name: "BSD-4-clause-POWERDOG"},
 							{Name: "Zlib"},
@@ -66,6 +66,7 @@ func Test_dpkgLicenseAnalyzer_Analyze(t *testing.T) {
 						Type:     types.LicenseTypeDpkg,
 						FilePath: "usr/share/doc/apt/copyright",
 						Findings: []types.LicenseFinding{
+							{Name: "GPL-2.0-or-later"},
 							{Name: "GPL-2.0-only"},
 						},
 						PkgName: "apt",
