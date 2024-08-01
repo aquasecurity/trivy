a49980d6a5a8553ae24577923e9f79583597885a
afdesk <work@afdesk.com>
2024-08-01 13:42:18 +0600

    refactor: clean and rename

--- magefiles/docs.go
+++ magefiles/docs.go
@@ -53,11 +53,54 @@ func generateConfigDocs(filename string) error {
 	f.WriteString("# " + title + "\n\n")
 	f.WriteString(description + "\n")
 
-	flagsMetadata := buildFlagsTree()
-	genMarkdown(flagsMetadata, -1, f)
+	flagsTree := buildFlagsTree()
+
+	// -1 - is a level for title(section)
+	generateMarkdownByFlagDetailsTree(flagsTree, -1, f)
 
 	f.WriteString(footer)
 	return nil
+}
+
+func generateMarkdownByFlagDetailsTree(flagTree map[string]any, indent int, w *os.File) {
+	// Extract and sort keys
+	keys := make([]string, 0, len(flagTree))
+	for key := range flagTree {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	if indent == -1 {
+		for _, key := range keys {
+			w.WriteString("## " + key + " options\n\n")
+			w.WriteString("```yaml\n")
+			generateMarkdownByFlagDetailsTree(flagTree[key].(map[string]any), 0, w)
+			w.WriteString("```\n\n")
+		}
+		return
+	}
+	indentation := strings.Repeat("  ", indent)
+
+	for _, key := range keys {
+		switch v := flagTree[key].(type) {
+		case map[string]any:
+			fmt.Fprintf(w, "%s%s:\n", indentation, key)
+			generateMarkdownByFlagDetailsTree(v, indent+1, w)
+		case *flagDetails:
+			if v.name != "" {
+				fmt.Fprintf(w, "%s# Same as '--%s'\n", indentation, v.name)
+			}
+			fmt.Fprintf(w, "%s# Default is %v\n", indentation, v.defaultValue)
+			if len(v.example) > 1 {
+				fmt.Fprintf(w, "%s%s:\n", indentation, key)
+				for _, line := range v.example {
+					fmt.Fprintf(w, "%s - %s\n", indentation, line)
+				}
+			} else {
+				fmt.Fprintf(w, "%s%s: %s\n\n", indentation, key, v.example[0])
+			}
+		}
+	}
 }
 
 type flagDetails struct {
@@ -104,6 +147,43 @@ func getFlagDetails(section string, flagGroup []flag.Flagger) []*flagDetails {
 	return result
 }
 
+func buildFlagsTree() map[string]any {
+	var allFlags = map[string][]flag.Flagger{
+		"Global":           flag.NewGlobalFlagGroup().Flags(),
+		"Report":           flag.NewReportFlagGroup().Flags(),
+		"Image":            flag.NewImageFlagGroup().Flags(),
+		"DB":               flag.NewDBFlagGroup().Flags(),
+		"Cache":            flag.NewCacheFlagGroup().Flags(),
+		"License":          flag.NewLicenseFlagGroup().Flags(),
+		"Misconfiguration": flag.NewMisconfFlagGroup().Flags(),
+		"Scan":             flag.NewScanFlagGroup().Flags(),
+		"Module":           flag.NewModuleFlagGroup().Flags(),
+		"Registry":         flag.NewRegistryFlagGroup().Flags(),
+		"Rego":             flag.NewRegoFlagGroup().Flags(),
+		"Secret":           flag.NewSecretFlagGroup().Flags(),
+		"Vulnerability":    flag.NewVulnerabilityFlagGroup().Flags(),
+		"Kubernetes":       flag.NewK8sFlagGroup().Flags(),
+		"Repository":       flag.NewRepoFlagGroup().Flags(),
+		"Clean":            flag.NewCleanFlagGroup().Flags(),
+		"Cloud":            flag.NewAWSFlagGroup().Flags(),
+	}
+	// remoteFlags should contain Client and Server flags.
+	// NewClientFlags doesn't initialize `Listen` field
+	remoteFlags := flag.NewClientFlags()
+	remoteFlags.Listen = flag.ServerListenFlag.Clone()
+	allFlags["Client/Server"] = remoteFlags.Flags()
+
+	var details []*flagDetails
+	for k, v := range allFlags {
+		details = append(details, getFlagDetails(k, v)...)
+	}
+	res := map[string]any{}
+	for _, m := range details {
+		addToMap(res, strings.Split(m.configName, "."), m)
+	}
+	return res
+}
+
 func addToMap(m map[string]any, parts []string, value *flagDetails) {
 	if len(parts) == 0 {
 		return
@@ -121,77 +201,4 @@ func addToMap(m map[string]any, parts []string, value *flagDetails) {
 		m[parts[0]] = subMap
 	}
 	addToMap(subMap, parts[1:], value)
-}
-
-func buildFlagsTree() map[string]any {
-	details := getFlagDetails("Global", flag.NewGlobalFlagGroup().Flags())
-	details = append(details, getFlagDetails("Report", flag.NewReportFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Image", flag.NewImageFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("DB", flag.NewDBFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Cache", flag.NewCacheFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("License", flag.NewLicenseFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Misconfiguration", flag.NewMisconfFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Scan", flag.NewScanFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Module", flag.NewModuleFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Registry", flag.NewRegistryFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Rego", flag.NewRegoFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Secret", flag.NewSecretFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Vulnerability", flag.NewVulnerabilityFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Kubernetes", flag.NewK8sFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Repository", flag.NewRepoFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Clean", flag.NewCleanFlagGroup().Flags())...)
-	details = append(details, getFlagDetails("Cloud", flag.NewAWSFlagGroup().Flags())...)
-
-	// remoteFlags should contain Client and Server flags.
-	// NewClientFlags doesn't initialize `Listen` field
-	remoteFlags := flag.NewClientFlags()
-	remoteFlags.Listen = flag.ServerListenFlag.Clone()
-	details = append(details, getFlagDetails("Client/Server", remoteFlags.Flags())...)
-
-	res := map[string]any{}
-	for _, m := range details {
-		addToMap(res, strings.Split(m.configName, "."), m)
-	}
-	return res
-}
-
-func genMarkdown(m map[string]any, indent int, w *os.File) {
-	// Extract and sort keys
-	keys := make([]string, 0, len(m))
-	for key := range m {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-
-	if indent == -1 {
-		for _, key := range keys {
-			w.WriteString("## " + key + " options\n\n")
-			w.WriteString("```yaml\n")
-			genMarkdown(m[key].(map[string]any), 0, w)
-			w.WriteString("```\n\n")
-		}
-		return
-	}
-	indentation := strings.Repeat("  ", indent)
-
-	for _, key := range keys {
-		switch v := m[key].(type) {
-		case map[string]any:
-			fmt.Fprintf(w, "%s%s:\n", indentation, key)
-			genMarkdown(v, indent+1, w)
-		case *flagDetails:
-			if v.name != "" {
-				fmt.Fprintf(w, "%s# Same as '--%s'\n", indentation, v.name)
-			}
-			fmt.Fprintf(w, "%s# Default is %v\n", indentation, v.defaultValue)
-			if len(v.example) > 1 {
-				fmt.Fprintf(w, "%s%s:\n", indentation, key)
-				for _, line := range v.example {
-					fmt.Fprintf(w, "%s - %s\n", indentation, line)
-				}
-			} else {
-				fmt.Fprintf(w, "%s%s: %s\n\n", indentation, key, v.example[0])
-			}
-		}
-	}
 }
