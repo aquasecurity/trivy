b731f780a0208b4ac32a06cdccfd6979cf5482b1
knqyf263 <knqyf263@gmail.com>
2024-08-30 18:12:34 +0400

    refactor: add DeferredHandler
    Signed-off-by: knqyf263 <knqyf263@gmail.com>

--- pkg/log/deferred.go
+++ pkg/log/deferred.go
@@ -1,56 +1,62 @@
 package log
 
-// DeferredLogger are needed to save logs and print them after calling `PrintLogs()` command.
-// for example, this may be necessary when the logger is not yet initialized, but messages need to be transmitted
-// in this case, the messages are saved and printed after the logger is initialized
-var DeferredLogger deferredLogger
+import (
+	"context"
+	"log/slog"
+	"slices"
+)
 
-type deferredLogger struct {
-	deferredLogs []deferredLog
+func init() {
+	// Set the default logger so that logs are buffered until the logger is initialized.
+	slog.SetDefault(New(&DeferredHandler{records: new([]deferredRecord)}))
 }
 
-type deferredLog struct {
-	logFunc func(format string, args ...any)
-	message string
-	args    []any
+// DeferredHandler is needed to save logs and print them after calling `PrintLogs()` command.
+// For example, this may be necessary when the logger is not yet initialized, but messages need to be transmitted.
+// In this case, the messages are saved and printed when the logger is initialized.
+type DeferredHandler struct {
+	attrs []slog.Attr
+
+	// Shared with all instances of the handler.
+	// NOTE: non-thread safe
+	records *[]deferredRecord
 }
 
-func (d *deferredLogger) Debug(message string, args ...any) {
-	d.deferredLogs = append(d.deferredLogs, deferredLog{
-		logFunc: Debug,
-		message: message,
-		args:    args,
+type deferredRecord struct {
+	ctx context.Context
+	slog.Record
+}
+
+func (*DeferredHandler) Enabled(context.Context, slog.Level) bool {
+	return true
+}
+
+func (d *DeferredHandler) Handle(ctx context.Context, record slog.Record) error {
+	record.AddAttrs(d.attrs...)
+	*d.records = append(*d.records, deferredRecord{
+		ctx:    ctx,
+		Record: record,
 	})
+	return nil
 }
 
-func (d *deferredLogger) Info(message string, args ...any) {
-	d.deferredLogs = append(d.deferredLogs, deferredLog{
-		logFunc: Info,
-		message: message,
-		args:    args,
-	})
+func (d *DeferredHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	h := *d
+	h.attrs = slices.Clip(d.attrs)
+	h.attrs = append(h.attrs, attrs...)
+	return &h
 }
 
-func (d *deferredLogger) Warn(message string, args ...any) {
-	d.deferredLogs = append(d.deferredLogs, deferredLog{
-		logFunc: Warn,
-		message: message,
-		args:    args,
-	})
+func (*DeferredHandler) WithGroup(_ string) slog.Handler {
+	panic("WithGroup is not implemented")
 }
 
-func (d *deferredLogger) Error(message string, args ...any) {
-	d.deferredLogs = append(d.deferredLogs, deferredLog{
-		logFunc: Error,
-		message: message,
-		args:    args,
-	})
-}
-
-func (d *deferredLogger) PrintLogs() {
-	for _, l := range d.deferredLogs {
-		l.logFunc(l.message, l.args...)
+func (d *DeferredHandler) Flush(h slog.Handler) {
+	for _, record := range *d.records {
+		if !h.Enabled(record.ctx, record.Level) {
+			continue
+		}
+		_ = h.Handle(record.ctx, record.Record)
 	}
-	// Clear deferredLogs
-	d.deferredLogs = nil
+	d.records = nil
 }
