3d537b908bebf4caff821871853b6029791706ed
Aqua Security automated builds <54269356+aqua-bot@users.noreply.github.com>
2024-11-18 01:41:56 -0700

    test: change branch in spdx schema link to check in integration tests [backport: release/v0.57] (#7940)
    Co-authored-by: DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -41,7 +41,7 @@ import (
 
 var update = flag.Bool("update", false, "update golden files")
 
-const SPDXSchema = "https://raw.githubusercontent.com/spdx/spdx-spec/development/v%s/schemas/spdx-schema.json"
+const SPDXSchema = "https://raw.githubusercontent.com/spdx/spdx-spec/support/v%s/schemas/spdx-schema.json"
 
 func initDB(t *testing.T) string {
 	fixtureDir := filepath.Join("testdata", "fixtures", "db")
