22f9df9b4eae738e92d005ab7d53f2e6cc06ff5b
nikpivkin <nikita.pivkin@smartforce.io>
2024-12-05 15:21:44 +0600

    feat(misconf): generate placeholders for random provider resources
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>

--- pkg/iac/terraform/presets.go
+++ pkg/iac/terraform/presets.go
@@ -2,11 +2,22 @@ package terraform
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"strings"
 
 	"github.com/google/uuid"
 	"github.com/zclconf/go-cty/cty"
 )
+
+var resourceRandomAttributes = map[string][]string{
+	// If the user leaves the name blank, Terraform will automatically generate a unique name
+	"aws_launch_template": {"name"},
+	"random_id":           {"hex", "dec", "b64_url", "b64_std"},
+	"random_password":     {"result", "bcrypt_hash"},
+	"random_string":       {"result"},
+	"random_bytes":        {"base64", "hex"},
+	"random_uuid":         {"result"},
+}
 
 func createPresetValues(b *Block) map[string]cty.Value {
 	presets := make(map[string]cty.Value)
@@ -23,16 +34,21 @@ func createPresetValues(b *Block) map[string]cty.Value {
 	// workaround for weird iam feature
 	case "aws_iam_policy_document":
 		presets["json"] = cty.StringVal(b.ID())
-	// If the user leaves the name blank, Terraform will automatically generate a unique name
-	case "aws_launch_template":
-		presets["name"] = cty.StringVal(uuid.New().String())
 	// allow referencing the current region name
 	case "aws_region":
 		presets["name"] = cty.StringVal("current-region")
+	case "random_integer":
+		//nolint:gosec
+		presets["result"] = cty.NumberIntVal(rand.Int64())
+	}
+
+	if attrs, exists := resourceRandomAttributes[b.TypeLabel()]; exists {
+		for _, attr := range attrs {
+			presets[attr] = cty.StringVal(uuid.New().String())
+		}
 	}
 
 	return presets
-
 }
 
 func postProcessValues(b *Block, input map[string]cty.Value) map[string]cty.Value {
