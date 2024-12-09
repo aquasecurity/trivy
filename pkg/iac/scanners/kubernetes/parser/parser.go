89dd5270c5ed9e1994d54a71b2930cfa780787de
nikpivkin <nikita.pivkin@smartforce.io>
2024-12-09 17:44:08 +0600

    fix(misconf): fix incorrect k8s locations due to JSON to YAML conversion
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>

--- pkg/iac/scanners/kubernetes/parser/parser.go
+++ pkg/iac/scanners/kubernetes/parser/parser.go
@@ -2,14 +2,12 @@ package parser
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"regexp"
 	"strings"
 
 	"gopkg.in/yaml.v3"
-	kyaml "sigs.k8s.io/yaml"
 )
 
 func Parse(_ context.Context, r io.Reader, path string) ([]any, error) {
@@ -23,15 +21,11 @@ func Parse(_ context.Context, r io.Reader, path string) ([]any, error) {
 	}
 
 	if strings.TrimSpace(string(contents))[0] == '{' {
-		var target any
-		if err := json.Unmarshal(contents, &target); err != nil {
-			return nil, err
-		}
-
-		contents, err = kyaml.JSONToYAML(contents) // convert into yaml to reuse file parsing logic
+		manifest, err := ManifestFromJSON(path, contents)
 		if err != nil {
 			return nil, err
 		}
+		return []any{manifest.ToRego()}, nil
 	}
 
 	var results []any
