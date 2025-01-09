f352f6b66355fe3636c9e4e9f3edd089c551a81c
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2025-01-09 18:45:45 +0600

    fix(redhat): check `usr/share/buildinfo/` dir to detect content sets (#8222)

--- pkg/fanal/analyzer/buildinfo/content_manifest_test.go
+++ pkg/fanal/analyzer/buildinfo/content_manifest_test.go
@@ -73,12 +73,22 @@ func Test_contentManifestAnalyzer_Required(t *testing.T) {
 		want     bool
 	}{
 		{
-			name:     "happy path",
+			name:     "happy path root dir",
 			filePath: "root/buildinfo/content_manifests/nodejs-12-container-1-66.json",
 			want:     true,
 		},
 		{
-			name:     "sad path",
+			name:     "happy path usr dir",
+			filePath: "usr/share/buildinfo/nodejs-12-container-1-66.json",
+			want:     true,
+		},
+		{
+			name:     "sad path wrong dir",
+			filePath: "foo/bar/nodejs-12-container-1-66.json",
+			want:     false,
+		},
+		{
+			name:     "sad path wrong extension",
 			filePath: "root/buildinfo/content_manifests/nodejs-12-container-1-66.xml",
 			want:     false,
 		},
