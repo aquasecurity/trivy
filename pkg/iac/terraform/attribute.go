bd72f6f88e14939022746dc9b045c826f34bd118
Steven Masley <stevenmasley@gmail.com>
2025-01-21 16:36:37 -0600

    fix(terraform): hcl object expressions to return references
    References included in hcl objects should be returned from
    'Attribute.AllReferences()'. Traverse the object fields recursively
    to locate references.

--- pkg/iac/terraform/attribute.go
+++ pkg/iac/terraform/attribute.go
@@ -971,6 +971,10 @@ func (a *Attribute) AllReferences(blocks ...*Block) []*Reference {
 func (a *Attribute) referencesFromExpression(expression hcl.Expression) []*Reference {
 	var refs []*Reference
 	switch t := expression.(type) {
+	case *hclsyntax.ObjectConsExpr:
+		for _, item := range t.Items {
+			refs = append(refs, a.referencesFromExpression(item.ValueExpr)...)
+		}
 	case *hclsyntax.ConditionalExpr:
 		if ref, err := createDotReferenceFromTraversal(a.module, t.TrueResult.Variables()...); err == nil {
 			refs = append(refs, ref)
---
+++ pkg/iac/terraform/attribute_test.go
@@ -0,0 +1,73 @@
+package terraform
+
+import (
+	"testing"
+
+	"github.com/aquasecurity/trivy/pkg/iac/terraform/context"
+	"github.com/aquasecurity/trivy/pkg/iac/types"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_AllReferences(t *testing.T) {
+	cases := []struct {
+		input string
+		refs  []string
+	}{
+		{
+			input: "42", // literal
+			refs:  []string{},
+		},
+		{
+			input: "5 == 5", // comparison
+			refs:  []string{},
+		},
+		{
+			input: "var.foo",
+			refs:  []string{"variable.foo"},
+		},
+		{
+			input: "resource.aws_instance.id",
+			refs:  []string{"aws_instance.id"},
+		},
+		{
+			input: "data.aws_ami.ubuntu.most_recent",
+			refs:  []string{"data.aws_ami.ubuntu.most_recent"},
+		},
+		{
+			input: `{x = 1, y = data.aws_ami.ubuntu.most_recent}`,
+			refs:  []string{"data.aws_ami.ubuntu.most_recent"},
+		},
+		{
+			input: `{foo = 1 == 1 ? true : data.aws_ami.ubuntu.most_recent}`,
+			refs:  []string{"data.aws_ami.ubuntu.most_recent"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.input, func(t *testing.T) {
+			ctx := context.NewContext(&hcl.EvalContext{}, nil)
+
+			exp, diags := hclsyntax.ParseExpression([]byte(test.input), "", hcl.Pos{Line: 1, Column: 1})
+			if diags != nil && diags.HasErrors() {
+				t.Fatal(diags.Error())
+			}
+
+			a := NewAttribute(&hcl.Attribute{
+				Name:      "test",
+				Expr:      exp,
+				Range:     hcl.Range{},
+				NameRange: hcl.Range{},
+			}, ctx, "", types.Metadata{}, Reference{}, "", nil)
+
+			refs := a.AllReferences()
+			humanRefs := make([]string, 0, len(refs))
+			for _, ref := range refs {
+				humanRefs = append(humanRefs, ref.HumanReadable())
+			}
+
+			require.ElementsMatch(t, test.refs, humanRefs)
+		})
+	}
+}
