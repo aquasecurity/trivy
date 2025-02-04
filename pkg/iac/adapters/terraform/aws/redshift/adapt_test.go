8468ed8b9dd18702861ce4e54dd30ef8d415c4e9
nikpivkin <nikita.pivkin@smartforce.io>
2025-02-04 17:02:54 +0600

    chore: remove debug prints
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>

--- pkg/iac/adapters/terraform/aws/redshift/adapt_test.go
+++ pkg/iac/adapters/terraform/aws/redshift/adapt_test.go
@@ -1,7 +1,6 @@
 package redshift
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -76,7 +75,6 @@ func Test_Adapt(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			modules := tftestutil.CreateModulesFromSource(t, test.terraform, ".tf")
 			adapted := Adapt(modules)
-			fmt.Println(adapted.SecurityGroups[0].Description.Value())
 			testutil.AssertDefsecEqual(t, test.expected, adapted)
 		})
 	}
