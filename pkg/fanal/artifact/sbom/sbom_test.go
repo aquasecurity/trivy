8c61de8c724e338671474235f0fbcef5909b8ede
knqyf263 <knqyf263@gmail.com>
2025-02-14 12:12:56 +0400

    test(sbom): use memory cache
    Signed-off-by: knqyf263 <knqyf263@gmail.com>

--- pkg/fanal/artifact/sbom/sbom_test.go
+++ pkg/fanal/artifact/sbom/sbom_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/aquasecurity/trivy/internal/cachetest"
 	"github.com/aquasecurity/trivy/pkg/cache"
 	"github.com/aquasecurity/trivy/pkg/fanal/artifact"
 	"github.com/aquasecurity/trivy/pkg/fanal/artifact/sbom"
@@ -19,18 +20,18 @@ import (
 
 func TestArtifact_Inspect(t *testing.T) {
 	tests := []struct {
-		name               string
-		filePath           string
-		putBlobExpectation cache.ArtifactCachePutBlobExpectation
-		want               artifact.Reference
-		wantErr            []string
+		name      string
+		filePath  string
+		wantBlobs []cachetest.WantBlob
+		want      artifact.Reference
+		wantErr   []string
 	}{
 		{
 			name:     "happy path",
 			filePath: filepath.Join("testdata", "bom.json"),
-			putBlobExpectation: cache.ArtifactCachePutBlobExpectation{
-				Args: cache.ArtifactCachePutBlobArgs{
-					BlobID: "sha256:76bc49ae239d24c6a122e730bafb9d5295d0af380492aeb92a3bf34bea3a14ca",
+			wantBlobs: []cachetest.WantBlob{
+				{
+					ID: "sha256:76bc49ae239d24c6a122e730bafb9d5295d0af380492aeb92a3bf34bea3a14ca",
 					BlobInfo: types.BlobInfo{
 						SchemaVersion: types.BlobJSONSchemaVersion,
 						OS: types.OS{
@@ -186,179 +187,173 @@ func TestArtifact_Inspect(t *testing.T) {
 						},
 					},
 				},
-				Returns: cache.ArtifactCachePutBlobReturns{},
 			},
-			want: artifact.Reference{
-				Name: filepath.Join("testdata", "bom.json"),
-				Type: artifact.TypeCycloneDX,
-				ID:   "sha256:76bc49ae239d24c6a122e730bafb9d5295d0af380492aeb92a3bf34bea3a14ca",
-				BlobIDs: []string{
-					"sha256:76bc49ae239d24c6a122e730bafb9d5295d0af380492aeb92a3bf34bea3a14ca",
+				want: artifact.Reference{
+					Name: filepath.Join("testdata", "bom.json"),
+					Type: artifact.TypeCycloneDX,
+					ID:   "sha256:76bc49ae239d24c6a122e730bafb9d5295d0af380492aeb92a3bf34bea3a14ca",
+					BlobIDs: []string{
+						"sha256:76bc49ae239d24c6a122e730bafb9d5295d0af380492aeb92a3bf34bea3a14ca",
+					},
 				},
 			},
-		},
-		{
-			name:     "happy path for sbom attestation",
-			filePath: filepath.Join("testdata", "sbom.cdx.intoto.jsonl"),
-			putBlobExpectation: cache.ArtifactCachePutBlobExpectation{
-				Args: cache.ArtifactCachePutBlobArgs{
-					BlobID: "sha256:76bc49ae239d24c6a122e730bafb9d5295d0af380492aeb92a3bf34bea3a14ca",
-					BlobInfo: types.BlobInfo{
-						SchemaVersion: types.BlobJSONSchemaVersion,
-						OS: types.OS{
-							Family: "alpine",
-							Name:   "3.16.0",
-						},
-						PackageInfos: []types.PackageInfo{
-							{
-								Packages: types.Packages{
-									{
-										ID:         "musl@1.2.3-r0",
-										Name:       "musl",
-										Version:    "1.2.3-r0",
-										SrcName:    "musl",
-										SrcVersion: "1.2.3-r0",
-										Licenses:   []string{"MIT"},
-										Identifier: types.PkgIdentifier{
-											PURL: &packageurl.PackageURL{
-												Type:      packageurl.TypeApk,
-												Namespace: "alpine",
-												Name:      "musl",
-												Version:   "1.2.3-r0",
-												Qualifiers: packageurl.Qualifiers{
-													{
-														Key:   "distro",
-														Value: "3.16.0",
-													},
+			{
+				name:     "happy path for sbom attestation",
+				filePath: filepath.Join("testdata", "sbom.cdx.intoto.jsonl"),
+				wantBlobs: []cachetest.WantBlob{
+					SchemaVersion: types.BlobJSONSchemaVersion,
+					OS: types.OS{
+						Family: "alpine",
+						Name:   "3.16.0",
+					},
+					PackageInfos: []types.PackageInfo{
+						{
+							Packages: types.Packages{
+								{
+									ID:         "musl@1.2.3-r0",
+									Name:       "musl",
+									Version:    "1.2.3-r0",
+									SrcName:    "musl",
+									SrcVersion: "1.2.3-r0",
+									Licenses:   []string{"MIT"},
+									Identifier: types.PkgIdentifier{
+										PURL: &packageurl.PackageURL{
+											Type:      packageurl.TypeApk,
+											Namespace: "alpine",
+											Name:      "musl",
+											Version:   "1.2.3-r0",
+											Qualifiers: packageurl.Qualifiers{
+												{
+													Key:   "distro",
+													Value: "3.16.0",
 												},
 											},
-											BOMRef: "pkg:apk/alpine/musl@1.2.3-r0?distro=3.16.0",
 										},
-										Layer: types.Layer{
-											DiffID: "sha256:dd565ff850e7003356e2b252758f9bdc1ff2803f61e995e24c7844f6297f8fc3",
-										},
+										BOMRef: "pkg:apk/alpine/musl@1.2.3-r0?distro=3.16.0",
 									},
-								},
-							},
-						},
-						Applications: []types.Application{
-							{
-								Type:     "composer",
-								FilePath: "app/composer/composer.lock",
-								Packages: types.Packages{
-									{
-										ID:      "pear/log@1.13.1",
-										Name:    "pear/log",
-										Version: "1.13.1",
-										Identifier: types.PkgIdentifier{
-											PURL: &packageurl.PackageURL{
-												Type:      packageurl.TypeComposer,
-												Namespace: "pear",
-												Name:      "log",
-												Version:   "1.13.1",
-											},
-											BOMRef: "pkg:composer/pear/log@1.13.1",
-										},
-										Layer: types.Layer{
-											DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
-										},
-									},
-									{
-										ID:      "pear/pear_exception@v1.0.0",
-										Name:    "pear/pear_exception",
-										Version: "v1.0.0",
-										Identifier: types.PkgIdentifier{
-											PURL: &packageurl.PackageURL{
-												Type:      packageurl.TypeComposer,
-												Namespace: "pear",
-												Name:      "pear_exception",
-												Version:   "v1.0.0",
-											},
-											BOMRef: "pkg:composer/pear/pear_exception@v1.0.0",
-										},
-										Layer: types.Layer{
-											DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
-										},
-									},
-								},
-							},
-							{
-								Type:     "gobinary",
-								FilePath: "app/gobinary/gobinary",
-								Packages: types.Packages{
-									{
-										ID:      "github.com/package-url/packageurl-go@v0.1.1-0.20220203205134-d70459300c8a",
-										Name:    "github.com/package-url/packageurl-go",
-										Version: "v0.1.1-0.20220203205134-d70459300c8a",
-										Identifier: types.PkgIdentifier{
-											PURL: &packageurl.PackageURL{
-												Type:      packageurl.TypeGolang,
-												Namespace: "github.com/package-url",
-												Name:      "packageurl-go",
-												Version:   "v0.1.1-0.20220203205134-d70459300c8a",
-											},
-											BOMRef: "pkg:golang/github.com/package-url/packageurl-go@v0.1.1-0.20220203205134-d70459300c8a",
-										},
-										Layer: types.Layer{
-											DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
-										},
-									},
-								},
-							},
-							{
-								Type:     "jar",
-								FilePath: "",
-								Packages: types.Packages{
-									{
-										ID:      "org.codehaus.mojo:child-project:1.0",
-										Name:    "org.codehaus.mojo:child-project",
-										Version: "1.0",
-										Identifier: types.PkgIdentifier{
-											PURL: &packageurl.PackageURL{
-												Type:      packageurl.TypeMaven,
-												Namespace: "org.codehaus.mojo",
-												Name:      "child-project",
-												Version:   "1.0",
-											},
-											// Keep the original value
-											BOMRef: "pkg:maven/org.codehaus.mojo/child-project@1.0?file_path=app%2Fmaven%2Ftarget%2Fchild-project-1.0.jar",
-										},
-										Layer: types.Layer{
-											DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
-										},
-										FilePath: "app/maven/target/child-project-1.0.jar",
-									},
-								},
-							},
-							{
-								Type:     "node-pkg",
-								FilePath: "",
-								Packages: types.Packages{
-									{
-										ID:      "bootstrap@5.0.2",
-										Name:    "bootstrap",
-										Version: "5.0.2",
-										Identifier: types.PkgIdentifier{
-											PURL: &packageurl.PackageURL{
-												Type:    packageurl.TypeNPM,
-												Name:    "bootstrap",
-												Version: "5.0.2",
-											},
-											// Keep the original value
-											BOMRef: "pkg:npm/bootstrap@5.0.2?file_path=app%2Fapp%2Fpackage.json",
-										},
-										Licenses: []string{"MIT"},
-										Layer: types.Layer{
-											DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
-										},
-										FilePath: "app/app/package.json",
+									Layer: types.Layer{
+										DiffID: "sha256:dd565ff850e7003356e2b252758f9bdc1ff2803f61e995e24c7844f6297f8fc3",
 									},
 								},
 							},
 						},
 					},
+					Applications: []types.Application{
+						{
+							Type:     "composer",
+							FilePath: "app/composer/composer.lock",
+							Packages: types.Packages{
+								{
+									ID:      "pear/log@1.13.1",
+									Name:    "pear/log",
+									Version: "1.13.1",
+									Identifier: types.PkgIdentifier{
+										PURL: &packageurl.PackageURL{
+											Type:      packageurl.TypeComposer,
+											Namespace: "pear",
+											Name:      "log",
+											Version:   "1.13.1",
+										},
+										BOMRef: "pkg:composer/pear/log@1.13.1",
+									},
+									Layer: types.Layer{
+										DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
+									},
+								},
+								{
+									ID:      "pear/pear_exception@v1.0.0",
+									Name:    "pear/pear_exception",
+									Version: "v1.0.0",
+									Identifier: types.PkgIdentifier{
+										PURL: &packageurl.PackageURL{
+											Type:      packageurl.TypeComposer,
+											Namespace: "pear",
+											Name:      "pear_exception",
+											Version:   "v1.0.0",
+										},
+										BOMRef: "pkg:composer/pear/pear_exception@v1.0.0",
+									},
+									Layer: types.Layer{
+										DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
+									},
+								},
+							},
+						},
+						{
+							Type:     "gobinary",
+							FilePath: "app/gobinary/gobinary",
+							Packages: types.Packages{
+								{
+									ID:      "github.com/package-url/packageurl-go@v0.1.1-0.20220203205134-d70459300c8a",
+									Name:    "github.com/package-url/packageurl-go",
+									Version: "v0.1.1-0.20220203205134-d70459300c8a",
+									Identifier: types.PkgIdentifier{
+										PURL: &packageurl.PackageURL{
+											Type:      packageurl.TypeGolang,
+											Namespace: "github.com/package-url",
+											Name:      "packageurl-go",
+											Version:   "v0.1.1-0.20220203205134-d70459300c8a",
+										},
+										BOMRef: "pkg:golang/github.com/package-url/packageurl-go@v0.1.1-0.20220203205134-d70459300c8a",
+									},
+									Layer: types.Layer{
+										DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
+									},
+								},
+							},
+						},
+						{
+							Type:     "jar",
+							FilePath: "",
+							Packages: types.Packages{
+								{
+									ID:      "org.codehaus.mojo:child-project:1.0",
+									Name:    "org.codehaus.mojo:child-project",
+									Version: "1.0",
+									Identifier: types.PkgIdentifier{
+										PURL: &packageurl.PackageURL{
+											Type:      packageurl.TypeMaven,
+											Namespace: "org.codehaus.mojo",
+											Name:      "child-project",
+											Version:   "1.0",
+										},
+										// Keep the original value
+										BOMRef: "pkg:maven/org.codehaus.mojo/child-project@1.0?file_path=app%2Fmaven%2Ftarget%2Fchild-project-1.0.jar",
+									},
+									Layer: types.Layer{
+										DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
+									},
+									FilePath: "app/maven/target/child-project-1.0.jar",
+								},
+							},
+						},
+						{
+							Type:     "node-pkg",
+							FilePath: "",
+							Packages: types.Packages{
+								{
+									ID:      "bootstrap@5.0.2",
+									Name:    "bootstrap",
+									Version: "5.0.2",
+									Identifier: types.PkgIdentifier{
+										PURL: &packageurl.PackageURL{
+											Type:    packageurl.TypeNPM,
+											Name:    "bootstrap",
+											Version: "5.0.2",
+										},
+										// Keep the original value
+										BOMRef: "pkg:npm/bootstrap@5.0.2?file_path=app%2Fapp%2Fpackage.json",
+									},
+									Licenses: []string{"MIT"},
+									Layer: types.Layer{
+										DiffID: "sha256:3c79e832b1b4891a1cb4a326ef8524e0bd14a2537150ac0e203a5677176c1ca1",
+									},
+									FilePath: "app/app/package.json",
+								},
+							},
+						},
+					},
 				},
-				Returns: cache.ArtifactCachePutBlobReturns{},
 			},
 			want: artifact.Reference{
 				Name: filepath.Join("testdata", "sbom.cdx.intoto.jsonl"),
@@ -400,8 +395,7 @@ func TestArtifact_Inspect(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := new(cache.MockArtifactCache)
-			c.ApplyPutBlobExpectation(tt.putBlobExpectation)
+			c := cachetest.NewCache(t, nil)
 
 			a, err := sbom.NewArtifact(tt.filePath, c, artifact.Option{})
 			require.NoError(t, err)
@@ -425,6 +419,8 @@ func TestArtifact_Inspect(t *testing.T) {
 
 			require.NoError(t, err)
 			assert.Equal(t, tt.want, got)
+
+			cachetest.AssertBlobs(t, c, tt.wantBlobs)
 		})
 	}
 }
