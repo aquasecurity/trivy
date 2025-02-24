b49549ac87b38437d42b1b58ffced601c8c04489
albe19029 <albe19021990@gmail.com>
2025-02-24 20:59:20 +0300

    fix

--- pkg/fanal/applier/docker.go
+++ pkg/fanal/applier/docker.go
@@ -174,6 +174,7 @@ func deleteDpkgLicensesByString(key, sep string, dpkgLicensesMap *map[string]*Ma
 				result, ok := (*dpkgLicensesMap)[v.FilePath]
 				if ok {
 					for _, dep := range result.Deps {
+						_ = nestedMap.DeleteByString(dep.FilePath, sep) // nolint
 						delete(*dpkgLicensesMap, dep.FilePath)
 					}
 					delete(*dpkgLicensesMap, v.FilePath)
