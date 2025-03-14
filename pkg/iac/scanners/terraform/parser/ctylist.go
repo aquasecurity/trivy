ce933c9ccc857c039c6f3017d0ebc55dcc14ee5d
Steven Masley <stevenmasley@gmail.com>
2025-03-14 18:37:53 -0500

    add unit tests for ctylist

--- pkg/iac/scanners/terraform/parser/ctylist.go
+++ pkg/iac/scanners/terraform/parser/ctylist.go
@@ -5,10 +5,18 @@ import "github.com/zclconf/go-cty/cty"
 // insertTupleElement inserts a value into a tuple at the specified index.
 // If the idx is outside the bounds of the list, it grows the tuple to
 // the new size, and fills in `cty.NilVal` for the missing elements.
+//
+// This function will not panic. If the list value is not a list, it will
+// be replaced with an empty list.
 func insertTupleElement(list cty.Value, idx int, val cty.Value) cty.Value {
-	if list.IsNull() {
-		// no null lists
+	if list.IsNull() || !list.Type().IsTupleType() {
+		// better than a panic
 		list = cty.EmptyTupleVal
+	}
+
+	if idx < 0 {
+		// Nothing to do?
+		return list
 	}
 
 	newList := make([]cty.Value, max(idx+1, list.LengthInt()))
