b43cac8d04cda61a96f8d9d9368ed4e065d6007f
Steven Masley <stevenmasley@gmail.com>
2025-03-14 14:12:02 -0500

    simplier example

--- pkg/iac/scanners/terraform/parser/testdata/countarguments/main.tf
+++ pkg/iac/scanners/terraform/parser/testdata/countarguments/main.tf
@@ -1,13 +1,43 @@
-data "null_data_source" "list" {
-  count = 3
-  inputs = {
-    foo = "Index ${count.index}"
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.8.1"
+    }
   }
 }
 
-data "null_data_source" "ref_list" {
-  count = 1
-  inputs = {
-    foo = data.null_data_source.list[2].outputs.foo
-  }
+
+// aother works
+data "o" "b"{
+    count = 1
+    value = "Index ${count.index}"
 }
+
+data "b" "b" {
+    count = 1
+    value = data.o.b[0].value
+}
+
+data "c" "c" {
+  count = 1
+  value = data.b.b[0].value
+}
+
+
+# data "other" "base" {
+#   count = 1
+#   value = "Index 0"
+#   # value = data.null_data_source.list[2].inputs.foo
+# }
+#
+#
+# data "aother" "ref" {
+#   count = 1
+#   value = data.other.base[0].value
+# }
+#
+# data "other" "other" {
+#   count = 1
+#   value = data.aother.ref[0].value
+# }
