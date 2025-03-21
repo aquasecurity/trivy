68b164ddf4832039ee1c5be4f8a85d344dd6e572
simar7 <1254783+simar7@users.noreply.github.com>
2025-03-21 16:38:26 -0600

    refactor(misconf): Simplify misconfig checks bundle parsing (#8533)

--- pkg/policy/policy_test.go
+++ pkg/policy/policy_test.go
@@ -58,39 +58,17 @@ func newBrokenLayer(t *testing.T) v1.Layer {
 	return brokenLayer{layer}
 }
 
-func TestClient_LoadBuiltinPolicies(t *testing.T) {
+func TestClient_LoadBuiltinChecks(t *testing.T) {
 	tests := []struct {
 		name     string
 		cacheDir string
-		want     []string
+		want     string
 		wantErr  string
 	}{
 		{
 			name:     "happy path",
 			cacheDir: "testdata/happy",
-			want: []string{
-				filepath.Join("testdata", "happy", "policy", "content", "kubernetes"),
-				filepath.Join("testdata", "happy", "policy", "content", "docker"),
-			},
-		},
-		{
-			name:     "empty roots",
-			cacheDir: "testdata/empty",
-			want: []string{
-				filepath.Join("testdata", "empty", "policy", "content"),
-			},
-		},
-		{
-			name:     "broken manifest",
-			cacheDir: "testdata/broken",
-			want:     []string{},
-			wantErr:  "json decode error",
-		},
-		{
-			name:     "no such file",
-			cacheDir: "testdata/unknown",
-			want:     []string{},
-			wantErr:  "manifest file open error",
+			want:     filepath.Join("testdata", "happy", "policy", "content"),
 		},
 	}
 	for _, tt := range tests {
@@ -119,7 +97,7 @@ func TestClient_LoadBuiltinPolicies(t *testing.T) {
 			c, err := policy.NewClient(tt.cacheDir, true, "", policy.WithOCIArtifact(art))
 			require.NoError(t, err)
 
-			got, err := c.LoadBuiltinChecks()
+			got := c.LoadBuiltinChecks()
 			if tt.wantErr != "" {
 				require.ErrorContains(t, err, tt.wantErr)
 				return
@@ -265,7 +243,7 @@ func TestClient_NeedsUpdate(t *testing.T) {
 	}
 }
 
-func TestClient_DownloadBuiltinPolicies(t *testing.T) {
+func TestClient_DownloadBuiltinChecks(t *testing.T) {
 	type digestReturns struct {
 		h   v1.Hash
 		err error
