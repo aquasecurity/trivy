3134d5dbef0ca74637cbde5c7ec2ed5f1add8a6a
DmitriyLewen <dmitriy.lewen@smartforce.io>
2025-04-01 17:06:23 +0600

    refactor(xjson): use `bytes.IndexByte`

--- pkg/x/json/json.go
+++ pkg/x/json/json.go
@@ -39,14 +39,9 @@ func unmarshaler(data []byte, visited set.Set[any]) *json.Unmarshalers {
 		inputOffset := dec.InputOffset()
 		kind := dec.PeekKind()
 
-		// dec.InputOffset() returns `It gives the location of the next byte immediately after the most recently returned token or value.`
-		// So, we need to find the location of the first token of the current object.
-		for {
-			if data[inputOffset] == byte(kind) {
-				break
-			}
-			inputOffset += 1
-		}
+		//dec.InputOffset() returns `It gives the location of the next byte immediately after the most recently returned token or value.`
+		//So, we need to find the location of the first token of the current object.
+		inputOffset += int64(bytes.IndexByte(data[inputOffset:], byte(kind)))
 
 		// Check visited set to avoid infinity loops
 		if visited.Contains(inputOffset) {
