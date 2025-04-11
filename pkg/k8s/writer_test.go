407905112e21b4221a2663ac33217dd1a2e09a53
afdesk <work@afdesk.com>
2025-04-11 13:52:35 +0600

    test(k8s): add testcase with vulns and misconfigs

--- pkg/k8s/writer_test.go
+++ pkg/k8s/writer_test.go
@@ -324,6 +324,43 @@ See https://google.com/search?q=bad%20config
 ────────────────────────────────────────`,
 		},
 		{
+			name: "vulns and misconfig with `--report all`",
+			report: report.Report{
+				ClusterName: "test",
+				Resources: []report.Resource{
+					deployOrionWithSingleVuln,
+					deployOrionWithSingleMisconfig,
+				},
+			},
+			scanners: types.Scanners{types.VulnerabilityScanner, types.MisconfigScanner},
+			severities: []dbTypes.Severity{
+				dbTypes.SeverityCritical, dbTypes.SeverityLow,
+			},
+			reportType: report.AllReport,
+			expectedOutput: `namespace: default, deploy: orion ()
+====================================
+Total: 1 (LOW: 1, CRITICAL: 0)
+
+┌─────────┬───────────────┬──────────┬─────────┬───────────────────┬───────────────┬───────────────────────────────────────────┐
+│ Library │ Vulnerability │ Severity │ Status  │ Installed Version │ Fixed Version │                   Title                   │
+├─────────┼───────────────┼──────────┼─────────┼───────────────────┼───────────────┼───────────────────────────────────────────┤
+│ foo/bar │ CVE-2022-1111 │ LOW      │ unknown │ v0.0.1            │ v0.0.2        │ https://avd.aquasec.com/nvd/cve-2022-1111 │
+└─────────┴───────────────┴──────────┴─────────┴───────────────────┴───────────────┴───────────────────────────────────────────┘
+
+namespace: default, deploy: orion ()
+====================================
+Tests: 1 (SUCCESSES: 0, FAILURES: 1)
+Failures: 1 (LOW: 1, CRITICAL: 0)
+
+ (LOW): Oh no, a bad config.
+════════════════════════════════════════
+Your config file is not good.
+
+See https://google.com/search?q=bad%20config
+────────────────────────────────────────`,
+		},
+
+		{
 			name: "Only vuln, all severities",
 			report: report.Report{
 				ClusterName: "test",
@@ -528,6 +565,7 @@ Infra Assessment
 └─────────────┴────────────────────┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
 Severities: C=CRITICAL H=HIGH M=MEDIUM L=LOW U=UNKNOWN`,
 		},
+		{},
 	}
 
 	for _, tc := range tests {
