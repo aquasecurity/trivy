2135add88f3c5c986f8acf3588382777c5d372f2
sneaky-potato <ashwanikamal.im421@gmail.com>
2025-05-09 13:19:06 +0530

    feat(parser): add bun parser

---
+++ pkg/dependency/parser/nodejs/bun/parse_testcase.go
@@ -0,0 +1,30 @@
+package bun
+
+import (
+	ftypes "github.com/aquasecurity/trivy/pkg/fanal/types"
+)
+
+var (
+	normalPkgs = []ftypes.Package{
+		{
+			ID:           "bun-types@1.2.12",
+			Name:         "bun-types",
+			Version:      "1.2.12",
+			Relationship: ftypes.RelationshipIndirect,
+		},
+		{
+			ID:           "typescript@5.8.3",
+			Name:         "typescript",
+			Version:      "5.8.3",
+			Relationship: ftypes.RelationshipDirect,
+		},
+		{
+			ID:           "undici-types@6.21.0",
+			Name:         "undici-types",
+			Version:      "6.21.0",
+			Relationship: ftypes.RelationshipIndirect,
+		},
+	}
+
+	normalDeps = []ftypes.Dependency(nil)
+)
