53ea7bc572abe5c6bd6e96de911667182d337ee4
Ori Zerah <ori.zerah@echohq.com>
2025-05-20 13:44:50 +0300

    add debian sid mapping to debian detector

--- pkg/detector/ospkg/debian/debian_test.go
+++ pkg/detector/ospkg/debian/debian_test.go
@@ -87,6 +87,62 @@ func TestScanner_Detect(t *testing.T) {
 			},
 		},
 		{
+			name: "sid",
+			fixtures: []string{
+				"testdata/fixtures/data-source.yaml",
+				"testdata/fixtures/sid.yaml",
+			},
+			args: args{
+				osVer: "trixie/sid",
+				pkgs: []ftypes.Package{
+					{
+						Name:       "htpasswd",
+						Version:    "2.4.24",
+						SrcName:    "apache2",
+						SrcVersion: "2.4.24",
+						Layer: ftypes.Layer{
+							DiffID: "sha256:932da51564135c98a49a34a193d6cd363d8fa4184d957fde16c9d8527b3f3b02",
+						},
+					},
+				},
+			},
+			want: []types.DetectedVulnerability{
+				{
+					PkgName:          "htpasswd",
+					VulnerabilityID:  "CVE-2020-11985",
+					VendorIDs:        []string{"DSA-4884-1"},
+					InstalledVersion: "2.4.24",
+					FixedVersion:     "2.4.25-1",
+					Layer: ftypes.Layer{
+						DiffID: "sha256:932da51564135c98a49a34a193d6cd363d8fa4184d957fde16c9d8527b3f3b02",
+					},
+					DataSource: &dbTypes.DataSource{
+						ID:   vulnerability.Debian,
+						Name: "Debian Security Tracker",
+						URL:  "https://salsa.debian.org/security-tracker-team/security-tracker",
+					},
+				},
+				{
+					PkgName:          "htpasswd",
+					VulnerabilityID:  "CVE-2021-31618",
+					InstalledVersion: "2.4.24",
+					Status:           dbTypes.StatusWillNotFix,
+					SeveritySource:   vulnerability.Debian,
+					Vulnerability: dbTypes.Vulnerability{
+						Severity: dbTypes.SeverityMedium.String(),
+					},
+					Layer: ftypes.Layer{
+						DiffID: "sha256:932da51564135c98a49a34a193d6cd363d8fa4184d957fde16c9d8527b3f3b02",
+					},
+					DataSource: &dbTypes.DataSource{
+						ID:   vulnerability.Debian,
+						Name: "Debian Security Tracker",
+						URL:  "https://salsa.debian.org/security-tracker-team/security-tracker",
+					},
+				},
+			},
+		},
+		{
 			name: "invalid bucket",
 			fixtures: []string{
 				"testdata/fixtures/invalid.yaml",
