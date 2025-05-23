07ef63b4830f9f3d791a07433287a99118d7590a
David du Colombier <0intro@gmail.com>
2025-05-23 09:50:59 +0200

    feat: add Bottlerocket OS package analyzer (#8653)

--- pkg/fanal/analyzer/os/release/release_test.go
+++ pkg/fanal/analyzer/os/release/release_test.go
@@ -150,6 +150,16 @@ func Test_osReleaseAnalyzer_Analyze(t *testing.T) {
 			},
 		},
 		{
+			name:      "Bottlerocket",
+			inputFile: "testdata/bottlerocket",
+			want: &analyzer.AnalysisResult{
+				OS: types.OS{
+					Family: types.Bottlerocket,
+					Name:   "1.34.0",
+				},
+			},
+		},
+		{
 			name:      "Unknown OS",
 			inputFile: "testdata/unknown",
 			want:      nil,
