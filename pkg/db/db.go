4aeb7258d6b709e80ed0925333797a2371677a4a
DmitriyLewen <dmitriy.lewen@smartforce.io>
2025-06-05 12:57:19 +0600

    fix(db): fix confusing log messages - don't show log about corrupted trivy-db for empty db dir - don't show `Skipping DB update...` if metadata.json is invalid

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -110,11 +110,12 @@ func (c *Client) NeedsUpdate(ctx context.Context, cliVersion string, skip bool) 
 		meta = metadata.Metadata{Version: db.SchemaVersion}
 	}
 
-	// There are 2 cases when DownloadAt field is zero:
+	// There are 3 cases when DownloadAt field is zero:
+	// - metadata file was not created yet. This is the first run of Trivy.
 	// - trivy-db was downloaded with `oras`. In this case user can use `--skip-db-update` (like for air-gapped) or re-download trivy-db.
 	// - trivy-db was corrupted while copying from tmp directory to cache directory. We should update this trivy-db.
 	// We can't detect these cases, so we will show warning for users who use oras + air-gapped.
-	if meta.DownloadedAt.IsZero() && !skip {
+	if !noRequiredFiles && meta.DownloadedAt.IsZero() && !skip {
 		log.WarnContext(ctx, "Trivy DB may be corrupted and will be re-downloaded. If you manually downloaded DB - use the `--skip-db-update` flag to skip updating DB.")
 		return true, nil
 	}
@@ -131,10 +132,11 @@ func (c *Client) NeedsUpdate(ctx context.Context, cliVersion string, skip bool) 
 	}
 
 	if skip {
-		log.DebugContext(ctx, "Skipping DB update...")
 		if err = c.validate(meta); err != nil {
 			return false, xerrors.Errorf("validate error: %w", err)
 		}
+
+		log.DebugContext(ctx, "Skipping DB update...")
 		return false, nil
 	}
 
