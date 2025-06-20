79d5363340a22b55795d44dfb131b9b216adc8bf
Ali Yachnes <ali@arnica.io>
2025-06-20 18:07:29 -0400

    Update tag.sh

--- tag.sh
+++ tag.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
 
-git tag --force v0.56.2-maven-patch
+TAG="v0.56.2-maven-patch"
+
+sed -i '' -E "s/(var ver = \")[^\"]+(\")/\1$TAG\2/" pkg/version/app/version.go
+
+if ! git diff --quiet pkg/version/app/version.go; then
+  git add pkg/version/app/version.go
+  git commit -m "Update tag to $TAG"
+  git push
+else
+  echo "version.go is already up-to-date"
+fi
+
+git tag --force $TAG
 git push --force --tags
