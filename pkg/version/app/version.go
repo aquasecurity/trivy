458f71cb81b73d0c967a831c765f7a769ba29c23
Ali Yachnes <ali@arnica.io>
2025-06-23 09:49:30 -0400

    Explanatory comment

--- pkg/version/app/version.go
+++ pkg/version/app/version.go
@@ -1,5 +1,7 @@
 package app
 
+// For some reason, the version is not being injected from git tags through goreleaser
+// The version is hardcoded here for now
 var ver = "v0.56.2-maven-patch"
 
 func Version() string {
