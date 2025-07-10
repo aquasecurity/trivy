dea3ba7558539222d5d6738e394b2bcddad24733
hoangvu-circle <hoang.vu@circle.com>
2025-07-10 01:07:09 -0500

    fix(aws): update amazon linux 2 EOL date (#9176)

--- pkg/detector/ospkg/amazon/amazon.go
+++ pkg/detector/ospkg/amazon/amazon.go
@@ -21,7 +21,7 @@ var (
 		// https://aws.amazon.com/jp/blogs/aws/update-on-amazon-linux-ami-end-of-life/
 		"1": time.Date(2023, 12, 31, 23, 59, 59, 0, time.UTC),
 		// https://aws.amazon.com/amazon-linux-2/faqs/?nc1=h_ls
-		"2": time.Date(2025, 6, 30, 23, 59, 59, 0, time.UTC),
+		"2": time.Date(2026, 6, 30, 23, 59, 59, 0, time.UTC),
 		// Amazon Linux 2022 was renamed to 2023. AL2022 is not currently supported.
 		"2023": time.Date(2028, 3, 15, 23, 59, 59, 0, time.UTC),
 	}
