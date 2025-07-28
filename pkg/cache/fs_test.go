74777b9d2fedc9302fe91ef50da1c26cf4e6948c
knqyf263 <knqyf263@gmail.com>
2025-07-28 11:10:39 +0400

    test: fix tests after omitempty to omitzero changes
    - Remove empty Identifier and Layer fields from fs_test.go expectations
    - Remove empty Layer and CauseMetadata fields from compliance report test data
    - Update SHA256 hashes in remote_sbom_test.go and vm_test.go due to JSON serialization changes

--- pkg/cache/fs_test.go
+++ pkg/cache/fs_test.go
@@ -227,9 +227,7 @@ func TestFSCache_PutBlob(t *testing.T) {
 				      "Packages": [
 				        {
 				          "Name": "musl",
-				          "Version": "1.1.22-r3",
-                          "Identifier": {},
-						  "Layer": {}
+				          "Version": "1.1.22-r3"
 				        }
 				      ]
 				    }
@@ -241,15 +239,11 @@ func TestFSCache_PutBlob(t *testing.T) {
 				      "Packages": [
                         {
                            "Name":"guzzlehttp/guzzle",
-                           "Version":"6.2.0",
-                           "Identifier": {},
-						   "Layer": {}
+                           "Version":"6.2.0"
                         },
                         {
                            "Name":"guzzlehttp/promises",
-                           "Version":"v1.3.1",
-                           "Identifier": {},
-						   "Layer": {}
+                           "Version":"v1.3.1"
                         }
 				      ]
 				    }
@@ -347,9 +341,7 @@ func TestFSCache_PutArtifact(t *testing.T) {
 				  "HistoryPackages": [
 				    {
 				      "Name": "musl",
-				      "Version": "1.2.3",
-                      "Identifier": {},
-					  "Layer": {}
+				      "Version": "1.2.3"
 				    }
 				  ]
 				}
