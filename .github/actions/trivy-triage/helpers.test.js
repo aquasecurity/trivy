6f7af56adc66a194c48335e27c3f6518a4a7ce1c
Nikita Pivkin <nikita.pivkin@smartforce.io>
2025-07-29 17:50:48 +0600

    ci: skip undefined labels in discussion triage action (#9175)
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>
    Co-authored-by: Itay <itay@itaysk.com>

--- .github/actions/trivy-triage/helpers.test.js
+++ .github/actions/trivy-triage/helpers.test.js
@@ -62,6 +62,17 @@ describe('trivy-triage', async function() {
       assert(labels.includes('ContainerImageLabel'));
       assert(labels.includes('VulnerabilityLabel'));
     });
+    it('detect scanner and target labels on windows', async function() {
+      const discussion = {
+        body: 'hello hello\r\nbla bla.\r\n### Scanner\r\n\r\nVulnerability\r\n### Target\r\n\r\nContainer Image\r\nbye bye.',
+        category: {
+          name: 'Ideas'
+        }
+      };
+      const labels = detectDiscussionLabels(discussion, configDiscussionLabels);
+      assert(labels.includes('ContainerImageLabel'));
+      assert(labels.includes('VulnerabilityLabel'));
+    });
     it('not detect other labels', async function() {
       const discussion = {
         body: 'hello hello\nbla bla.\n### Scanner\n\nVulnerability\n### Target\n\nContainer Image\nbye bye.', 
@@ -72,6 +83,16 @@ describe('trivy-triage', async function() {
       const labels = detectDiscussionLabels(discussion, configDiscussionLabels);
       assert(!labels.includes('FilesystemLabel'));
       assert(!labels.includes('MisconfigurationLabel'));
+    });
+    it('ignores unmatched label values from body', async function() {
+      const discussion = {
+        body: '### Target\r\n\r\nNone\r\n\r\n### Scanner\r\n\r\nMisconfiguration', 
+        category: {
+          name: 'Ideas'
+        }
+      };
+      const labels = detectDiscussionLabels(discussion, configDiscussionLabels);
+      assert.deepStrictEqual(labels, ['MisconfigurationLabel']);
     });
     it('process only relevant categories', async function() {
       const discussion = {
