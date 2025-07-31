669832e8e8bdfbeeaca79ff7c8fc9540c5345e8d
DmitriyLewen <dmitriy.lewen@smartforce.io>
2025-07-31 15:31:07 +0600

    refactor(python): select lower case for normalize package name

--- pkg/dependency/parser/python/python.go
+++ pkg/dependency/parser/python/python.go
@@ -7,9 +7,16 @@ import (
 
 var normalizePkgNameRegexp = regexp.MustCompile(`[-_.]+`)
 
-// NormalizePkgName normalizes the package name based on pep-0503
-// https://peps.python.org/pep-0503/#normalized-names:
+// NormalizePkgName normalizes the package name based on pep-0503 (with the option to disable conversion to lowercase).
+// cf. https://peps.python.org/pep-0503/#normalized-names:
 // The name should be lowercased with all runs of the characters ., -, or _ replaced with a single - character.
-func NormalizePkgName(name string) string {
-	return strings.ToLower(normalizePkgNameRegexp.ReplaceAllString(name, "-"))
+func NormalizePkgName(name string, inLowerCase bool) string {
+	name = normalizePkgNameRegexp.ReplaceAllString(name, "-")
+
+	// pep-0503 requires that all packages names MUST be lowercase.
+	// But there are cases where the original case should be preserved (e.g. dist-info dir names).
+	if inLowerCase {
+		name = strings.ToLower(name)
+	}
+	return name
 }
