9425d3e54f6ae8c26a8bacd762e7c1ed2e685dba
nikpivkin <nikita.pivkin@smartforce.io>
2025-08-18 18:45:27 +0600

    refactor(test): replace cmp.Diff with assert.Equal
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>

--- pkg/iac/scanners/ansible/parser/node_test.go
+++ pkg/iac/scanners/ansible/parser/node_test.go
@@ -3,8 +3,7 @@ package parser
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v3"
 )
@@ -85,18 +84,11 @@ state:
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var attr Node
-			err := yaml.Unmarshal([]byte(tt.src), &attr)
+			var node Node
+			err := yaml.Unmarshal([]byte(tt.src), &node)
 			require.NoError(t, err)
 
-			diff := cmp.Diff(tt.expected, attr,
-				cmp.AllowUnexported(Node{}, Range{}),
-				cmpopts.IgnoreFields(Node{}, "metadata"),
-			)
-
-			if diff != "" {
-				t.Error(diff)
-			}
+			assert.Equal(t, tt.expected, node)
 		})
 	}
 }
