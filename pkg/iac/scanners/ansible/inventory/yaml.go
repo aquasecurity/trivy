11ff4b2a1b291936766245b0a95bb53caa654d79
nikpivkin <nikita.pivkin@smartforce.io>
2025-08-25 15:28:10 +0600

    refactor: move OrderedMap to separate package
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>

--- pkg/iac/scanners/ansible/inventory/yaml.go
+++ pkg/iac/scanners/ansible/inventory/yaml.go
@@ -1,81 +1,22 @@
 package inventory
 
 import (
-	"iter"
-
 	"golang.org/x/xerrors"
 	"gopkg.in/yaml.v3"
 
+	"github.com/aquasecurity/trivy/pkg/iac/scanners/ansible/orderedmap"
 	"github.com/aquasecurity/trivy/pkg/iac/scanners/ansible/vars"
 	"github.com/aquasecurity/trivy/pkg/set"
 )
 
-// OrderedMap is a map that preserves insertion order of keys.
-type OrderedMap[K comparable, V any] struct {
-	keys []K
-	kv   map[K]V
-}
-
-func NewOrderedMap[K comparable, V any](keys []K, kv map[K]V) *OrderedMap[K, V] {
-	return &OrderedMap[K, V]{keys: keys, kv: kv}
-}
-
-// Len returns the number of entires in the map
-func (m *OrderedMap[K, V]) Len() int {
-	return len(m.keys)
-}
-
-// Iter returns an iterator over the map
-func (m *OrderedMap[K, V]) Iter() iter.Seq2[K, V] {
-	return func(yield func(K, V) bool) {
-		for _, k := range m.keys {
-			v := m.kv[k]
-			if !yield(k, v) {
-				return
-			}
-		}
-	}
-}
-
-func (m *OrderedMap[K, V]) UnmarshalYAML(n *yaml.Node) error {
-	if n.Kind != yaml.MappingNode {
-		return xerrors.Errorf("expected map node, got %s", n.Tag)
-	}
-
-	if len(n.Content)%2 != 0 {
-		return xerrors.New("invalid map node content length")
-	}
-
-	size := len(n.Content) / 2
-	m.keys = make([]K, 0, size)
-	m.kv = make(map[K]V, size)
-
-	for i := 0; i < len(n.Content); i += 2 {
-		keyNode, valueNode := n.Content[i], n.Content[i+1]
-
-		var key K
-		if err := keyNode.Decode(&key); err != nil {
-			return err
-		}
-		m.keys = append(m.keys, key)
-
-		var value V
-		if err := valueNode.Decode(&value); err != nil {
-			return err
-		}
-		m.kv[key] = value
-	}
-	return nil
-}
-
 type rawGroup struct {
-	Hosts    map[string]vars.Vars         `yaml:"hosts"`
-	Children OrderedMap[string, rawGroup] `yaml:"children"`
-	Vars     vars.Vars                    `yaml:"vars"`
+	Hosts    map[string]vars.Vars                    `yaml:"hosts"`
+	Children orderedmap.OrderedMap[string, rawGroup] `yaml:"children"`
+	Vars     vars.Vars                               `yaml:"vars"`
 }
 
 func ParseYAML(data []byte) (*Inventory, error) {
-	var raw OrderedMap[string, rawGroup]
+	var raw orderedmap.OrderedMap[string, rawGroup]
 	if err := yaml.Unmarshal(data, &raw); err != nil {
 		return nil, xerrors.Errorf("unmarshal inventory yaml: %w", err)
 	}
