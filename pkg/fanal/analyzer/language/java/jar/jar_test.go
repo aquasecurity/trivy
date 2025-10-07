ed458705bdacf967fdc3ea3894be8e3f4ab6c801
Samuel Veloso <veloso.lopez.samuel@gmail.com>
2025-10-07 09:50:46 +0200

    feat(java): do not use the java-db in offline mode

--- pkg/fanal/analyzer/language/java/jar/jar_test.go
+++ pkg/fanal/analyzer/language/java/jar/jar_test.go
@@ -151,6 +151,138 @@ func Test_javaLibraryAnalyzer_Analyze(t *testing.T) {
 	}
 }
 
+func Test_javaLibraryAnalyzer_Analyze_Offline(t *testing.T) {
+	tests := []struct {
+		name            string
+		inputFile       string
+		includeChecksum bool
+		want            *analyzer.AnalysisResult
+	}{
+		{
+			name:      "happy path (WAR file)",
+			inputFile: "testdata/test.war",
+			want: &analyzer.AnalysisResult{
+				Applications: []types.Application{
+					{
+						Type:     types.Jar,
+						FilePath: "testdata/test.war",
+						Packages: types.Packages{
+							{
+								Name:     "org.glassfish:javax.el",
+								FilePath: "testdata/test.war/WEB-INF/lib/javax.el-3.0.0.jar",
+								Version:  "3.0.0",
+							},
+							{
+								Name:     "com.fasterxml.jackson.core:jackson-databind",
+								FilePath: "testdata/test.war/WEB-INF/lib/jackson-databind-2.9.10.6.jar",
+								Version:  "2.9.10.6",
+							},
+							{
+								Name:     "com.fasterxml.jackson.core:jackson-annotations",
+								FilePath: "testdata/test.war/WEB-INF/lib/jackson-annotations-2.9.10.jar",
+								Version:  "2.9.10",
+							},
+							{
+								Name:     "com.fasterxml.jackson.core:jackson-core",
+								FilePath: "testdata/test.war/WEB-INF/lib/jackson-core-2.9.10.jar",
+								Version:  "2.9.10",
+							},
+							{
+								Name:     "org.slf4j:slf4j-api",
+								FilePath: "testdata/test.war/WEB-INF/lib/slf4j-api-1.7.30.jar",
+								Version:  "1.7.30",
+							},
+							{
+								Name:     "com.cronutils:cron-utils",
+								FilePath: "testdata/test.war/WEB-INF/lib/cron-utils-9.1.2.jar",
+								Version:  "9.1.2",
+							},
+							{
+								Name:     "org.apache.commons:commons-lang3",
+								FilePath: "testdata/test.war/WEB-INF/lib/commons-lang3-3.11.jar",
+								Version:  "3.11",
+							},
+							{
+								Name:     "com.example:web-app",
+								FilePath: "testdata/test.war",
+								Version:  "1.0-SNAPSHOT",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:            "happy path (PAR file)",
+			inputFile:       "testdata/test.par",
+			includeChecksum: true,
+			want: &analyzer.AnalysisResult{
+				Applications: []types.Application{
+					{
+						Type:     types.Jar,
+						FilePath: "testdata/test.par",
+						Packages: types.Packages{
+							{
+								Name:     "com.fasterxml.jackson.core:jackson-core",
+								FilePath: "testdata/test.par/lib/jackson-core-2.9.10.jar",
+								Version:  "2.9.10",
+								Digest:   "sha1:d40913470259cfba6dcc90f96bcaa9bcff1b72e0",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:      "happy path (JAR file)",
+			inputFile: "testdata/test.jar",
+			want: &analyzer.AnalysisResult{
+				Applications: []types.Application{
+					{
+						Type:     types.Jar,
+						FilePath: "testdata/test.jar",
+						Packages: types.Packages{
+							{
+								Name:     "org.apache:javax.websocket",
+								FilePath: "testdata/test.jar",
+								Version:  "1.1.FR",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:      "sad path",
+			inputFile: "testdata/test.txt",
+			want:      &analyzer.AnalysisResult{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := javaLibraryAnalyzer{}
+			ctx := t.Context()
+
+			mfs := mapfs.New()
+			err := mfs.MkdirAll(filepath.Dir(tt.inputFile), os.ModePerm)
+			require.NoError(t, err)
+			err = mfs.WriteFile(tt.inputFile, tt.inputFile)
+			require.NoError(t, err)
+
+			got, err := a.PostAnalyze(ctx, analyzer.PostAnalysisInput{
+				FS: mfs,
+				Options: analyzer.AnalysisOptions{
+					Offline:      true,
+					FileChecksum: tt.includeChecksum,
+				},
+			})
+
+			require.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
 func Test_javaLibraryAnalyzer_Required(t *testing.T) {
 	tests := []struct {
 		name     string
