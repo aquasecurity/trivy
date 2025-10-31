790eaa7a430f9b95370d9a13695174b13c09b6d3
DmitriyLewen <dmitriy.lewen@smartforce.io>
2025-10-31 12:01:21 +0600

    test: add npm test cases

--- pkg/detector/library/rootio/rootio_test.go
+++ pkg/detector/library/rootio/rootio_test.go
@@ -25,7 +25,7 @@ func TestScanner_Detect(t *testing.T) {
 		wantVulns []types.DetectedVulnerability
 	}{
 		{
-			name: "Root.io pip package",
+			name: "Root.io pip package with Root.io and GitHub vulns",
 			fixtures: []string{
 				"testdata/fixtures/pip.yaml",
 				"testdata/fixtures/rootio.yaml",
@@ -41,6 +41,7 @@ func TestScanner_Detect(t *testing.T) {
 			},
 			wantVulns: []types.DetectedVulnerability{
 				{
+					// Vulnerability from GitHub Security Advisory Pip
 					VulnerabilityID:  "CVE-2022-1234",
 					PkgName:          "django",
 					InstalledVersion: "4.0.1+root.io.1",
@@ -55,10 +56,11 @@ func TestScanner_Detect(t *testing.T) {
 					},
 				},
 				{
+					// Vulnerability from Root.io pip
 					VulnerabilityID:  "CVE-2022-5678",
 					PkgName:          "django",
 					InstalledVersion: "4.0.1+root.io.1",
-					FixedVersion:     "4.0.2.post1",
+					FixedVersion:     "4.0.1+root.io.2",
 					Layer:            ftypes.Layer{Digest: "sha256:layer"},
 					PkgPath:          "/path/to/requirements.txt",
 					PkgIdentifier:    ftypes.PkgIdentifier{UID: "01"},
@@ -69,10 +71,12 @@ func TestScanner_Detect(t *testing.T) {
 					},
 				},
 				{
+					// GitHub Security Advisory Pip and Root.io pip have the same CVE
+					// Use  Root.io pip
 					VulnerabilityID:  "CVE-2023-1234",
 					PkgName:          "django",
 					InstalledVersion: "4.0.1+root.io.1",
-					FixedVersion:     "4.0.2.post1",
+					FixedVersion:     "4.0.1+root.io.2",
 					Layer:            ftypes.Layer{Digest: "sha256:layer"},
 					PkgPath:          "/path/to/requirements.txt",
 					PkgIdentifier:    ftypes.PkgIdentifier{UID: "01"},
@@ -80,6 +84,70 @@ func TestScanner_Detect(t *testing.T) {
 						ID:   vulnerability.RootIO,
 						Name: "root.io pip",
 						URL:  "https://api.root.io/external/patch_feed",
+					},
+				},
+			},
+		},
+		{
+			name: "pip package with Root.io vulns",
+			fixtures: []string{
+				"testdata/fixtures/pip.yaml",
+				"testdata/fixtures/rootio.yaml",
+				"testdata/fixtures/data-source.yaml",
+			},
+			ecosystem: ecosystem.Pip,
+			pkg: ftypes.Package{
+				Name:       "requests",
+				Version:    "2.30.0",
+				Layer:      ftypes.Layer{Digest: "sha256:layer"},
+				FilePath:   "/path/to/requirements.txt",
+				Identifier: ftypes.PkgIdentifier{UID: "01"},
+			},
+			wantVulns: []types.DetectedVulnerability{
+				{
+					VulnerabilityID:  "CVE-2023-32681",
+					PkgName:          "requests",
+					InstalledVersion: "2.30.0",
+					FixedVersion:     "2.30.0+root.io.3",
+					Layer:            ftypes.Layer{Digest: "sha256:layer"},
+					PkgPath:          "/path/to/requirements.txt",
+					PkgIdentifier:    ftypes.PkgIdentifier{UID: "01"},
+					DataSource: &dbTypes.DataSource{
+						ID:   vulnerability.RootIO,
+						Name: "root.io pip",
+						URL:  "https://api.root.io/external/patch_feed",
+					},
+				},
+			},
+		},
+		{
+			name: "Root.io npm package with Root.io and GitHub vulns",
+			fixtures: []string{
+				//"testdata/fixtures/npm.yaml",
+				"testdata/fixtures/rootio.yaml",
+				"testdata/fixtures/data-source.yaml",
+			},
+			ecosystem: ecosystem.Npm,
+			pkg: ftypes.Package{
+				Name:       "lodash",
+				Version:    "4.17.20+root.io.1",
+				Layer:      ftypes.Layer{Digest: "sha256:layer"},
+				FilePath:   "/path/to/package.json",
+				Identifier: ftypes.PkgIdentifier{UID: "01"},
+			},
+			wantVulns: []types.DetectedVulnerability{
+				{
+					VulnerabilityID:  "CVE-2020-28500",
+					PkgName:          "lodash",
+					InstalledVersion: "4.17.20+root.io.1",
+					FixedVersion:     "4.17.21",
+					Layer:            ftypes.Layer{Digest: "sha256:layer"},
+					PkgPath:          "/path/to/package.json",
+					PkgIdentifier:    ftypes.PkgIdentifier{UID: "01"},
+					DataSource: &dbTypes.DataSource{
+						ID:   vulnerability.GHSA,
+						Name: "GitHub Security Advisory npm",
+						URL:  "https://github.com/advisories?query=type%3Areviewed+ecosystem%3Anpm",
 					},
 				},
 			},
