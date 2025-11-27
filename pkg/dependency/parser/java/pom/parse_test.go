ec428df47be863480cbf3162b555d6791fe86f12
DmitriyLewen <dmitriy.lewen@smartforce.io>
2025-11-27 16:24:43 +0600

    test: add module tests from #7879

--- pkg/dependency/parser/java/pom/parse_test.go
+++ pkg/dependency/parser/java/pom/parse_test.go
@@ -1304,6 +1304,191 @@ func TestPom_Parse(t *testing.T) {
 			},
 		},
 		{
+			name:      "multi module with deps with same GAV",
+			inputFile: filepath.Join("testdata", "multiple-modules-with-deps-with-same-gav", "pom.xml"),
+			local:     true,
+			want: []ftypes.Package{
+				{
+					ID:           "af5c1bca8a964540",
+					Name:         "com.example:root",
+					Version:      "1.0.0",
+					Relationship: ftypes.RelationshipRoot,
+				},
+				{
+					ID:           "9f9255f9043d446f",
+					Name:         "com.example:module1",
+					Version:      "1.0.0",
+					Relationship: ftypes.RelationshipWorkspace,
+				},
+				{
+					ID:           "84ab73e1b93aa7f5",
+					Name:         "com.example:module2",
+					Version:      "2.0.0",
+					Relationship: ftypes.RelationshipWorkspace,
+				},
+				{
+					ID:           "b80d1889cd7ec6f3",
+					Name:         "org.example:example-api",
+					Version:      "1.7.30",
+					Licenses:     []string{"The Apache Software License, Version 2.0"},
+					Relationship: ftypes.RelationshipDirect,
+				},
+				{
+					ID:           "b80d1889cd7ec6f3",
+					Name:         "org.example:example-api",
+					Version:      "1.7.30",
+					Licenses:     []string{"The Apache Software License, Version 2.0"},
+					Relationship: ftypes.RelationshipDirect,
+				},
+			},
+			//[INFO] --------------------------------[ pom ]---------------------------------
+			//[INFO]
+			//[INFO] --- dependency:3.7.0:tree (default-cli) @ module1 ---
+			//[INFO] com.example:module1:pom:1.0.0
+			//[INFO] \- org.example:example-api:jar:1.7.30:compile
+			//[INFO] --------------------------------[ pom ]---------------------------------
+			//[INFO]
+			//[INFO] --- dependency:3.7.0:tree (default-cli) @ module2 ---
+			//[INFO] com.example:module2:pom:2.0.0
+			//[INFO] \- org.example:example-api:jar:1.7.30:compile
+			//[INFO]
+			//[INFO] --------------------------------[ pom ]---------------------------------
+			//[INFO]
+			//[INFO] --- dependency:3.7.0:tree (default-cli) @ root ---
+			//[INFO] com.example:root:pom:1.0.0
+			//[INFO] ------------------------------------------------------------------------
+			wantDeps: []ftypes.Dependency{
+				{
+					ID: "af5c1bca8a964540",
+					DependsOn: []string{
+						"9f9255f9043d446f",
+						"84ab73e1b93aa7f5",
+					},
+				},
+				{
+					ID: "9f9255f9043d446f",
+					DependsOn: []string{
+						"b80d1889cd7ec6f3",
+					},
+				},
+				{
+					ID: "84ab73e1b93aa7f5",
+					DependsOn: []string{
+						"b80d1889cd7ec6f3",
+					},
+				},
+			},
+		},
+		{
+			name:      "multi module with similar deps, but different children",
+			inputFile: filepath.Join("testdata", "multiple-modules-with-deps-with-same-gav-with-props", "pom.xml"),
+			local:     true,
+			want: []ftypes.Package{
+				{
+					ID:           "af5c1bca8a964540",
+					Name:         "com.example:root",
+					Version:      "1.0.0",
+					Relationship: ftypes.RelationshipRoot,
+				},
+				{
+					ID:           "1c8d2e652e50cfcf",
+					Name:         "com.example:module1",
+					Version:      "1.0.0",
+					Relationship: ftypes.RelationshipWorkspace,
+				},
+				{
+					ID:           "bbc7734e78f26979",
+					Name:         "com.example:module2",
+					Version:      "2.0.0",
+					Relationship: ftypes.RelationshipWorkspace,
+				},
+				{
+					ID:           "b8bf69bea1855d36",
+					Name:         "org.example:example-dependency",
+					Version:      "1.2.5",
+					Relationship: ftypes.RelationshipDirect,
+				},
+				{
+					ID:           "9b8caa828f0d4a32",
+					Name:         "org.example:example-dependency",
+					Version:      "1.2.5",
+					Relationship: ftypes.RelationshipDirect,
+				},
+				{
+					ID:           "b80d1889cd7ec6f3",
+					Name:         "org.example:example-api",
+					Version:      "1.7.30",
+					Licenses:     []string{"The Apache Software License, Version 2.0"},
+					Relationship: ftypes.RelationshipIndirect,
+				},
+				{
+					ID:           "fda861a0a2dc688a",
+					Name:         "org.example:example-api",
+					Version:      "2.0.0",
+					Licenses:     []string{"The Apache Software License, Version 2.0"},
+					Relationship: ftypes.RelationshipIndirect,
+				},
+			},
+			//[INFO] --------------------------------[ jar ]---------------------------------
+			//[INFO]
+			//[INFO] --- dependency:3.7.0:tree (default-cli) @ module1 ---
+			//[INFO] com.example:module1:jar:1.0.0
+			//[INFO] \- org.example:example-dependency:jar:1.2.5:compile
+			//[INFO]    \- org.example:example-api:jar:1.7.30:compile
+			//[INFO]
+			//[INFO] ------------------------< com.example:module2 >-------------------------
+			//[INFO] Building module2 2.0.0                                             [2/3]
+			//[INFO]   from module2/pom.xml
+			//[INFO] --------------------------------[ jar ]---------------------------------
+			//[INFO]
+			//[INFO] --- dependency:3.7.0:tree (default-cli) @ module2 ---
+			//[INFO] com.example:module2:jar:2.0.0
+			//[INFO] \- org.example:example-dependency:jar:1.2.5:compile
+			//[INFO]    \- org.example:example-api:jar:2.0.0:compile
+			//[INFO]
+			//[INFO] --------------------------< com.example:root >--------------------------
+			//[INFO] Building root 1.0.0                                                [3/3]
+			//[INFO]   from pom.xml
+			//[INFO] --------------------------------[ pom ]---------------------------------
+			//[INFO]
+			//[INFO] --- dependency:3.7.0:tree (default-cli) @ root ---
+			//[INFO] com.example:root:pom:1.0.0
+			//[INFO] ------------------------------------------------------------------------
+			wantDeps: []ftypes.Dependency{
+				{
+					ID: "1c8d2e652e50cfcf",
+					DependsOn: []string{
+						"b8bf69bea1855d36",
+					},
+				},
+				{
+					ID: "9b8caa828f0d4a32",
+					DependsOn: []string{
+						"fda861a0a2dc688a",
+					},
+				},
+				{
+					ID: "af5c1bca8a964540",
+					DependsOn: []string{
+						"1c8d2e652e50cfcf",
+						"bbc7734e78f26979",
+					},
+				},
+				{
+					ID: "b8bf69bea1855d36",
+					DependsOn: []string{
+						"b80d1889cd7ec6f3",
+					},
+				},
+				{
+					ID: "bbc7734e78f26979",
+					DependsOn: []string{
+						"9b8caa828f0d4a32",
+					},
+				},
+			},
+		},
+		{
 			name:      "nested modules",
 			inputFile: filepath.Join("testdata", "nested-modules", "pom.xml"),
 			local:     true,
