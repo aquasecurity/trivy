73d7f425c5fa558c9ff2bf358354d3a89d133cea
DmitriyLewen <dmitriy.lewen@smartforce.io>
2025-12-15 15:38:43 +0600

    refactor: add separate function to normalize licenses

--- pkg/fanal/analyzer/language/analyze.go
+++ pkg/fanal/analyzer/language/analyze.go
@@ -104,6 +104,7 @@ func toApplication(fileType types.LangType, filePath, libFilePath string, r xio.
 		pkgs[i].DependsOn = deps[pkg.ID]
 		pkgs[i].Digest = d
 		pkgs[i].Indirect = isIndirect(pkg.Relationship) // For backward compatibility
+		NormalizeLicenses(pkgs[i].Licenses)
 
 		for j, license := range pkg.Licenses {
 			pkgs[i].Licenses[j] = licensing.Normalize(license)
@@ -135,5 +136,11 @@ func isIndirect(rel types.Relationship) bool {
 		return true
 	default:
 		return false
+	}
+}
+
+func NormalizeLicenses(licenses []string) {
+	for j, license := range licenses {
+		licenses[j] = licensing.Normalize(license)
 	}
 }
