0dc33718cde636e0ea5ee7726f6d16bdb2a5762a
nikpivkin <nikita.pivkin@smartforce.io>
2025-12-24 15:48:51 +0600

    refactor(misconf): move common logic to base value and simplify typed values
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>

--- pkg/iac/types/bool.go
+++ pkg/iac/types/bool.go
@@ -1,81 +1,33 @@
 package types
 
 import (
-	"encoding/json"
 	"strings"
 
 	"github.com/zclconf/go-cty/cty"
 )
 
 type BoolValue struct {
-	BaseAttribute
-	value bool
+	BaseValue[bool]
 }
 
-func (b BoolValue) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]any{
-		"value":    b.value,
-		"metadata": b.metadata,
-	})
+func Bool(value bool, m Metadata) BoolValue {
+	return BoolValue{newValue(value, m)}
 }
 
-func (b *BoolValue) UnmarshalJSON(data []byte) error {
-	var keys map[string]any
-	if err := json.Unmarshal(data, &keys); err != nil {
-		return err
-	}
-	if keys["value"] != nil {
-		b.value = keys["value"].(bool)
-	}
-	if keys["metadata"] != nil {
-		raw, err := json.Marshal(keys["metadata"])
-		if err != nil {
-			return err
-		}
-		var m Metadata
-		if err := json.Unmarshal(raw, &m); err != nil {
-			return err
-		}
-		b.metadata = m
-	}
-	return nil
-}
-
-func Bool(value bool, metadata Metadata) BoolValue {
-	return BoolValue{
-		value:         value,
-		BaseAttribute: BaseAttribute{metadata: metadata},
-	}
-}
-
-func BoolTest(value bool) BoolValue {
-	return Bool(value, NewTestMetadata())
-}
-
-func BoolDefault(value bool, metadata Metadata) BoolValue {
-	b := Bool(value, metadata)
-	b.BaseAttribute.metadata.isDefault = true
-	return b
+func BoolDefault(value bool, m Metadata) BoolValue {
+	return BoolValue{defaultValue(value, m)}
 }
 
 func BoolUnresolvable(m Metadata) BoolValue {
-	b := Bool(false, m)
-	b.BaseAttribute.metadata.isUnresolvable = true
-	return b
+	return BoolValue{unresolvableValue[bool](m)}
 }
 
-func BoolExplicit(value bool, metadata Metadata) BoolValue {
-	b := Bool(value, metadata)
-	b.BaseAttribute.metadata.isExplicit = true
-	return b
+func BoolExplicit(value bool, m Metadata) BoolValue {
+	return BoolValue{explicitValue(value, m)}
 }
 
-func (b BoolValue) Value() bool {
-	return b.value
-}
-
-func (b BoolValue) GetRawValue() any {
-	return b.value
+func BoolTest(value bool) BoolValue {
+	return BoolValue{testValue(value)}
 }
 
 func (b BoolValue) IsTrue() bool {
@@ -93,16 +45,7 @@ func (b BoolValue) IsFalse() bool {
 }
 
 func (b BoolValue) Invert() BoolValue {
-	return BoolValue{
-		BaseAttribute: b.BaseAttribute,
-		value:         !b.value,
-	}
-}
-
-func (b BoolValue) ToRego() any {
-	m := b.metadata.ToRego().(map[string]any)
-	m["value"] = b.Value()
-	return m
+	return Bool(!b.value, b.metadata)
 }
 
 // BoolFromCtyValue converts a cty.Value to iacTypes.BoolValue.
