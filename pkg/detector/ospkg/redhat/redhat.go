9d345b2c54aed7750294d5a23c1491308f505709
DmitriyLewen <dmitriy.lewen@smartforce.io>
2026-02-03 16:24:22 +0600

    refactor: use datasource from trivy-db

--- pkg/detector/ospkg/redhat/redhat.go
+++ pkg/detector/ospkg/redhat/redhat.go
@@ -153,11 +153,7 @@ func (s *Scanner) detect(osVer string, pkg ftypes.Package) ([]types.DetectedVuln
 			Status:           adv.Status,
 			Layer:            pkg.Layer,
 			SeveritySource:   vulnerability.RedHat,
-			DataSource: &dbTypes.DataSource{
-				ID:   vulnerability.RedHat,
-				Name: "Red Hat",
-				URL:  "https://access.redhat.com/security/cve/",
-			},
+			DataSource:       adv.DataSource,
 			Vulnerability: dbTypes.Vulnerability{
 				Severity: adv.Severity.String(),
 			},
