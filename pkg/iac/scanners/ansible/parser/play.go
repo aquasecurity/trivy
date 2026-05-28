0bc5c6dfa453aa8866ec7fd27044b2603d77cbcc
Nikita Pivkin <nikita.pivkin@smartforce.io>
2026-05-28 14:53:40 +0600

    fix(misconf): reject nil plays during playbook parsing (#10273)
    Signed-off-by: nikpivkin <nikita.pivkin@smartforce.io>
    Co-authored-by: DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>

--- pkg/iac/scanners/ansible/parser/play.go
+++ pkg/iac/scanners/ansible/parser/play.go
@@ -3,6 +3,8 @@ package parser
 import (
 	"path/filepath"
 
+	"github.com/samber/lo"
+	"golang.org/x/xerrors"
 	"gopkg.in/yaml.v3"
 
 	"github.com/aquasecurity/trivy/pkg/iac/scanners/ansible/fsutils"
@@ -36,6 +38,15 @@ type Playbook struct {
 
 func (pb *Playbook) resolveIncludedSrc(incPath string) fsutils.FileSource {
 	return pb.Src.Dir().Join(incPath)
+}
+
+func parsePlays(f fsutils.FileSource) ([]*Play, error) {
+	var plays []*Play
+	if err := decodeYAMLFile(f, &plays); err != nil {
+		return nil, xerrors.Errorf("decode YAML file: %w", err)
+	}
+
+	return lo.Compact(plays), nil
 }
 
 // Play represents a single play in an Ansible playbook.
