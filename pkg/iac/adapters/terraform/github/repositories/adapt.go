abb5174b7e6932ffcee88ccd35c5c8cf7c55df12
Nikita Pivkin <nikita.pivkin@smartforce.io>
2026-06-29 16:55:28 +0600

    fix(misconf): support github_repository_vulnerability_alerts resource (#10680)

--- pkg/iac/adapters/terraform/github/repositories/adapt.go
+++ pkg/iac/adapters/terraform/github/repositories/adapt.go
@@ -14,19 +14,24 @@ func adaptRepositories(modules terraform.Modules) []github.Repository {
 	var repositories []github.Repository
 	for _, module := range modules {
 		for _, resource := range module.GetResourcesByType("github_repository") {
-			repositories = append(repositories, adaptRepository(resource))
+			repositories = append(repositories, adaptRepository(modules, resource))
 		}
 	}
 	return repositories
 }
 
-func adaptRepository(resource *terraform.Block) github.Repository {
-
+func adaptRepository(modules terraform.Modules, resource *terraform.Block) github.Repository {
 	repo := github.Repository{
 		Metadata:            resource.GetMetadata(),
 		Public:              types.Bool(true, resource.GetMetadata()),
 		VulnerabilityAlerts: resource.GetAttribute("vulnerability_alerts").AsBoolValueOrDefault(false, resource),
 		Archived:            resource.GetAttribute("archived").AsBoolValueOrDefault(false, resource),
+	}
+
+	// The standalone github_repository_vulnerability_alerts resource is the recommended
+	// approach (vulnerability_alerts on github_repository is deprecated). If present, it takes precedence.
+	if blocks := modules.GetReferencingResources(resource, "github_repository_vulnerability_alerts", "repository"); len(blocks) > 0 {
+		repo.VulnerabilityAlerts = blocks[0].GetAttribute("enabled").AsBoolValueOrDefault(true, blocks[0])
 	}
 
 	privateAttr := resource.GetAttribute("private")
