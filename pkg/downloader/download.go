5f5e0c6281a25a95ecc237049745424cf94f0ef6
Chiman Jain <chimanjain15@gmail.com>
2026-07-08 13:19:42 +0530

    chore: upgrade google/go-github dependency to v89.0.0
    Signed-off-by: Chiman Jain <chimanjain15@gmail.com>

--- pkg/downloader/download.go
+++ pkg/downloader/download.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v88/github"
+	"github.com/google/go-github/v89/github"
 	"github.com/hashicorp/go-getter"
 	"github.com/samber/lo"
 	"golang.org/x/xerrors"
