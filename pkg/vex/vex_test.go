ae561f8cca36b4b11434b0a6d171b39ec1ad871e
Ashvin <76151462+ashvinctrl@users.noreply.github.com>
2026-09-25 09:43:14 +0000

    fix(vex): avoid panic on CSAF relationships without a sub-component (#11067)

--- pkg/vex/vex_test.go
+++ pkg/vex/vex_test.go
@@ -184,6 +184,13 @@ var (
 		InstalledVersion: goTransitivePackage.Version,
 		PkgIdentifier:    goTransitivePackage.Identifier,
 	}
+	// CVE-2024-0001 detected on go-direct1
+	vuln6 = types.DetectedVulnerability{
+		VulnerabilityID:  "CVE-2024-0001",
+		PkgName:          goDirectPackage1.Name,
+		InstalledVersion: goDirectPackage1.Version,
+		PkgIdentifier:    goDirectPackage1.Identifier,
+	}
 )
 
 func TestMain(m *testing.M) {
@@ -537,6 +544,31 @@ func TestFilter(t *testing.T) {
 			want: imageReport([]types.Result{
 				goMultiPathResult(types.Result{
 					Vulnerabilities: []types.DetectedVulnerability{vuln5}, // Will not be filtered because of multi paths
+				}),
+			}),
+		},
+		{
+			name: "CSAF with relationships, vulnerability on the parent product, not the sub-component",
+			args: args{
+				// The statement covers go-transitive as a component of go-direct1,
+				// while the vulnerability is detected on go-direct1 itself.
+				report: imageReport([]types.Result{
+					goSinglePathResult(types.Result{
+						Vulnerabilities: []types.DetectedVulnerability{vuln6},
+					}),
+				}),
+				opts: vex.Options{
+					Sources: []vex.Source{
+						{
+							Type:     vex.TypeFile,
+							FilePath: "testdata/csaf-relationships.json",
+						},
+					},
+				},
+			},
+			want: imageReport([]types.Result{
+				goSinglePathResult(types.Result{
+					Vulnerabilities: []types.DetectedVulnerability{vuln6}, // The statement doesn't apply to the product itself
 				}),
 			}),
 		},
